26fec63ed14c3c9da4651266f3ec2b09ac89ea4a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-18 23:38:11 +0100

    Comments updated for ensure_browser_type().

--- src/browserist/model/browser.py
+++ src/browserist/model/browser.py
@@ -41,7 +41,7 @@ class BrowserClass(ABC):
 
     @abstractmethod
     def ensure_browser_type(self) -> None:
-        """Method to ensure the correct browser type if a specific browser instance is created directly from a subclass (e.g. ChromeBrowser, FirefoxBrowser, etc.) without the optional settings as argument."""
+        """Method to ensure the correct browser type if a specific browser instance is created directly from a subclass (e.g. FirefoxBrowser) without the optional settings as argument, simply as Chrome is default browser."""
         
         raise NotImplementedError
 
