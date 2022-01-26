14f075bdc492ca31bf22997144c9db92aeb5e9cb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-01-26 07:00:08 +0100

    Add version info.

--- src/browserist/__init__.py
+++ src/browserist/__init__.py
@@ -2,5 +2,7 @@ __all__ = ["browser", "helper", "model"]
 
 from . import browser, helper, model
 
+from .version import __version__
+
 from .browser import Browser
 from .model.browser import BrowserSettings, BrowserType
