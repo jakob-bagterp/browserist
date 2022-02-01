7c15cd9737be383819479c10786cae61b3aad618
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-01 17:04:04 +0100

    Minor reorder of imports.

--- src/browserist/__init__.py
+++ src/browserist/__init__.py
@@ -2,7 +2,7 @@ __all__ = ["browser", "helper", "model"]
 
 from . import browser, helper, model
 
-from .version import __version__
 
 from .browser import Browser
 from .model.browser import BrowserSettings, BrowserType
+from .version import __version__
