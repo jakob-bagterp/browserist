d34dd5f9cc4141f0419fe2bde7304c82116311b3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-04 14:26:50 +0100

    Add extra browser types: Edge, Safari, IE, Opera.

--- src/browserist/model/browser/base/type.py
+++ src/browserist/model/browser/base/type.py
@@ -5,4 +5,8 @@ class BrowserType(Enum):
     """Class to define browser type, e.g. Chrome, Firefox, etc."""
     
     CHROME = "Chrome"
+    EDGE = "Edge"
+    INTERNET_EXPLORER = "Internet Explorer"
     FIREFOX = "Firefox"
+    OPERA = "Opera"
+    SAFARI = "Safari"
