5b80b1f5cd54612a8663619443eac35767ed6d85
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-04 19:57:27 +0100

    Add settings model for page load strategy (reference: https://www.selenium.dev/documentation/webdriver/capabilities/shared/#pageloadstrategy).

---
+++ src/browserist/model/browser/base/page_load_strategy.py
@@ -0,0 +1,15 @@
+from enum import Enum, unique
+
+@unique
+class PageLoadStrategy(Enum):
+    """Class to configure page load strategy for Selenium.
+
+    NORMAL: Used by default. Waits for all resources to download. Ready state: Complete.
+
+    EAGER: DOM access is ready, but other resources like images may still be loading. Ready state: Interactive.
+
+    NONE: Does not block web driver at all. Ready state: Any."""
+    
+    NORMAL = "normal"    
+    EAGER = "eager"
+    NONE = "none"
