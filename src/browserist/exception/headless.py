253272d89e75bd83acf582cb126325060ec522e8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-04 17:53:07 +0100

    Minor fix to browser type in headless exception as an enum needs to point the its value to get the string representation.

--- src/browserist/exception/headless.py
+++ src/browserist/exception/headless.py
@@ -1,8 +1,8 @@
-from .browser.base.type import BrowserType
+from ..model.browser.base.type import BrowserType
 
 class HeadlessNotSupportedException(Exception):
     def __init__(self, browser_type: BrowserType) -> None:
-        self.message = f"{browser_type}: This browser doesn't support headless."
+        self.message = f"{browser_type.value}: This browser doesn't support headless."
         super().__init__(self.message)
 
     def __str__(self) -> str:
