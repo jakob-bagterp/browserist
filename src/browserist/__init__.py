46b7819911dffc9cfe84b80b628cf08a2206047c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-04 19:58:04 +0100

    Add page load strategy to settings, including default value.

--- src/browserist/__init__.py
+++ src/browserist/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["Browser", "BrowserSettings", "BrowserType"]
+__all__ = ["Browser", "BrowserSettings", "BrowserType", "PageLoadStrategy"]
 
 from .browser import Browser
+from .model.browser.base.page_load_strategy import PageLoadStrategy
 from .model.browser.base.settings import BrowserSettings
 from .model.browser.base.type import BrowserType
 from .version import __version__
