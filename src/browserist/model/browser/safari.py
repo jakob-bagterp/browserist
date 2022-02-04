2ed0c0b72856a3081ebc1f0d4f32c88b80d95954
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-04 18:11:53 +0100

    Safari: Add disable images function (reference: https://tarunlalwani.com/post/selenium-disable-image-loading-different-browsers/).

--- src/browserist/model/browser/safari.py
+++ src/browserist/model/browser/safari.py
@@ -1,3 +1,4 @@
+import subprocess
 from selenium import webdriver
 from .base.driver import BrowserDriver
 from .base.type import BrowserType
@@ -15,7 +16,8 @@ class SafariBrowserDriver(BrowserDriver):
                 executable_path = self.settings.path_to_executable)
 
     def disable_images(self) -> None:
-        pass
+        subprocess.call("defaults write WebKitDisplayImagesKey -bool false".split())
+        subprocess.call("defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2LoadsImagesAutomatically -bool false".split())
 
     def enable_headless(self) -> None:
         raise HeadlessNotSupportedException(self.settings.type)
