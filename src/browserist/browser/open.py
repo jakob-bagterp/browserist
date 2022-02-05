13f33fb8372bac2cf3313b6aad5a54cab02bff97
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-05 07:53:51 +0100

    Add browser driver to method initialiser.

--- src/browserist/browser/open.py
+++ src/browserist/browser/open.py
@@ -2,6 +2,7 @@ from ..model.browser.base.driver import BrowserDriver
 
 class Open():
     def __init__(self, browser_driver: BrowserDriver) -> None:
+        self._browser_driver: BrowserDriver = browser_driver
         self._driver: object = browser_driver.webdriver
 
     def url(self, url: str) -> None:
