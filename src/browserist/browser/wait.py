3359afdb35e3cc48741494a59646174f7b8dc0b9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-07 06:49:00 +0100

    Refactor wait methods into functional pattern.

--- src/browserist/browser/wait.py
+++ src/browserist/browser/wait.py
@@ -2,6 +2,9 @@ import time, random
 from ..model.browser.base.driver import BrowserDriver
 from ..model.driver_methods import DriverMethods
 
+def wait_random_time(self, min_seconds: int = 1, max_seconds: int = 5) -> None:
+    time.sleep(random.uniform(min_seconds, max_seconds))
+
 class WaitDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver) -> None:
         super().__init__(browser_driver)
@@ -9,4 +12,4 @@ class WaitDriverMethods(DriverMethods):
     def random_time(self, min_seconds: int = 1, max_seconds: int = 5) -> None:
         """Randomize sleep timing to make actions look less like a bot."""
 
-        time.sleep(random.uniform(min_seconds, max_seconds))
+        wait_random_time(min_seconds, max_seconds)
