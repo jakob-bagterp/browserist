37a85a5d0d45fdd67d30b6cbb135e3f12291971c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 20:30:10 +0100

    Minor fix of retry function to ensure return type is string.

--- src/browserist/helper/driver.py
+++ src/browserist/helper/driver.py
@@ -3,11 +3,11 @@ from typing import Callable
 from ..constant import timeout
 
 def retry_and_get_text_from_element(func: Callable[[object, str], str], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = 0.5) -> str:
-    text = func
+    text = str(func)
     i = 0
     retries = timeout / wait_interval_seconds
-    while len(text) == 0 and i < retries:
+    while not text and i < retries:
         time.sleep(wait_interval_seconds)
-        text = func
+        text = str(func)
         i += 1
     return text
