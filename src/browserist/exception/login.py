0e202bdb8dbe905eb58869dc6b1223fff2a8f40d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 08:42:21 +0100

    Add combo method and data objects for login.

---
+++ src/browserist/exception/login.py
@@ -0,0 +1,9 @@
+from ..model.browser.base.type import BrowserType
+
+class LoginException(Exception):
+    def __init__(self, username: str) -> None:
+        self.message = f"Something went wrong when trying to log in with username {username}. Please try again..."
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
