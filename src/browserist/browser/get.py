d389206a7ed75105e2eb7f026774006f2b38b206
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 18:50:29 +0100

    Add method to get text from element.

--- src/browserist/browser/get.py
+++ src/browserist/browser/get.py
@@ -1,8 +1,24 @@
+import time
+from .wait import wait_for_element
+from ..constant import timeout
 from ..model.browser.base.driver import BrowserDriver
 from ..model.driver_methods import DriverMethods
 
 def get_current_url(driver: object) -> str:
     return driver.current_url
+
+def get_text(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
+    def get_inner_text_of_element(xpath: str) -> str:
+        return driver.find_element_by_xpath(xpath).text
+    
+    wait_for_element(driver, xpath, timeout)
+    text = get_inner_text_of_element(xpath)
+    i = 0
+    while len(text) == 0 and i < 10:
+        time.sleep(0.5)
+        text = get_inner_text_of_element(xpath)
+        i += 1
+    return text
 
 class GetDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver) -> None:
@@ -12,3 +28,10 @@ class GetDriverMethods(DriverMethods):
         """Get URL of the current page."""
         
         return get_current_url(self._driver)
+
+    def text(self, xpath: str, timeout: int = timeout.DEFAULT) -> str:
+        """Get text from element.
+        
+        This method assumes that the text field shouldn't be empty and therefore will retry to get the text (for better support of single-page apps with extended loading time)."""
+
+        return get_text(self._driver, xpath, timeout)
