94564ca8ac9addfc82db5160e33dbfe397598211
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 07:37:31 +0100

    Add main classes for timeout strategy.

--- src/browserist/__init__.py
+++ src/browserist/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["Browser", "BrowserSettings", "BrowserType", "PageLoadStrategy"]
+__all__ = ["Browser", "BrowserSettings", "BrowserType", "PageLoadStrategy", "TimeoutSettings", "TimeoutStrategy"]
 
 from .browser import Browser
 from .model.browser.base.page_load_strategy import PageLoadStrategy
 from .model.browser.base.settings import BrowserSettings
 from .model.browser.base.type import BrowserType
+from .model.timeout_strategy import TimeoutSettings, TimeoutStrategy
 from .version import __version__
