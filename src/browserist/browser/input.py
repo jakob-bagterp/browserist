e93f52e0dbcd386fa029261934145c8cdf076396
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 07:44:05 +0100

    Add method to clear values from form field.

--- src/browserist/browser/input.py
+++ src/browserist/browser/input.py
@@ -5,6 +5,16 @@ from ..exception.timeout import WaitTimeoutException
 from ..model.browser.base.driver import BrowserDriver
 from ..model.driver_methods import DriverMethods
 
+def input_clear(driver: object, input_xpath: str) -> None:
+    wait_for_element(driver, input_xpath)
+    try:
+        input_field = driver.find_element_by_xpath(input_xpath)
+        input_field.clear()
+    except TimeoutException:
+        raise WaitTimeoutException(driver, input_xpath)
+    except NoSuchElementException:
+        raise NoElementFoundException(driver, input_xpath)
+
 def input_value(driver: object, input_xpath: str, value: str) -> None:
     wait_for_element(driver, input_xpath)
     try:
@@ -19,6 +29,11 @@ def input_value(driver: object, input_xpath: str, value: str) -> None:
 class InputDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver) -> None:
         super().__init__(browser_driver)
+    
+    def clear(self, input_xpath: str) -> None:
+        """Clear input form field."""
+
+        input_clear(self._driver, input_xpath)
 
     def value(self, input_xpath: str, value: str) -> None:
         """Input value into form field."""
