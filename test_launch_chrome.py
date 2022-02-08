46280b6f491ded61dda735ff58e8cc66728ca580
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 19:34:53 +0100

    Add tests of get text methods.

--- test_launch_chrome.py
+++ test_launch_chrome.py
@@ -9,6 +9,8 @@ browser.refresh()
 
 #browser.get("https://github.com/jakob-bagterp/")
 browser.open.url("https://github.com/jakob-bagterp/")
+print(browser.get.text_from_element("/html/body/div[4]/main/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/h1"))
+print(browser.get.texts_from_multiple_elements("/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div/div[1]/div/ol/li/div/div/div/div/a"))
 xpath = "/html/body/div[4]/main/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/a/img"
 browser.wait.for_element(xpath)
 print(browser.check_if.is_element_clickable(xpath))
