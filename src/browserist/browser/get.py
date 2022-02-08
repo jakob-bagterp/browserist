6dba2ec129e2d9e3fc446f698b8a2577a69dd4c4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-08 19:06:12 +0100

    Rename get text functions so they're easier to distinguish.

--- src/browserist/browser/get.py
+++ src/browserist/browser/get.py
@@ -8,7 +8,7 @@ from ..model.driver_methods import DriverMethods
 def get_current_url(driver: object) -> str:
     return driver.current_url
 
-def get_text(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
+def get_text_from_element(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
     def get_inner_text_of_element(xpath: str) -> str:
         return driver.find_element_by_xpath(xpath).text
     
@@ -21,7 +21,7 @@ def get_text(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
         i += 1
     return text
 
-def get_texts_from_elements(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> List[str]:
+def get_texts_from_multiple_elements(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> List[str]:
     wait_for_element(driver, xpath, timeout)
     elements = browser.find_elements_by_xpath(xpath)
     return [element.text for element in elements]
@@ -35,16 +35,16 @@ class GetDriverMethods(DriverMethods):
 
         return get_current_url(self._driver)
 
-    def text(self, xpath: str, timeout: int = timeout.DEFAULT) -> str:
+    def text_from_element(self, xpath: str, timeout: int = timeout.DEFAULT) -> str:
         """Get text from element.
         
         This method assumes that the text field shouldn't be empty and therefore will retry to get the text (for better support of single-page apps with extended loading time)."""
 
-        return get_text(self._driver, xpath, timeout)
+        return get_text_from_element(self._driver, xpath, timeout)
 
-    def multiple_texts(self, xpath: str, timeout: int = timeout.DEFAULT) -> List[str]:
+    def texts_from_multiple_elements(self, xpath: str, timeout: int = timeout.DEFAULT) -> List[str]:
         """Get array of texts from elements.
         
         Assumes that the XPath targets multiple elements."""
         
-        return get_texts_from_elements(self._driver, xpath, timeout)
+        return get_texts_from_multiple_elements(self._driver, xpath, timeout)
