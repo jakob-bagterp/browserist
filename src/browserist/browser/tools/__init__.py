337406d6a009fba1a331f0b02d3283fd016e93d1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-10 17:21:22 +0100

    Refactor and move tools_is_input_valid into separate file.

--- src/browserist/browser/tools/__init__.py
+++ src/browserist/browser/tools/__init__.py
@@ -1,15 +1,9 @@
 import re
+from .is_input_valid import tools_is_input_valid
 from ..wait.for_element import wait_for_element
 from ...constant import regex, timeout
 from ...model.browser.base.driver import BrowserDriver
 from ...model.driver_methods import DriverMethods
-
-def is_input_valid(text: str, regex: str, ignore_case: bool = True) -> bool:
-    if ignore_case:
-        match = re.match(regex, text, re.IGNORECASE)
-    else:
-        match = re.match(regex, text)
-    return bool(match)
 
 def is_url_valid(url: str) -> bool:
     return bool(re.match(regex.VALID_URL, url, re.IGNORECASE))
@@ -26,7 +20,7 @@ class ToolsDriverMethods(DriverMethods):
     def is_input_valid(self, text: str, regex: str, ignore_case: bool = True) -> bool:
         """Check if input matches regex condition."""
 
-        return is_input_valid(text, regex, ignore_case)
+        return tools_is_input_valid(text, regex, ignore_case)
 
     def is_url_valid(self, url: str) -> bool:
         """Check if input is a valid URL."""
---
+++ src/browserist/browser/tools/is_input_valid.py
@@ -0,0 +1,8 @@
+import re
+
+def tools_is_input_valid(text: str, regex: str, ignore_case: bool = True) -> bool:
+    if ignore_case:
+        match = re.match(regex, text, re.IGNORECASE)
+    else:
+        match = re.match(regex, text)
+    return bool(match)
