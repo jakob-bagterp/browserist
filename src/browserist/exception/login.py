2689684994110e979611bdad4c70632c10ad71b2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-10 18:33:57 +0100

    Minor update to exception message so the variable is in double quotes.

--- src/browserist/exception/login.py
+++ src/browserist/exception/login.py
@@ -1,6 +1,6 @@
 class LoginException(Exception):
     def __init__(self, username: str) -> None:
-        self.message = f"Something went wrong when trying to log in with username {username}. Please try again..."
+        self.message = f"Something went wrong when trying to log in with username \"{username}\". Please try again..."
         super().__init__(self.message)
 
     def __str__(self) -> str:
