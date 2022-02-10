4aa43a8f31a01eb7dc5a295319cd1fd177ddbe62
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-10 12:16:31 +0100

    Minor linting update of white space.

--- src/browserist/browser/check_if_element_contains_text.py
+++ src/browserist/browser/check_if_element_contains_text.py
@@ -3,9 +3,9 @@ from .get import get_text_from_element
 from ..constant import timeout
 
 def check_if_element_contains_text(driver: object, xpath: str, regex: str, ignore_case: bool = True, timeout: int = timeout.DEFAULT) -> bool:
-	current_text = get_text_from_element(driver, xpath, timeout)
-	if ignore_case:
-		match = re.search(regex, current_text, re.IGNORECASE)
-	else:
-		match = re.search(regex, current_text)
-	return bool(match)
+    current_text = get_text_from_element(driver, xpath, timeout)
+    if ignore_case:
+        match = re.search(regex, current_text, re.IGNORECASE)
+    else:
+        match = re.search(regex, current_text)
+    return bool(match)
