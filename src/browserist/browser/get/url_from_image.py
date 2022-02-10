57467360488b5d6299732a1f378b5c45a7dbbc80
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-10 16:28:48 +0100

    Refactor get URL from image and link to use new get attribute method.

--- src/browserist/browser/get/url_from_image.py
+++ src/browserist/browser/get/url_from_image.py
@@ -1,10 +1,12 @@
+from .value_from_attribute import get_value_from_attribute
 from ..wait.for_element import wait_for_element
+from ... import constant
 from ... import helper
 from ...constant import timeout
 
 def get_url_from_image(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
     def get_src_attribute_of_element(driver: object, xpath: str) -> str:
-        return driver.find_element_by_xpath(xpath).get_attribute("src")
+        return get_value_from_attribute(driver, xpath, "src", constant.timeout.BYPASS)
 
     wait_for_element(driver, xpath, timeout)
     return helper.driver.retry_and_get_text_from_element(get_src_attribute_of_element(driver, xpath))
--- src/browserist/browser/get/url_from_link.py
+++ src/browserist/browser/get/url_from_link.py
@@ -1,10 +1,12 @@
+from .value_from_attribute import get_value_from_attribute
 from ..wait.for_element import wait_for_element
+from ... import constant
 from ... import helper
 from ...constant import timeout
 
 def get_url_from_link(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
     def get_href_attribute_of_element(driver: object, xpath: str) -> str:
-        return driver.find_element_by_xpath(xpath).get_attribute("href")
+        return get_value_from_attribute(driver, xpath, "href", constant.timeout.BYPASS)
 
     wait_for_element(driver, xpath, timeout)
     return helper.driver.retry_and_get_text_from_element(get_href_attribute_of_element(driver, xpath))
