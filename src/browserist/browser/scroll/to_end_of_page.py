b95b31b7ec9f6f591d3dda8eadd58e8a97d45ced
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-12 00:21:51 +0100

    Minor add comments.

--- src/browserist/browser/scroll/to_end_of_page.py
+++ src/browserist/browser/scroll/to_end_of_page.py
@@ -8,6 +8,6 @@ def scroll_to_end_of_page(driver: object):
     try:
         body = driver.find_element_by_tag_name("body") # Select the whole page before pressing any keys...
         body.send_keys(Keys.END) # ... and then simulate pressing End on the keyboard.
-        time.sleep(timeout.VERY_SHORT)
+        time.sleep(timeout.VERY_SHORT) # Small delay to ensure the view is updated.
     except NoSuchElementException:
         raise NoElementFoundException(driver, "body") from NoSuchElementException
