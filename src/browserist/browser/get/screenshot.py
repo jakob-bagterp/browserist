1a619534bc194fb5115a2484b12f66c4694fac9c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-12 23:51:06 +0100

    Ensure that there's always a slash "/" between the directory and filename.

--- src/browserist/browser/get/screenshot.py
+++ src/browserist/browser/get/screenshot.py
@@ -7,5 +7,6 @@ def get_screenshot(driver: object, settings: BrowserSettings, file_name: str | N
     if destination_dir is None:
         destination_dir = settings.screenshot_dir
     else:
+        destination_dir = helper.directory.ensure_trailing_slash(destination_dir)
         helper.directory.create_if_not_exists(destination_dir)
     driver.save_screenshot(f"{destination_dir}{file_name}")
--- src/browserist/helper/directory.py
+++ src/browserist/helper/directory.py
@@ -6,3 +6,6 @@ def create_if_not_exists(dir_name: str) -> None:
         return
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
+
+def ensure_trailing_slash(dir_name: str) -> str:
+    return dir_name if dir_name[-1] == "/" else f"{dir_name}/"
