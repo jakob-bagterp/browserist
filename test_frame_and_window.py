2a6c75036bb6ab0c89c8c45dc3e6b2c5039a2524
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-13 22:57:04 +0100

    Split main test into frame and window methods.

---
+++ test_frame_and_window.py
@@ -0,0 +1,27 @@
+from src.browserist import Browser
+
+browser = Browser()
+
+print("window_size", browser.window.get.size())
+print("window_position", browser.window.get.position())
+browser.window.set.position(0, 0)
+print("window_position", browser.window.get.position())
+
+browser.open.url_if_not_current("https://www.dr.dk/")
+browser.window.set.size(300, 300)
+print("window_size", browser.window.get.size())
+browser.window.minimize()
+browser.window.maximize()
+browser.window.fullscreen()
+
+browser.open.url_in_new_tab("https://github.com/jakob-bagterp/")
+print("current_handle", browser.window.get.current_handle())
+print(browser.get.url.current())
+browser.open.new_tab()
+browser.close()
+browser.open.new_window()
+window_handle = browser.window.get.current_handle()
+print("", window_handle)
+browser.switch_to.original_window()
+browser.switch_to.window(window_handle)
+browser.close()
