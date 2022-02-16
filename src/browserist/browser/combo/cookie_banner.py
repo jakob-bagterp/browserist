1727e29da6d59ed064e8f3269737283b2c6a076d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-16 20:43:58 +0100

    Optimise timing of post click for cookie banner so the browser has time to write the cookie.

--- src/browserist/browser/combo/cookie_banner.py
+++ src/browserist/browser/combo/cookie_banner.py
@@ -3,6 +3,7 @@ from ..click.button import click_button
 from ..open.url_if_not_current import open_url_if_not_current
 from ..wait.until_element_disappears import wait_until_element_disappears
 from ..wait.for_element import wait_for_element
+from ...constant import timeout
 from ...exception.cookie_banner import CookieBannerException
 from ...model.combo_settings.cookie_banner import CookieBannerSettings
 
@@ -17,6 +18,8 @@ def combo_cookie_banner(driver: object, settings: CookieBannerSettings) -> None:
         click_button(driver, settings.button_xpath)
         if settings.has_disappeared_wait_seconds is not None:
             time.sleep(settings.has_disappeared_wait_seconds)
+        else:
+            time.sleep(timeout.VERY_SHORT)
         wait_until_element_disappears(driver, settings.button_xpath)
     except Exception:
         raise CookieBannerException() from Exception
