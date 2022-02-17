a2635059574f4e80efc05c94232222c8655f5107
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 21:48:02 +0100

    Fix type settings for Edge configuration.

--- test/config/browser_settings/edge.py
+++ test/config/browser_settings/edge.py
@@ -1,16 +1,16 @@
 from src.browserist import BrowserSettings, BrowserType
 
 DEFAULT = BrowserSettings(
-    type = BrowserType.CHROME,
+    type = BrowserType.EDGE,
 )
 
 HEADLESS = BrowserSettings(
-    type     = BrowserType.CHROME,
+    type     = BrowserType.EDGE,
     headless = True
 )
 
 HEADLESS_AND_DISABLE_IMAGES = BrowserSettings(
-    type           = BrowserType.CHROME,
+    type           = BrowserType.EDGE,
     headless       = True,
     disable_images = True
 )
