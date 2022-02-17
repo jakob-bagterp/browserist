3885dccd39c1530da583f7d1fed0489515e12bf0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 22:51:54 +0100

    Fix warning "DeprecationWarning: find_element_by_* commands are deprecated. Please use find_element() instead" (part 2).

--- src/browserist/browser/get/attribute/value.py
+++ src/browserist/browser/get/attribute/value.py
@@ -1,6 +1,7 @@
+from selenium.webdriver.common.by import By
 from ...wait.for_element import wait_for_element
 from ....constant import timeout
 
 def get_attribute_value(driver: object, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> str:
     wait_for_element(driver, xpath, timeout)
-    return driver.find_element_by_xpath(xpath).get_attribute(attribute)
+    return driver.find_element(By.XPATH, xpath).get_attribute(attribute)
