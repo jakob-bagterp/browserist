6d4c352691b7bc16476c47c780ba82a9b04adc24
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 22:49:41 +0100

    Fix warning "DeprecationWarning: find_element_by_* commands are deprecated. Please use find_element() instead" (part 1).

--- src/browserist/browser/get/text/from_element.py
+++ src/browserist/browser/get/text/from_element.py
@@ -1,10 +1,11 @@
+from selenium.webdriver.common.by import By
 from ...wait.for_element import wait_for_element
 from .... import helper
 from ....constant import timeout
 
 def get_text_from_element(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> str:
     def get_inner_text_of_element(driver: object, xpath: str) -> str:
-        return driver.find_element_by_xpath(xpath).text
+        return driver.find_element(By.XPATH, xpath).text
 
     wait_for_element(driver, xpath, timeout)
     return helper.retry.get_text_from_element(get_inner_text_of_element(driver, xpath))
