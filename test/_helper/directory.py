519615e63b82d18f9e918a0c4580cb12e2e80eb6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 21:54:48 +0100

    Move helper and mock data to test base directory so it's easier to separate from the the static and dynamic test directories.

--- test/_helper/directory.py
+++ test/_helper/directory.py
@@ -3,4 +3,4 @@ from pathlib import Path
 def get_path_for_static_mock_data() -> str:
     path_of_this_file = Path(__file__)
     path_of_static_directory = path_of_this_file.parent.parent.absolute()
-    return f"{path_of_static_directory}/mock_data"
+    return f"{path_of_static_directory}/_mock_data"
