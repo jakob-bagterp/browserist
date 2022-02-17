0e24231b450a82c0df986c6aa303e4d0673db683
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 01:25:46 +0100

    Minor tweak: Ensure number of retries left is an integer rather and a float.

--- src/browserist/helper/retry.py
+++ src/browserist/helper/retry.py
@@ -2,9 +2,12 @@ import time
 from typing import Callable, List
 from ..constant import interval, timeout
 
+def calculate_number_of_retries(total_time: int, interval: int | float) -> int:
+    return int(total_time // interval)
+
 def get_text_from_element(func: Callable[[object, str], str], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> str:
     text = str(func)
-    retries_left = timeout / wait_interval_seconds
+    retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while not text and retries_left > 0:
         time.sleep(wait_interval_seconds)
         text = str(func)
@@ -12,14 +15,14 @@ def get_text_from_element(func: Callable[[object, str], str], timeout: int = tim
     return text
 
 def until_condition_is_true(func: Callable[[object, str | List[object]], bool], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
-    retries_left = timeout / wait_interval_seconds
+    retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func is False and retries_left > 0:
         time.sleep(wait_interval_seconds)
         retries_left -= 1
         # TODO: Raise exception if it times out/runs out of retries.
 
 def until_condition_is_false(func: Callable[[object, str | List[object]], bool], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
-    retries_left = timeout / wait_interval_seconds
+    retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func is True and retries_left > 0:
         time.sleep(wait_interval_seconds)
         retries_left -= 1
