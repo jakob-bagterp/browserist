c45ecf51316630d00d60c918cd8103f19cc49969
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-17 01:12:37 +0100

    Refactor retry helper so number of retries is a countdown instead of an iteration.

--- src/browserist/helper/retry.py
+++ src/browserist/helper/retry.py
@@ -4,26 +4,23 @@ from ..constant import interval, timeout
 
 def get_text_from_element(func: Callable[[object, str], str], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> str:
     text = str(func)
-    i = 0
-    retries = timeout / wait_interval_seconds
-    while not text and i < retries:
+    retries_left = timeout / wait_interval_seconds
+    while not text and retries_left > 0:
         time.sleep(wait_interval_seconds)
         text = str(func)
-        i += 1
+        retries_left -= 1
     return text
 
 def until_condition_is_true(func: Callable[[object, str | List[object]], bool], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
-    i = 0
-    retries = timeout / wait_interval_seconds
-    while func is False and i < retries:
+    retries_left = timeout / wait_interval_seconds
+    while func is False and retries_left > 0:
         time.sleep(wait_interval_seconds)
-        i += 1
+        retries_left -= 1
         # TODO: Raise exception if it times out/runs out of retries.
 
 def until_condition_is_false(func: Callable[[object, str | List[object]], bool], timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
-    i = 0
-    retries = timeout / wait_interval_seconds
-    while func is True and i < retries:
+    retries_left = timeout / wait_interval_seconds
+    while func is True and retries_left > 0:
         time.sleep(wait_interval_seconds)
-        i += 1
+        retries_left -= 1
         # TODO: Raise exception if it times out/runs out of retries.
