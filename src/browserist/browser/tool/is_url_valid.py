5d162eceeb289aa6c59a3d899432740ee73c2f09
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 00:14:40 +0100

    Precompile regex pattern for check validity of URLs so it's cached and faster.

--- src/browserist/browser/tool/is_url_valid.py
+++ src/browserist/browser/tool/is_url_valid.py
@@ -1,5 +1,7 @@
 import re
 from ... import constant
 
+_url_pattern = re.compile(constant.regex.VALID_URL, re.IGNORECASE)
+
 def tool_is_url_valid(url: str) -> bool:
-    return bool(re.match(constant.regex.VALID_URL, url, re.IGNORECASE))
+    return bool(_url_pattern.match(url))
