125251cd0e9367a1365062f2903cca5943e7850a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-18 23:58:39 +0100

    Add tests of is input valid tool method.

---
+++ test/tool/is_input_valid_test.py
@@ -0,0 +1,21 @@
+from browserist import Browser
+from _config.browser_settings import default
+from _helper import internal_url
+
+def with_browser_tool_is_input_valid(text: str, regex: str, ignore_case: bool) -> bool:
+    with Browser(default.HEADLESS) as browser:
+        browser.open.url(internal_url.EXAMPLE_COM)
+        return browser.tool.is_input_valid(text, regex, ignore_case)
+
+class TestToolIsInputValid():
+    def test_tool_is_input_valid_1(self) -> None:
+        assert with_browser_tool_is_input_valid("test", r"test", ignore_case = False) is True
+
+    def test_tool_is_input_valid_2(self) -> None:
+        assert with_browser_tool_is_input_valid("test", r"TEST", ignore_case = False) is False
+
+    def test_tool_is_input_valid_3(self) -> None:
+        assert with_browser_tool_is_input_valid("test", r"tEsT", ignore_case = True) is True
+
+    def test_tool_is_input_valid_4(self) -> None:
+        assert with_browser_tool_is_input_valid("testing", r"test", ignore_case = False) is False
