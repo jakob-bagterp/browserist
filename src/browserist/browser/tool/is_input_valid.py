a5a5ee828352acc6fa858b759950cd46da36f24a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 00:02:50 +0100

    Fix re.match to re.fullmatch as the intention is to check exact matches (https://docs.python.org/3/library/re.html#re.fullmatch).

--- src/browserist/browser/tool/is_input_valid.py
+++ src/browserist/browser/tool/is_input_valid.py
@@ -2,7 +2,7 @@ import re
 
 def tool_is_input_valid(text: str, regex: str, ignore_case: bool = True) -> bool:
     if ignore_case:
-        match = re.match(regex, text, re.IGNORECASE)
+        match = re.fullmatch(regex, text, re.IGNORECASE)
     else:
-        match = re.match(regex, text)
+        match = re.fullmatch(regex, text)
     return bool(match)
