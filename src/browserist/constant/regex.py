08f71d8e46c423189773e32f5bdd451a24481bd8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 00:13:54 +0100

    Minor update comments.

--- src/browserist/constant/regex.py
+++ src/browserist/constant/regex.py
@@ -2,4 +2,5 @@ VALID_URL: str = r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$
 """References for regex pattern:
 
 https://www.regextester.com/94502
+
 https://mathiasbynens.be/demo/url-regex"""
