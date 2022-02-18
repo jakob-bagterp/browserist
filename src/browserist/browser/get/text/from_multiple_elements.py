39fc0e61e5bda22255b62de5c84d97a556709ca7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-18 23:32:34 +0100

    Fix warning "DeprecationWarning: find_element_by_* commands are deprecated. Please use find_element() instead" (part 4).

--- src/browserist/browser/get/text/from_multiple_elements.py
+++ src/browserist/browser/get/text/from_multiple_elements.py
@@ -1,8 +1,9 @@
+from selenium.webdriver.common.by import By
 from typing import List
 from ...wait.for_element import wait_for_element
 from ....constant import timeout
 
 def get_text_from_multiple_elements(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> List[str]:
     wait_for_element(driver, xpath, timeout)
-    elements = driver.find_elements_by_xpath(xpath)
+    elements = driver.find_element(By.XPATH, xpath)
     return [element.text for element in elements]
--- src/browserist/browser/tool/count_elements.py
+++ src/browserist/browser/tool/count_elements.py
@@ -1,7 +1,8 @@
+from selenium.webdriver.common.by import By
 from ..wait.for_element import wait_for_element
 from ...constant import timeout
 
 def tool_count_elements(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> int:
     wait_for_element(driver, xpath, timeout)
-    elements = driver.find_elements_by_xpath(xpath)
+    elements = driver.find_element(By.XPATH, xpath)
     return len(elements)
