e12234dd52a759e3e979565b856f47e8637160e2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-18 22:40:33 +0100

    Add exception if retry helper runs out of iterations.

--- src/browserist/helper/retry.py
+++ src/browserist/helper/retry.py
@@ -1,5 +1,6 @@
 import time
 from ..constant import interval, timeout
+from ..exception.retry import RetryTimeoutException
 from ..model.callable import DriverGetBoolCallable, DriverGetTextCallable
 
 def calculate_number_of_retries(total_time: int, interval: int | float) -> int:
@@ -12,6 +13,8 @@ def get_text_from_element(func: DriverGetTextCallable, timeout: int = timeout.DE
         time.sleep(wait_interval_seconds)
         text = str(func)
         retries_left -= 1
+        if retries_left == 0:
+            raise RetryTimeoutException(func)
     return text
 
 def until_condition_is_true(func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
@@ -19,11 +22,13 @@ def until_condition_is_true(func: DriverGetBoolCallable, timeout: int = timeout.
     while func is False and retries_left > 0:
         time.sleep(wait_interval_seconds)
         retries_left -= 1
-        # TODO: Raise exception if it times out/runs out of retries.
+        if retries_left == 0:
+            raise RetryTimeoutException(func)
 
 def until_condition_is_false(func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func is True and retries_left > 0:
         time.sleep(wait_interval_seconds)
         retries_left -= 1
-        # TODO: Raise exception if it times out/runs out of retries.
+        if retries_left == 0:
+            raise RetryTimeoutException(func)
