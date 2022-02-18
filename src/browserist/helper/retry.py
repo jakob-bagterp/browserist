ba4d014f9815cb6c80748589b355e959e6a41ea8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-18 22:38:02 +0100

    Refactor callables for retry helper into separate model.

--- src/browserist/helper/retry.py
+++ src/browserist/helper/retry.py
@@ -1,14 +1,11 @@
 import time
-from typing import Callable, List
 from ..constant import interval, timeout
-
-_DriverGetTextCallable = Callable[[object, str], str]
-_DriverGetBoolCallable = Callable[[object, str | List[object]], bool]
+from ..model.callable import DriverGetBoolCallable, DriverGetTextCallable
 
 def calculate_number_of_retries(total_time: int, interval: int | float) -> int:
     return int(total_time // interval)
 
-def get_text_from_element(func: _DriverGetTextCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> str:
+def get_text_from_element(func: DriverGetTextCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> str:
     text = str(func)
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while not text and retries_left > 0:
@@ -17,14 +14,14 @@ def get_text_from_element(func: _DriverGetTextCallable, timeout: int = timeout.D
         retries_left -= 1
     return text
 
-def until_condition_is_true(func: _DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
+def until_condition_is_true(func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func is False and retries_left > 0:
         time.sleep(wait_interval_seconds)
         retries_left -= 1
         # TODO: Raise exception if it times out/runs out of retries.
 
-def until_condition_is_false(func: _DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
+def until_condition_is_false(func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func is True and retries_left > 0:
         time.sleep(wait_interval_seconds)
