b2d4bef186a87279822ec9df78f233ef4c3e9d23
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 13:20:21 +0100

    Add fixture and first attempt to reuse shared, default browser across tests (so it's much faster instead of creating new instances of Browser for each test).

---
+++ test/browser/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from browserist import Browser
+from typing import Generator
+
+from _config.browser_settings import default
+
+@pytest.fixture(scope = "session")
+def browser_default_headless() -> Generator[Browser, None, None]:
+    """Reuse a shared Browser in default, headless mode across tests so each test doesn't have to initialize a new Browser, which is slower."""
+
+    with Browser(default.HEADLESS) as browser:
+        yield browser
