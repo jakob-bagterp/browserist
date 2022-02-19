6104e13272268811996bcae647dc3e3e69f2f318
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-19 11:55:32 +0100

    Refactor test of is input valid so it uses the pytest.mark.parametrize decorator instead of multiple functions of the same.

--- test/tool/is_input_valid_test.py
+++ test/tool/is_input_valid_test.py
@@ -1,20 +1,15 @@
+import pytest
 from browserist import Browser
 from _config.browser_settings import default
 from _helper import internal_url
 
-def with_browser_tool_is_input_valid(text: str, regex: str, ignore_case: bool) -> bool:
+@pytest.mark.parametrize("text, regex, ignore_case, expected", [
+    ("test", r"test", False, True),
+    ("test", r"TEST", False, False),
+    ("test", r"tEsT", True, True),
+    ("testing", r"test", False, False)
+])
+def test_tool_is_input_valid(text: str, regex: str, ignore_case: bool, expected: bool) -> None:
     with Browser(default.HEADLESS) as browser:
         browser.open.url(internal_url.EXAMPLE_COM)
-        return browser.tool.is_input_valid(text, regex, ignore_case)
-
-def test_tool_is_input_valid_1() -> None:
-    assert with_browser_tool_is_input_valid("test", r"test", ignore_case = False) is True
-
-def test_tool_is_input_valid_2() -> None:
-    assert with_browser_tool_is_input_valid("test", r"TEST", ignore_case = False) is False
-
-def test_tool_is_input_valid_3() -> None:
-    assert with_browser_tool_is_input_valid("test", r"tEsT", ignore_case = True) is True
-
-def test_tool_is_input_valid_4() -> None:
-    assert with_browser_tool_is_input_valid("testing", r"test", ignore_case = False) is False
+        assert browser.tool.is_input_valid(text, regex, ignore_case) is expected
