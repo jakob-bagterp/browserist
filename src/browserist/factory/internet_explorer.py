cf16ba5f7b540ab71605615e7911f133d35e8e18
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 00:18:44 +0100

    Fix: Handle Windows-specific issue since winreg is only relevant in a Microsoft OS context : "ModuleNotFoundError: No module named winreg"

--- src/browserist/factory/internet_explorer.py
+++ src/browserist/factory/internet_explorer.py
@@ -1,11 +1,12 @@
-# from winreg import CloseKey, OpenKey, SetValueEx, HKEY_CURRENT_USER, KEY_ALL_ACCESS, REG_SZ
-# TODO: Fix ModuleNotFoundError of winreg
+import sys
+if sys.platform.startswith("win32"):
+    from winreg import CloseKey, OpenKey, SetValueEx, HKEY_CURRENT_USER, KEY_ALL_ACCESS, REG_SZ
 from ..model.browser.base.driver import BrowserDriver
 from ..model.browser.base.type import BrowserType
 
 
 def set_image_loading(browser_driver: BrowserDriver, load_images: bool = True):
-    if browser_driver.settings.type is BrowserType.INTERNET_EXPLORER:
+    if browser_driver.settings.type is BrowserType.INTERNET_EXPLORER and sys.platform.startswith("win32"):
         value = "yes" if load_images else "no"
         key = OpenKey(HKEY_CURRENT_USER, r"Software\Microsoft\Internet Explorer\Main", 0, KEY_ALL_ACCESS)
         SetValueEx(key, "Display Inline Images", 0, REG_SZ, value)
