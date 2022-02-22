ef76929818de55d4afa12ef2b59e507166a2e94f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 11:54:27 +0100

    Add new helper method to remove parameters from URLs, including unit test.

--- src/browserist/helper/url.py
+++ src/browserist/helper/url.py
@@ -2,3 +2,7 @@ def ensure_trailing_slash(url: str) -> str:
     if "?" in url:  # If the URL contains a parameter (e.g. https://example.com/search?page=1), ignore trailing slash.
         return url
     return url if url[-1] == "/" else f"{url}/"
+
+
+def remove_parameters(url: str) -> str:
+    return url if "?" not in url else url.split("?")[0]
