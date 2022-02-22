f955e64e750e96c9c105ef42d6d649cdcb28755a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 15:15:16 +0100

    Finalise option to ignore HTTPS in open URL if not current method.

--- src/browserist/browser/open/__main__.py
+++ src/browserist/browser/open/__main__.py
@@ -27,14 +27,16 @@ class OpenDriverMethods(DriverMethods):
 
         open_url(self._driver, url)
 
-    def url_if_not_current(self, url: str, ignore_trailing_slash: bool = True, ignore_parameters: bool = False) -> None:
+    def url_if_not_current(self, url: str, ignore_trailing_slash: bool = True, ignore_parameters: bool = False, ignore_https: bool = False) -> None:
         """Open a URL if it isn't already the current URL. Useful when doing multiple operations on a page where.
 
         ignore_trailing_slash: Ignore whether the URL is "http://example.com" or "http://example.com/".
 
-        ignore_parameters: Ignore parameters in the URL, e.g. "http://example.com/list?page=1"."""
+        ignore_parameters: Ignore parameters in the URL, e.g. "http://example.com/list?page=1".
 
-        open_url_if_not_current(self._driver, url, ignore_trailing_slash, ignore_parameters)
+        ignore_https: Ignore whether the URL is "http://example.com" or "https://example.com"."""
+
+        open_url_if_not_current(self._driver, url, ignore_trailing_slash, ignore_parameters, ignore_https)
 
     def url_in_new_tab(self, url: str) -> None:
         """Open a link in a new browser tab with support across browsers."""
--- src/browserist/browser/open/url_if_not_current.py
+++ src/browserist/browser/open/url_if_not_current.py
@@ -3,9 +3,10 @@ from ..get.url.current import get_current_url
 from .url import open_url
 
 
-def open_url_if_not_current(driver: object, url: str, ignore_trailing_slash: bool = True, ignore_parameters: bool = False) -> None:
-    # TODO: Ignore HTTP?(S) part of URL
+def open_url_if_not_current(driver: object, url: str, ignore_trailing_slash: bool = True, ignore_parameters: bool = False, ignore_https: bool = False) -> None:
     current_url = get_current_url(driver)
+    if ignore_https:
+        current_url, url = helper.url.mediate_https(current_url, url)
     if ignore_parameters:
         current_url = helper.url.remove_parameters(current_url)
         url = helper.url.remove_parameters(url)
