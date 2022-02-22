0150a8d4071a1938e3cd2db8b3d2d7c460a11037
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 11:51:06 +0100

    Finalise option to ignore trailing slash in open URL if not current method.

--- src/browserist/browser/open/url_if_not_current.py
+++ src/browserist/browser/open/url_if_not_current.py
@@ -1,11 +1,14 @@
+from ... import helper
 from ..get.url.current import get_current_url
 from .url import open_url
 
 
 def open_url_if_not_current(driver: object, url: str, ignore_trailing_slash: bool = True) -> None:
-    # TODO: Evaluate without trailing slash
     # TODO: Ignore HTTP?(S) part of URL
     # TODO: Option to ignore parameters in URL
     current_url = get_current_url(driver)
+    if ignore_trailing_slash:
+        current_url = helper.url.ensure_trailing_slash(current_url)
+        url = helper.url.ensure_trailing_slash(url)
     if current_url != url:
         open_url(driver, url)
