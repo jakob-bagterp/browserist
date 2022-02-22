058006a071f4a63c45703f011c169ad150edfaa6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 15:09:42 +0100

    Add new helper method to mediate HTTPS in URLs, including unit test.

--- src/browserist/helper/url.py
+++ src/browserist/helper/url.py
@@ -4,5 +4,19 @@ def ensure_trailing_slash(url: str) -> str:
     return url if url[-1] == "/" else f"{url}/"
 
 
+def is_https(url: str) -> bool:
+    return url.startswith("https:")
+
+
+def mediate_https(url1: str, url2: str) -> tuple[str, str]:
+    if (is_https(url1) and is_https(url2)) or (not is_https(url1) and not is_https(url2)):
+        return url1, url2
+    elif not is_https(url1):
+        url1 = url1.replace("http:", "https:")
+    elif not is_https(url2):
+        url2 = url2.replace("http:", "https:")
+    return url1, url2
+
+
 def remove_parameters(url: str) -> str:
     return url if "?" not in url else url.split("?")[0]
---
+++ test/helper/url/is_https_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+from browserist import helper
+
+
+@pytest.mark.parametrize("url, expected", [
+    ("http://example.com", False),
+    ("https://example.com/", True),
+])
+def test_helper_url_is_https(url: str, expected: bool) -> None:
+    assert helper.url.is_https(url) == expected
