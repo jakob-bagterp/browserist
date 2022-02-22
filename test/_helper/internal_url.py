3654ceb62de87ebd9bfd781843fb7f2aec71e5a3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 17:17:52 +0100

    Update test of count elements method.

--- test/_helper/internal_url.py
+++ test/_helper/internal_url.py
@@ -3,3 +3,5 @@ from . import directory
 _mock_data_directory = directory.get_path_for_static_mock_data()
 
 EXAMPLE_COM: str = f"file://{_mock_data_directory}/example_com.html"
+
+W3SCHOOLS_COM: str = f"file://{_mock_data_directory}/w3schools_com.html"
