de2b14a4c7a9ff38d1eab9e74a18654b095ca976
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 21:23:01 +0100

    Update and fix test of check visibility.

--- test/browser/check_if/is_element_visible_test.py
+++ test/browser/check_if/is_element_visible_test.py
@@ -4,11 +4,16 @@ from _helper import internal_url
 from browserist import Browser
 
 
-@pytest.mark.parametrize("xpath, expected", [
-    ("/html/body/div/p[2]/a", True),
-    ("/html/body/div/h1/div", False)  # Element doesn't exist.
+@pytest.mark.parametrize("url, xpath, expected", [
+    (internal_url.EXAMPLE_COM, "/html/body/div/h1", True),  # Headline always visible.
+    (internal_url.EXAMPLE_COM, "/html/body/div/p[2]/a", True),  # Link always visible.
+    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[1]/div/h1", True),  # Headline always visible.
+    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/footer", True),  # Footer visible, though you have to scroll down.
+    # Headline visible, though you have to scroll down.
+    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[14]/h2", True),
+    (internal_url.W3SCHOOLS_COM, "//*[@id='google_translate_element']", False),  # Is "display: none" in inline styling.
 ])
-def test_check_if_is_element_visible(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
+def test_check_if_is_element_visible(url: str, xpath: str, expected: bool, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
-    browser.open.url(internal_url.EXAMPLE_COM)
+    browser.open.url(url)
     assert browser.check_if.is_element_visible(xpath) is expected
