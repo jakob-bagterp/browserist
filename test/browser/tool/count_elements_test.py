1413a25fe8a15faff00810b4e3ca058b7ffda6b6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-22 10:49:20 +0100

    Update open.url() to open.url_if_not_current() for tests so it doesn't have to re-open the page every time (part 2).

--- test/browser/tool/count_elements_test.py
+++ test/browser/tool/count_elements_test.py
@@ -5,5 +5,5 @@ from browserist import Browser
 
 def test_tool_count_elements(browser_default_headless: Browser) -> None:
     browser = browser_default_headless
-    browser.open.url(internal_url.EXAMPLE_COM)
+    browser.open.url_if_not_current(internal_url.EXAMPLE_COM)
     assert browser.tool.count_elements("/html/body/div/p") == 2
