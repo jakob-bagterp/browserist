2d4f10c889e008af3dd546f68a8a46517b0ac97c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-24 19:03:57 +0100

    Fix check if element is enabled or disabled by removing dependency on wait for element method.

--- src/browserist/browser/check_if/is_element_enabled.py
+++ src/browserist/browser/check_if/is_element_enabled.py
@@ -1,11 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-from ..wait.for_element import wait_for_element
-
 
 def check_if_is_element_enabled(driver: object, xpath: str) -> bool:
-    wait_for_element(driver, xpath)
     try:
         element = driver.find_element(By.XPATH, xpath)
         return element.is_enabled()
