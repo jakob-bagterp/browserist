6c2af44b14e8ba486c9c94eda841e36b14fb4a1b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-24 07:52:33 +0100

    Minor grammar correction.

--- test/browser/get/attribute/value_test.py
+++ test/browser/get/attribute/value_test.py
@@ -4,7 +4,7 @@ from _helper import internal_url
 from browserist import Browser
 
 
-@pytest.mark.parametrize("url, xpath, attribute, excepted", [
+@pytest.mark.parametrize("url, xpath, attribute, expected", [
     (internal_url.EXAMPLE_COM, "/html/body/div/p[2]/a", "href", "https://www.iana.org/domains/example"),
     (internal_url.EXAMPLE_COM, "/html/head/meta[3]", "name", "viewport"),
     (internal_url.W3SCHOOLS_COM, "//*[@id='bgcodeimg2']/div/img",
@@ -13,7 +13,7 @@ from browserist import Browser
      "Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, Python, PHP, Bootstrap, Java, XML and more."),
     (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[8]/div/div[30]", "class", "w3-col l6 s12 w3-center"),
 ])
-def test_get_attribute_value(url: str, xpath: str, attribute: str, excepted: str, browser_default_headless: Browser) -> None:
+def test_get_attribute_value(url: str, xpath: str, attribute: str, expected: str, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
     browser.open.url(url)
-    assert browser.get.attribute.value(xpath, attribute) == excepted
+    assert browser.get.attribute.value(xpath, attribute) == expected
