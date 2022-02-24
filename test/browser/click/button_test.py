317ac5c00faf0a14a393bb8302c2642f337ba3a6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-24 18:13:39 +0100

    Update test of click button method so it has multiple parameters to test.

--- test/browser/click/button_test.py
+++ test/browser/click/button_test.py
@@ -1,12 +1,18 @@
+import pytest
 from _helper import external_url
 from _helper import internal_url
 
 from browserist import Browser
 
 
-def test_click_button(browser_default_headless: Browser) -> None:
+@pytest.mark.parametrize("url, xpath, url_expected", [
+    (internal_url.EXAMPLE_COM, "/html/body/div/p[2]/a", external_url.IANA_ORG),
+    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[2]/div/div[1]/a[1]",
+     "https://www.w3schools.com/html/default.asp"),
+])
+def test_click_button(url: str, xpath: str, url_expected: str, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
-    browser.open.url(internal_url.EXAMPLE_COM)
-    browser.click.button("/html/body/div/p[2]/a")
-    browser.wait.until_url_contains(external_url.IANA_ORG)
-    assert browser.get.url.current() == external_url.IANA_ORG
+    browser.open.url(url)
+    browser.click.button(xpath)
+    browser.wait.until_url_contains(url_expected)
+    assert browser.get.url.current() == url_expected
