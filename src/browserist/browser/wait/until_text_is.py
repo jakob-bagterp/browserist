829e1a31b933cfa1ed8d52cb53dfcd9355ed30e0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-25 22:59:26 +0100

    Remove default ignore case from wait until text is method (users should rather use the "contains" variations).

--- src/browserist/browser/wait/until_text_is.py
+++ src/browserist/browser/wait/until_text_is.py
@@ -9,7 +9,7 @@ from .for_element import wait_for_element
 def wait_until_text_is(driver: object, xpath: str, regex: str, timeout: int = timeout.DEFAULT) -> None:
     def is_element_text(driver: object, xpath: str, regex: str) -> bool:
         text = get_text_from_element(driver, xpath, constant.timeout.BYPASS)
-        return bool(re.match(regex, text, re.IGNORECASE))
+        return bool(re.match(regex, text))
 
     wait_for_element(driver, xpath, timeout)
     helper.retry.until_condition_is_true(is_element_text(driver, xpath, regex), timeout)
