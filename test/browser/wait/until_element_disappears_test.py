9533c4a145f521163c8da430ebc334cee07708d4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-25 23:28:47 +0100

    Add test of wait until element disappears method.

---
+++ test/browser/wait/until_element_disappears_test.py
@@ -0,0 +1,20 @@
+from contextlib import nullcontext as does_not_raise
+from typing import Any
+
+import pytest
+from _helper import internal_url
+
+from browserist import Browser
+from browserist.constant import timeout
+from browserist.exception.retry import RetryTimeoutException
+
+
+@pytest.mark.parametrize("xpath, expectation", [
+    ("/html/body/div/p[2]/a", does_not_raise()),
+    ("/html/body/div/p[2]/a/div", pytest.raises(RetryTimeoutException)),
+])
+def test_wait_until_element_disappears(xpath: str, expectation: Any, browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
+    with expectation:
+        browser.open.url(internal_url.EXAMPLE_COM)
+        browser.wait.until_element_disappears(xpath, timeout.VERY_SHORT) is not None
