3b890ac4cd3f24bfe1c10c5ad902f516a6555ad3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-25 23:05:02 +0100

    Minor update of default values to constants.

--- src/browserist/browser/wait/random_time.py
+++ src/browserist/browser/wait/random_time.py
@@ -4,5 +4,5 @@ import time
 from ...constant import timeout
 
 
-def wait_random_time(min_seconds: int = 1, max_seconds: int = timeout.DEFAULT) -> None:
+def wait_random_time(min_seconds: int = timeout.VERY_SHORT, max_seconds: int = timeout.DEFAULT) -> None:
     time.sleep(random.uniform(min_seconds, max_seconds))
