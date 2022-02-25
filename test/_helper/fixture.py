32ee4cf9704372e8e24c27a3a5f010ada5728c8d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-25 09:34:25 +0100

    Refactor fixture into helper directory so it can be reused.

---
+++ test/_helper/fixture.py
@@ -0,0 +1,14 @@
+from typing import Generator
+
+import pytest
+from _config.browser_settings import default
+
+from browserist import Browser
+
+
+@pytest.fixture(scope="session")
+def browser_default_headless() -> Generator[Browser, None, None]:
+    """Reuse a shared Browser in default, headless mode across tests so each test doesn't have to initialize a new Browser, which is slower."""
+
+    with Browser(default.HEADLESS) as browser:
+        yield browser
