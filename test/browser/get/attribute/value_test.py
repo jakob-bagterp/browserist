57c9e6f53abb99d18f61d914e26b47d7ce56b45f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-27 18:21:35 +0100

    Fix issue in test of get attribute value.

--- test/browser/get/attribute/value_test.py
+++ test/browser/get/attribute/value_test.py
@@ -9,7 +9,7 @@ from browserist.helper.directory import update_path_format_if_windows
     (internal_url.EXAMPLE_COM, "/html/body/div/p[2]/a", "href", "https://www.iana.org/domains/example"),
     (internal_url.EXAMPLE_COM, "/html/head/meta[3]", "name", "viewport"),
     (internal_url.W3SCHOOLS_COM, "//*[@id='bgcodeimg2']/div/img", "src",
-     update_path_format_if_windows(f"update_path_format_if_windows({internal_url.W3SCHOOLS_COM_DIR}/how-spaces-works3.png")),
+     update_path_format_if_windows(f"{internal_url.W3SCHOOLS_COM_DIR}/how-spaces-works3.png")),
     (internal_url.W3SCHOOLS_COM, "/html/head/meta[4]", "content",
      "Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, Python, PHP, Bootstrap, Java, XML and more."),
     (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[8]/div/div[30]", "class", "w3-col l6 s12 w3-center"),
