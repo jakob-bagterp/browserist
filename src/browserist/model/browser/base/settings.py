1ebd28c9b0c7415430215e5b3d7c125af4e269df
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-27 07:20:42 +0100

    Refactor check if Windows into separate, shared helper function.

--- src/browserist/model/browser/base/settings.py
+++ src/browserist/model/browser/base/settings.py
@@ -1,7 +1,7 @@
-import sys
 from dataclasses import dataclass
 
 from ....constant import directory
+from ....helper import operating_system
 from .page_load_strategy import PageLoadStrategy
 from .type import BrowserType
 
@@ -14,7 +14,7 @@ class BrowserSettings:
 
     screenshot_dir: Destination directory for screenshot files. If not set, default directory is is from where the script is executed."""
 
-    type: BrowserType = BrowserType.EDGE if sys.platform.startswith("win32") else BrowserType.CHROME
+    type: BrowserType = BrowserType.EDGE if operating_system.is_windows() else BrowserType.CHROME
     headless: bool = False
     disable_images: bool = False
     page_load_strategy: PageLoadStrategy = PageLoadStrategy.NORMAL
