8f6526ed1f39861f680a242ad00f90d96334cd9b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-27 13:57:11 +0100

    Fix format for internal URLs so they're compatible with Windows path names.

--- src/browserist/helper/directory.py
+++ src/browserist/helper/directory.py
@@ -2,6 +2,7 @@ import os
 import re
 
 from ..constant import directory
+from .operating_system import is_windows
 
 
 def create_if_not_exists(dir_name: str) -> None:
@@ -20,3 +21,7 @@ def replace_path_slash_with_backslash(path: str) -> str:
 
     path = path.replace("/", "\\")  # Raw replace slash with backslash.
     return re.sub(r"^file:[\\]+", "file://", path)  # Handle exceptions for "file://" declaration in URLs.
+
+
+def update_path_format_if_windows(path: str) -> str:
+    return replace_path_slash_with_backslash(path) if is_windows() else path
