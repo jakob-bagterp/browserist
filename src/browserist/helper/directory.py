6dbcff46f665f8ff246569ec735127abc7719d29
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-27 13:42:03 +0100

    Add helper method to handle Windows file path names that should be, e.g., "file://path\to\file.html" instead of "file://path/to/file.html".

--- src/browserist/helper/directory.py
+++ src/browserist/helper/directory.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from ..constant import directory
 
@@ -12,3 +13,10 @@ def create_if_not_exists(dir_name: str) -> None:
 
 def ensure_trailing_slash(dir_name: str) -> str:
     return dir_name if dir_name[-1] == "/" else f"{dir_name}/"
+
+
+def replace_path_slash_with_backslash(path: str) -> str:
+    """Relevant for Windows where a file path name should be "file://path\\to\\file.html" instead of "file://path/to/file.html"."""
+
+    path = path.replace("/", "\\")  # Raw replace slash with backslash.
+    return re.sub(r"^file:[\\]+", "file://", path)  # Handle exceptions for "file://" declaration in URLs.
---
+++ test/helper/directory/replace_path_slash_with_backslash_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from browserist import helper
+
+
+@pytest.mark.parametrize("path, expected", [
+    ("file://path/to/file.html", "file://path\\to\\file.html"),
+    ("file:///path/to/file.html", "file://path\\to\\file.html"),
+    ("file://C:path/to/file.html", "file://C:path\\to\\file.html"),
+    ("file:///C:path/to/file.html", "file://C:path\\to\\file.html"),
+])
+def test_helper_replace_path_slash_with_backslash(path: str, expected: str) -> None:
+    assert helper.directory.replace_path_slash_with_backslash(path) == expected
