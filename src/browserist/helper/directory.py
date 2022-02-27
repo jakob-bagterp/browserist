91a846aba2ad673db1f1cdd5f286649ce6f3c371
jakob_bagterp <jakob_bagterp@hotmail.com>
2022-02-27 22:51:24 +0100

    Fix space and other encoding issues when encode file path as URL.

--- src/browserist/helper/directory.py
+++ src/browserist/helper/directory.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import urllib
 from ..constant import directory
 from .operating_system import is_windows
 
@@ -16,15 +16,17 @@ def ensure_trailing_slash(dir_name: str) -> str:
     return dir_name if dir_name[-1] == "/" else f"{dir_name}/"
 
 
-def ensure_path_format_encoding_as_url(path: str) -> str:
+def encode_path_as_url(path: str) -> str:
+    return urllib.parse.quote(path, safe="/:").replace("%2520", "%20")
+
+def ensure_windows_file_path_format_encoding_as_url(path: str) -> str:
     """Relevant for Windows where a file path name should be "file://path/to/file.html" instead of "file://path\\to\\file.html" ."""
 
     output = path.replace("\\", "/")  # Raw replace backslash with slash.
     # Handle exception for "file:///C:/path/to/file.html" declaration in URLs.
     if re.match(r"^file:/+[A-Za-z]:", output, re.IGNORECASE):
         output = re.sub(r"^file:/+", "file:///", output, re.IGNORECASE)
-    return output
-
+    return encode_path_as_url(output)
 
 def update_path_format_if_windows(path: str) -> str:
-    return ensure_path_format_encoding_as_url(path) if is_windows() else path
+    return ensure_windows_file_path_format_encoding_as_url(path) if is_windows() else encode_path_as_url(path)
--- test/helper/directory/ensure_windows_file_path_format_encoding_as_url_test.py
+++ test/helper/directory/ensure_windows_file_path_format_encoding_as_url_test.py
@@ -9,5 +9,5 @@ from browserist import helper
     ("file://C:path\\to\\file.html", "file:///C:path/to/file.html"),
     ("file:///C:path\\to\\file.html", "file:///C:path/to/file.html"),
 ])
-def test_helper_ensure_path_format_encoding_as_url(path: str, expected: str) -> None:
-    assert helper.directory.ensure_path_format_encoding_as_url(path) == expected
+def test_helper_directory_ensure_windows_file_path_format_encoding_as_url(path: str, expected: str) -> None:
+    assert helper.directory.ensure_windows_file_path_format_encoding_as_url(path) == expected
