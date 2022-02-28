ce9a382d144c94e06613c6f57b2a1794faad4616
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 10:26:33 +0100

    Add test of check if image is loaded method, including update of main method to handle None as False.

---
+++ test/browser/check_if/is_image_loaded_test.py
@@ -0,0 +1,15 @@
+import pytest
+from _helper import internal_url
+
+from browserist import Browser
+from browserist.constant import timeout
+
+
+@pytest.mark.parametrize("xpath, expected", [
+    ("/html/body/div[3]/a[1]/i", False),
+    ("//*[@id='bgcodeimg2']/div/img", True),
+])
+def test_check_if_is_image_loaded(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
+    browser.open.url(internal_url.W3SCHOOLS_COM)
+    assert browser.check_if.is_image_loaded(xpath, timeout.VERY_SHORT) is expected
