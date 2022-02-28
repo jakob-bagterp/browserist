ba426e27847200872076a45a1b03c255d29752b1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 10:31:06 +0100

    Add test of check if image element is loaded method.

---
+++ test/browser/check_if/is_image_element_loaded_test.py
@@ -0,0 +1,16 @@
+import pytest
+from _helper import internal_url
+from selenium.webdriver.common.by import By
+
+from browserist import Browser
+
+
+@pytest.mark.parametrize("xpath, expected", [
+    ("/html/body/div[3]/a[1]/i", False),
+    ("//*[@id='bgcodeimg2']/div/img", True),
+])
+def test_check_if_is_image_element_loaded(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
+    browser.open.url(internal_url.W3SCHOOLS_COM)
+    element = browser.driver.find_element(By.XPATH, xpath)
+    assert browser.check_if.is_image_element_loaded(element) is expected
