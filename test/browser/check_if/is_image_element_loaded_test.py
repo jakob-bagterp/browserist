706e5207f6f631cd2b9c480a7840642f0279b3e8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 11:52:30 +0100

    Add extra image elements to troubleshoot failing tests of check if is loaded methods.

--- test/browser/check_if/is_image_element_loaded_test.py
+++ test/browser/check_if/is_image_element_loaded_test.py
@@ -8,6 +8,8 @@ from browserist import Browser
 @pytest.mark.parametrize("xpath, expected", [
     ("/html/body/div[3]/a[1]/i", False),
     ("//*[@id='bgcodeimg2']/div/img", True),
+    ("//*[@id='Frontend']/img", True),
+    ("//*[@id='Backend']/img", True),
 ])
 def test_check_if_is_image_element_loaded(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
