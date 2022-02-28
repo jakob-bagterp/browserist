80dd549017f1691746e63f78b3943363170f6d66
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 19:29:45 +0100

    Add test of scroll to position method.

---
+++ test/browser/scroll/to_position_test.py
@@ -0,0 +1,17 @@
+import pytest
+from _helper import internal_url
+
+from browserist import Browser
+
+
+@pytest.mark.parametrize("x, y", [
+    (0, 0),
+    (0, 200),
+    (0, 100),
+])
+def test_get_scroll_position(x: int, y: int, browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
+    browser.open.url(internal_url.W3SCHOOLS_COM)
+    browser.scroll.to_position(x, y)
+    get_x, get_y = browser.scroll.get_position()
+    assert get_x == x and get_y == y
