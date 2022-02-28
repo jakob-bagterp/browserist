d62291fa8de20f258b923a0402eb0f6b483ba57a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 18:27:13 +0100

    Add test of input value method (and rename other get value test to avoid name conflicts).

---
+++ test/browser/input/value_2_test.py
@@ -0,0 +1,20 @@
+from contextlib import nullcontext as does_not_raise
+from typing import Any
+
+import pytest
+from _helper import internal_url
+
+from browserist import Browser
+from browserist.constant import timeout
+from browserist.exception.timeout import WaitForElementTimeoutException
+
+
+@pytest.mark.parametrize("url, xpath, expectation", [
+    (internal_url.W3SCHOOLS_COM, "//input[@id='search2']", does_not_raise()),
+    (internal_url.W3SCHOOLS_COM, "//input[@id='search2']/div", pytest.raises(WaitForElementTimeoutException)),
+])
+def test_input_value(url: str, xpath: str, expectation: Any, browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
+    with expectation:
+        browser.open.url(url)
+        browser.input.value(xpath, "some text", timeout.VERY_SHORT)
