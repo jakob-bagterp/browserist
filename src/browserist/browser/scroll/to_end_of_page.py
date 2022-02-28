c5fe649107e3fc4bd67c8740c60a72e95ba4e6e9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 19:01:19 +0100

    Fix minor issue: "DeprecationWarning: find_element_by_* commands are deprecated. Please use find_element() instead"

--- src/browserist/browser/scroll/to_end_of_page.py
+++ src/browserist/browser/scroll/to_end_of_page.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from ...constant import timeout
@@ -9,7 +10,7 @@ from ...exception.element import NoElementFoundException
 
 def scroll_to_end_of_page(driver: object):
     try:
-        body = driver.find_element_by_tag_name("body")  # Select the whole page before pressing any keys...
+        body = driver.find_element(By.TAG_NAME, "body")  # Select the whole page before pressing any keys...
         body.send_keys(Keys.END)  # ... and then simulate pressing End on the keyboard.
         time.sleep(timeout.VERY_SHORT)  # Small delay to ensure the view is updated.
     except NoSuchElementException:
