02a7cce6f301598f86852d8f5bcac14ff001155f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-28 18:17:07 +0100

    Add optional timeout arguments to input value and clear methods.

--- src/browserist/browser/input/__main__.py
+++ src/browserist/browser/input/__main__.py
@@ -1,3 +1,4 @@
+from ...constant import timeout
 from ...model.browser.base.driver import BrowserDriver
 from ...model.browser.base.settings import BrowserSettings
 from ...model.driver_methods import DriverMethods
@@ -9,12 +10,12 @@ class InputDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
 
-    def clear(self, xpath: str) -> None:
+    def clear(self, xpath: str, timeout: int = timeout.DEFAULT) -> None:
         """Clear input form field."""
 
-        input_clear(self._driver, xpath)
+        input_clear(self._driver, xpath, timeout)
 
-    def value(self, xpath: str, value: str) -> None:
+    def value(self, xpath: str, value: str, timeout: int = timeout.DEFAULT) -> None:
         """Input value into form field."""
 
-        input_value(self._driver, xpath, value)
+        input_value(self._driver, xpath, value, timeout)
--- src/browserist/browser/input/clear.py
+++ src/browserist/browser/input/clear.py
@@ -1,13 +1,14 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 
+from ...constant import timeout
 from ...exception.element import NoElementFoundException
 from ...exception.timeout import WaitForElementTimeoutException
 from ..wait.for_element import wait_for_element
 
 
-def input_clear(driver: object, xpath: str) -> None:
-    wait_for_element(driver, xpath)
+def input_clear(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
+    wait_for_element(driver, xpath, timeout)
     try:
         input_field = driver.find_element(By.XPATH, xpath)
         input_field.clear()
