affc0347713305c0926e7f9e0b1a35a1b690aa4a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-02 16:23:06 +0100

    Minor refactor so it's that the we're using a helper method.

--- test/browser/open/url_test.py
+++ test/browser/open/url_test.py
@@ -1,8 +1,7 @@
 import pytest
 from _helper import external_url, internal_url
 
-from browserist import Browser
-from browserist.helper import url
+from browserist import Browser, helper
 
 
 @pytest.mark.parametrize("url1, url2", [
@@ -11,11 +10,11 @@ from browserist.helper import url
     (external_url.EXAMPLE_COM, external_url.IANA_ORG),
 ])
 def test_open_url(url1: str, url2: str, browser_default_headless: Browser) -> None:
-    url1 = url.ensure_trailing_slash(url1)
-    url2 = url.ensure_trailing_slash(url2)
+    url1 = helper.url.ensure_trailing_slash(url1)
+    url2 = helper.url.ensure_trailing_slash(url2)
     browser = browser_default_headless
     browser.open.url(url1)
-    get_url1 = url.ensure_trailing_slash(browser.get.url.current())
+    get_url1 = helper.url.ensure_trailing_slash(browser.get.url.current())
     browser.open.url(url2)
-    get_url2 = url.ensure_trailing_slash(browser.get.url.current())
+    get_url2 = helper.url.ensure_trailing_slash(browser.get.url.current())
     assert get_url1 == url1 and get_url2 == url2 and get_url1 != get_url2
