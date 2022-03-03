fb072f300015c9a0d7da4b622357460ba07c9568
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-03 08:34:38 +0100

    Minor rename "expected_match" to "expected".

--- test/browser/open/url_if_not_current_test.py
+++ test/browser/open/url_if_not_current_test.py
@@ -20,19 +20,19 @@ def test_open_url_if_not_current(url1: str, url2: str, browser_default_headless:
     assert get_url1 == url1 and get_url2 == url2 and get_url1 != get_url2
 
 
-@pytest.mark.parametrize("url1, url2, ignore_trailing_slash, expected_match", [
+@pytest.mark.parametrize("url1, url2, ignore_trailing_slash, expected", [
     (internal_url.EXAMPLE_COM, internal_url.EXAMPLE_COM, True, True),
     (internal_url.EXAMPLE_COM, internal_url.EXAMPLE_COM, False, False),
     (internal_url.EXAMPLE_COM, f"{internal_url.EXAMPLE_COM}/", True, True),
     (internal_url.EXAMPLE_COM, f"{internal_url.EXAMPLE_COM}/", False, False),
 ])
-def test_open_url_if_not_current_ignore_trailing_slash(url1: str, url2: str, ignore_trailing_slash: bool, expected_match: bool, browser_default_headless: Browser) -> None:
+def test_open_url_if_not_current_ignore_trailing_slash(url1: str, url2: str, ignore_trailing_slash: bool, expected: bool, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
     browser.open.url_if_not_current(url1)
     get_url1 = browser.get.url.current()
     browser.open.url_if_not_current(url2, ignore_trailing_slash=ignore_trailing_slash)
     get_url2 = browser.get.url.current()
-    assert (get_url1 == get_url2) is expected_match
+    assert (get_url1 == get_url2) is expected
 
 # TODO: Test timing for opening external sites. Should not be run on GitHub Actions
 # TODO: Test ignore parameters.
