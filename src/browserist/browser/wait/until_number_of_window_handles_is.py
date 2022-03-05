3b7c98d1208638b3543f2648d4c039b8368c5c62
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-05 11:08:15 +0100

    Minor rename parameter.

--- src/browserist/browser/wait/until_number_of_window_handles_is.py
+++ src/browserist/browser/wait/until_number_of_window_handles_is.py
@@ -6,8 +6,8 @@ from ...constant import timeout
 from ...exception.timeout import WaitForWindowTimeoutException
 
 
-def wait_until_number_of_window_handles_is(driver: object, expected_tab: int, timeout: int = timeout.DEFAULT) -> None:
+def wait_until_number_of_window_handles_is(driver: object, expected_handles: int, timeout: int = timeout.DEFAULT) -> None:
     try:
-        WebDriverWait(driver, timeout).until(EC.number_of_windows_to_be(expected_tab))
+        WebDriverWait(driver, timeout).until(EC.number_of_windows_to_be(expected_handles))
     except TimeoutException:
         raise WaitForWindowTimeoutException() from TimeoutException
