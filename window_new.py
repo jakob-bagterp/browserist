42fa9e00282af7ab2f53f25112f5612b35ca6563
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-05 10:12:32 +0100

    Fix and update examples so it works with newly move methods.

--- window_new.py
+++ window_new.py
@@ -3,13 +3,17 @@ import time
 from src.browserist import Browser
 
 with Browser() as browser:
-    browser.open.new_tab()
+    browser.window.open.new_tab()
     time.sleep(2)
-    browser.open.new_tab("http://example.com")
+    browser.window.open.new_tab("http://example.com")
     time.sleep(2)
-    browser.open.new_window()
+    browser.window.open.new()
     time.sleep(2)
-    browser.open.new_window("http://example.com")
+    browser.window.open.new("http://example.com")
+    window_handles = browser.driver.window_handles
+    print(len(window_handles), window_handles)
     time.sleep(2)
     browser.window.close()
+    window_handles = browser.driver.window_handles
+    print(len(window_handles), window_handles)
     time.sleep(2)
