9475d445d5d8bc4d5e997fe25b2a06a9a213d42e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-06 18:32:03 +0100

    Fix switch to new tab so it also changes focus.

--- src/browserist/browser/window/open/new_tab.py
+++ src/browserist/browser/window/open/new_tab.py
@@ -1,12 +1,11 @@
+from ...open.url import open_url
 from ...wait.until_number_of_window_handles_is import wait_until_number_of_window_handles_is
 from ..handle.count import count_window_handles
 
 
 def open_new_tab(driver: object, url: str | None = None) -> None:
     current_number_of_window_handles = count_window_handles(driver)
-    if url:
-        # To ensure support across browsers, use JavaScript rather than CTRL + K keys combination.
-        driver.execute_script(f"window.open('{url}', '_blank');")
-    else:
-        driver.switch_to.new_window("tab")
+    driver.switch_to.new_window("tab")
     wait_until_number_of_window_handles_is(driver, current_number_of_window_handles + 1)
+    if url:
+        open_url(driver, url)
