66f50e3f3a3d17d6a9190385d053af4a27c949f3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-07 22:36:04 +0100

    Move website mock data to separate folder.

--- test/_helper/internal_url.py
+++ test/_helper/internal_url.py
@@ -2,13 +2,13 @@ from browserist.helper.directory import update_path_format_if_windows
 
 from . import directory
 
-_mock_data_directory = directory.get_path_for_static_mock_data()
+_web_mock_data_directory = directory.get_path_for_web_mock_data()
 
 EXAMPLE_COM: str = update_path_format_if_windows(
-    f"file://{_mock_data_directory}/example_com.html")
+    f"file://{_web_mock_data_directory}/example_com.html")
 
 W3SCHOOLS_COM: str = update_path_format_if_windows(
-    f"file://{_mock_data_directory}/w3schools_com.html")
+    f"file://{_web_mock_data_directory}/w3schools_com.html")
 
 W3SCHOOLS_COM_DIR: str = update_path_format_if_windows(
-    f"file://{_mock_data_directory}/w3schools_com_files")
+    f"file://{_web_mock_data_directory}/w3schools_com_files")
