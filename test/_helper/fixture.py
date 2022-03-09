0fb9e3c135f07796d0d7f2ef14dbda1890771a65
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-09 17:48:08 +0100

    Add test of controller add handle method.

--- test/_helper/fixture.py
+++ test/_helper/fixture.py
@@ -2,8 +2,12 @@ from typing import Generator
 
 import pytest
 from _config.browser_settings import default
+from _mock_data.window_handles import (WINDOW_HANDLE_1_ID, WINDOW_HANDLE_1_NAME, WINDOW_HANDLE_2_ID,
+                                       WINDOW_HANDLE_2_NAME, WINDOW_HANDLE_3_ID, WINDOW_HANDLE_3_NAME)
 
 from browserist import Browser
+from browserist.model.window.controller import WindowHandleController
+from browserist.model.window.handle import WindowHandle
 
 
 @pytest.fixture(scope="session")
@@ -20,3 +24,16 @@ def browser_default() -> Generator[Browser, None, None]:
 
     with Browser(default.DEFAULT) as browser:
         yield browser
+
+
+@pytest.fixture(scope="module")
+def window_handle_controller() -> WindowHandleController:
+    with Browser(default.HEADLESS) as browser:
+        window_handle_controller = WindowHandleController(browser.driver)
+        window_handle_controller._window_handles = [
+            WindowHandle(WINDOW_HANDLE_1_NAME, WINDOW_HANDLE_1_ID),
+            WindowHandle(WINDOW_HANDLE_2_NAME, WINDOW_HANDLE_2_ID),
+            WindowHandle(WINDOW_HANDLE_3_NAME, WINDOW_HANDLE_3_ID),
+        ]
+        window_handle_controller._counter = len(window_handle_controller._window_handles)
+        return window_handle_controller
