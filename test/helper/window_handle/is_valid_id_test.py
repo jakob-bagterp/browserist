e48d4477e85988f6c318e27ab8aa506bb4334abb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-09 12:06:08 +0100

    Refactor mock data of window handles so both the list and single values can be reused for various tests (part 2).

--- test/helper/window_handle/is_valid_id_test.py
+++ test/helper/window_handle/is_valid_id_test.py
@@ -1,11 +1,12 @@
 import pytest
+from _mock_data.window_handles import WINDOW_HANDLE_1_ID
 
 from browserist import helper
 
 
 @pytest.mark.parametrize("id, expected", [
     ("Not valid ID", False),
-    ("CDwindow-8088CB616D7499360039D98453AE91FC", True),
+    (WINDOW_HANDLE_1_ID, True),
 ])
 def test_helper_window_handle_is_valid_id(id: str, expected: bool) -> None:
     assert helper.window_handle.is_valid_id(id) is expected
--- test/helper/window_handle/name_already_exists_test.py
+++ test/helper/window_handle/name_already_exists_test.py
@@ -1,12 +1,12 @@
 import pytest
-from _mock_data.window_handles import WINDOW_HANDLES
+from _mock_data.window_handles import WINDOW_HANDLE_1_NAME, WINDOW_HANDLES
 
 from browserist import helper
 
 
 @pytest.mark.parametrize("name, expected", [
     ("Name does not exist", False),
-    (WINDOW_HANDLES[0].name, True),
+    (WINDOW_HANDLE_1_NAME, True),
 ])
 def test_helper_window_handle_name_already_exists(name: str, expected: bool) -> None:
     assert helper.window_handle.name_already_exists(name, WINDOW_HANDLES) is expected
