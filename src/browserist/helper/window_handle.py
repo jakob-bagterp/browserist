ecf75ca512ce1ef83162cbdf240c32177d569c45
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-09 17:53:32 +0100

    Minor change of private variable to appearance as constant.

--- src/browserist/helper/window_handle.py
+++ src/browserist/helper/window_handle.py
@@ -11,10 +11,10 @@ def id_already_exists(id: str, window_handles: list[WindowHandle]) -> bool:
     return id in [window_handle.id for window_handle in window_handles]
 
 
-_window_handle_id_pattern = re.compile(r"^CDwindow-[A-Z0-9]{32}$")
+WINDOW_HANDLE_ID_PATTERN = re.compile(r"^CDwindow-[A-Z0-9]{32}$")
 
 
 def is_valid_id(id: str) -> bool:
     """Checks if handle ID is similar to this pattern: CDwindow-8088CB616D7499360039D98453AE91FC"""
 
-    return bool(_window_handle_id_pattern.match(id))
+    return bool(WINDOW_HANDLE_ID_PATTERN.match(id))
