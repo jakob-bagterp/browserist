76b818db29b6e268c5cf2a87517765f4b34f7a3f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-09 23:45:26 +0100

    Simplify test of close window/tab method so it uses new fixture of default headless browser with scope per function.

--- test/browser/window/close_test.py
+++ test/browser/window/close_test.py
@@ -1,29 +1,27 @@
-from _config.browser_settings import default
-
 from browserist import Browser
 
 
-def test_close_window() -> None:
-    with Browser(default.HEADLESS) as browser:
-        number_of_window_handles_1 = browser.window.handle.count(selenium=True)
-        browser.window.open.new()
-        number_of_window_handles_2 = browser.window.handle.count(selenium=True)
-        browser.window.close()
-        number_of_window_handles_3 = browser.window.handle.count(selenium=True)
+def test_close_window(browser_default_headless_scope_function: Browser) -> None:
+    browser = browser_default_headless_scope_function
+    number_of_window_handles_1 = browser.window.handle.count(selenium=True)
+    browser.window.open.new()
+    number_of_window_handles_2 = browser.window.handle.count(selenium=True)
+    browser.window.close()
+    number_of_window_handles_3 = browser.window.handle.count(selenium=True)
 
-        assert number_of_window_handles_1 == 1
-        assert number_of_window_handles_2 == 2
-        assert number_of_window_handles_3 == 1
+    assert number_of_window_handles_1 == 1
+    assert number_of_window_handles_2 == 2
+    assert number_of_window_handles_3 == 1
 
 
-def test_close_tab() -> None:
-    with Browser(default.HEADLESS) as browser:
-        number_of_window_handles_1 = browser.window.handle.count(selenium=True)
-        browser.window.open.new_tab()
-        number_of_window_handles_2 = browser.window.handle.count(selenium=True)
-        browser.window.close()
-        number_of_window_handles_3 = browser.window.handle.count(selenium=True)
+def test_close_tab(browser_default_headless_scope_function: Browser) -> None:
+    browser = browser_default_headless_scope_function
+    number_of_window_handles_1 = browser.window.handle.count(selenium=True)
+    browser.window.open.new_tab()
+    number_of_window_handles_2 = browser.window.handle.count(selenium=True)
+    browser.window.close()
+    number_of_window_handles_3 = browser.window.handle.count(selenium=True)
 
-        assert number_of_window_handles_1 == 1
-        assert number_of_window_handles_2 == 2
-        assert number_of_window_handles_3 == 1
+    assert number_of_window_handles_1 == 1
+    assert number_of_window_handles_2 == 2
+    assert number_of_window_handles_3 == 1
