95c313fcb5ab6186e10a6a1dc3d7eea754252479
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-09 21:14:06 +0100

    Update docstrings.

--- src/browserist/browser/window/open/__main__.py
+++ src/browserist/browser/window/open/__main__.py
@@ -12,11 +12,15 @@ class WindowOpenDriverMethods(DriverMethods):
         self._controller = controller
 
     def new(self, url: str | None = None, name: str | None = None) -> None:
-        """Open and switch to new window. The URL and name of the window are optional arguments."""
+        """Open and switch to new window. The URL and name of the window are optional arguments.
+
+        name: Can be used to switch to this window with the "browser.window.switch_to(name)" method."""
 
         open_new_window(self._driver, self._controller, url, name)
 
     def new_tab(self, url: str | None = None, name: str | None = None) -> None:
-        """Open and switch to new tab in current window. The URL and name of the tab are optional arguments."""
+        """Open and switch to new tab in current window. The URL and name of the tab are optional arguments.
+
+        name: Can be used to switch to this tab with the "browser.window.switch_to(name)" method."""
 
         open_new_tab(self._driver, self._controller, url, name)
