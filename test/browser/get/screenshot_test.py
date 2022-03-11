da1f227c89dd3eca8264e14cb67b9b047e62280d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-11 21:27:04 +0100

    Minor update of helper function names.

--- test/browser/get/screenshot_test.py
+++ test/browser/get/screenshot_test.py
@@ -10,11 +10,11 @@ CUSTOM_SCREENSHOT_FILENAME = "screenshot.png"
 MINIMUM_FILE_SIZE = 1000
 
 
-def has_image_minimum_file_size(directory: str, file_name: str) -> bool:
+def image_has_minimum_file_size(directory: str, file_name: str) -> bool:
     return os.path.getsize(f"{directory}/{file_name}") > MINIMUM_FILE_SIZE
 
 
-def have_images_minimum_file_size(directory: str) -> bool:
+def images_have_minimum_file_size(directory: str) -> bool:
     files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
     return all(os.path.getsize(file) > MINIMUM_FILE_SIZE for file in files)
 
@@ -25,7 +25,7 @@ def test_get_screenshot_1(browser_headless_screenshot: Browser, tmpdir: local) -
     browser = browser_headless_screenshot
     browser.open.url(internal_url.EXAMPLE_COM)
     browser.get.screenshot()
-    assert have_images_minimum_file_size(str(tmpdir))
+    assert images_have_minimum_file_size(str(tmpdir))
 
 
 def test_get_screenshot_2(browser_headless_screenshot: Browser, tmpdir: local) -> None:
@@ -34,7 +34,7 @@ def test_get_screenshot_2(browser_headless_screenshot: Browser, tmpdir: local) -
     browser = browser_headless_screenshot
     browser.open.url(internal_url.EXAMPLE_COM)
     browser.get.screenshot(CUSTOM_SCREENSHOT_FILENAME)
-    assert have_images_minimum_file_size(str(tmpdir))
+    assert images_have_minimum_file_size(str(tmpdir))
 
 
 def test_get_screenshot_3(browser_default_headless: Browser, tmpdir: local) -> None:
@@ -44,4 +44,4 @@ def test_get_screenshot_3(browser_default_headless: Browser, tmpdir: local) -> N
     browser.open.url(internal_url.EXAMPLE_COM)
     temp_dir = str(tmpdir.mkdir(CUSTOM_SCREENSHOT_DIRECTORY))
     browser.get.screenshot(CUSTOM_SCREENSHOT_FILENAME, temp_dir)
-    assert has_image_minimum_file_size(temp_dir, CUSTOM_SCREENSHOT_FILENAME)
+    assert image_has_minimum_file_size(temp_dir, CUSTOM_SCREENSHOT_FILENAME)
