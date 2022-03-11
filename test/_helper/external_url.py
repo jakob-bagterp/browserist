8bb11fe7f4d742f124c3d0629e44fb3d74bbe788
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-11 22:09:21 +0100

    Minor correction of external links.

--- test/_helper/external_url.py
+++ test/_helper/external_url.py
@@ -1,7 +1,7 @@
 DBA_DK: str = "https://www.dba.dk/"
 
-EXAMPLE_COM: str = "http://example.com"
+EXAMPLE_COM: str = "http://example.com/"
 
 IANA_ORG: str = "https://www.iana.org/domains/reserved"
 
-W3SCHOOLS_COM: str = "https://www.w3schools.com"
+W3SCHOOLS_COM: str = "https://www.w3schools.com/"
