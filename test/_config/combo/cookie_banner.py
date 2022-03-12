f935eda5095a5ef4cc66c7d2d696f664331a5bcc
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-12 08:12:35 +0100

    Add extra parameters to cookie banner combo so we cover and test more code.

--- test/_config/combo/cookie_banner.py
+++ test/_config/combo/cookie_banner.py
@@ -1,10 +1,14 @@
 from _helper import external_url
 
 from browserist import CookieBannerSettings
+from browserist.constant import timeout
 
 DBA_ACCEPT_COOKIES = CookieBannerSettings(
     button_xpath="//button[@id='onetrust-accept-btn-handler']",
-    url=external_url.DBA_DK
+    url=external_url.DBA_DK,
+    has_disappeared_wait_seconds=timeout.VERY_SHORT,
+    has_loaded_wait_seconds=timeout.VERY_SHORT,
+    has_loaded_xpath="//button[@id='onetrust-accept-btn-handler']"
 )
 
 GOOGLE_ACCEPT_COOKIES = CookieBannerSettings(
