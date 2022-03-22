3669af87d30b410ea4b7d5aa4c72bfeefa205617
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-22 18:30:57 +0100

    Add missing return type.

--- src/browserist/browser/wait/until_images_have_loaded.py
+++ src/browserist/browser/wait/until_images_have_loaded.py
@@ -7,7 +7,7 @@ from .for_element import wait_for_element
 
 
 def wait_until_images_have_loaded(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
-    def are_all_images_loaded(driver: object, elements: list[object]):
+    def are_all_images_loaded(driver: object, elements: list[object]) -> bool:
         return all(check_if_is_image_element_loaded(driver, element) is not False for element in elements)
 
     wait_for_element(driver, xpath, timeout)
