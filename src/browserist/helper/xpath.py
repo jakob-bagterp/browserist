204980ebfafaef40b29d95cdba0a6efa0572451c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-25 14:46:06 +0100

    Add helper for XPath syntax validator based on LXML as this seems to have good recommendations (references: https://lxml.de/xpathxslt.html and https://stackoverflow.com/a/50152379/13115170)

---
+++ src/browserist/helper/xpath.py
@@ -0,0 +1,13 @@
+import lxml.etree
+from lxml.etree import XPathSyntaxError as LXML_XPathSyntaxError
+
+from ..exception.xpath import XPathSyntaxError
+
+
+def is_valid(xpath: str) -> bool:
+    try:
+        return bool(lxml.etree.XPath(xpath))
+    except LXML_XPathSyntaxError:
+        raise XPathSyntaxError(xpath) from LXML_XPathSyntaxError(xpath)
+    except Exception:
+        raise XPathSyntaxError(xpath) from Exception
