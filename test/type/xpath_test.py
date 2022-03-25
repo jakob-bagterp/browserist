1d36ae48c95493d828fa5660ca756abe4a46a3df
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-25 17:16:56 +0100

    First attempt for test of XPath type representation as string (needs a little more work).

---
+++ test/type/xpath_test.py
@@ -0,0 +1,10 @@
+from browserist.model.type.xpath import XPath
+
+
+def test_xpath_type_representation() -> None:
+    """Test that the XPath tiny type represents itself as a an XPath string."""
+
+    xpath_input = expected_xpath_output = "/valid/xpath"
+    xpath_type = XPath(xpath_input)
+    assert expected_xpath_output == xpath_type
+    assert expected_xpath_output == xpath_type.value
