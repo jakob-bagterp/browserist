29c94b57187817aec5e0e19f34e1e2d82f0a212c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-25 19:17:16 +0100

    Convert browser methods to new XPath tiny type (part 1).

--- src/browserist/browser/check_if/is_element_clickable.py
+++ src/browserist/browser/check_if/is_element_clickable.py
@@ -4,10 +4,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 
 from ...constant import timeout
+from ...model.type.xpath import XPath
 
 
 def check_if_is_element_clickable(driver: object, xpath: str) -> bool:
     try:
+        xpath = XPath(xpath)
         element: object = WebDriverWait(driver, timeout.VERY_SHORT).until(
             EC.element_to_be_clickable((By.XPATH, xpath)))  # type: ignore
         return element is not None
--- src/browserist/browser/check_if/is_element_enabled.py
+++ src/browserist/browser/check_if/is_element_enabled.py
@@ -1,9 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+from ...model.type.xpath import XPath
+
 
 def check_if_is_element_enabled(driver: object, xpath: str) -> bool:
     try:
+        xpath = XPath(xpath)
         element = driver.find_element(By.XPATH, xpath)  # type: ignore
         return element.is_enabled()  # type: ignore
     except (NoSuchElementException, Exception):
