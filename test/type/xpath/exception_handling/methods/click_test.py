c592abc4a635d28fe9724a83a398b88943f22d64
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 11:54:19 +0100

    Refactor and split tests of XPath exception handling in click methods into separate file.

---
+++ test/type/xpath/exception_handling/methods/click_test.py
@@ -0,0 +1,29 @@
+import pytest
+from _helper.xpath import (exception_handling_for_methods_with_2_arguments,
+                           exception_handling_for_methods_with_3_arguments_or_more)
+
+from browserist import Browser
+from browserist.browser.click.button import click_button
+from browserist.browser.click.button_if_contains_text import click_button_if_contains_text
+from browserist.model.type.callable import BrowserMethodWith2ArgumentsCallable, BrowserMethodWith3ArgumentsCallable
+
+
+@pytest.mark.parametrize("method", [
+    click_button,
+])
+def test_xpath_exception_handling_for_check_if_methods_1(
+    browser_default_headless: Browser,
+    method: BrowserMethodWith2ArgumentsCallable
+) -> None:
+    exception_handling_for_methods_with_2_arguments(browser_default_headless, method)
+
+
+@pytest.mark.parametrize("method, text", [
+    (click_button_if_contains_text, "More information..."),
+])
+def test_xpath_exception_handling_for_check_if_methods_2(
+    browser_default_headless: Browser,
+    method: BrowserMethodWith3ArgumentsCallable,
+    text: str
+) -> None:
+    exception_handling_for_methods_with_3_arguments_or_more(browser_default_headless, method, text)
--- test/type/xpath/exception_handling/methods_test.py
+++ test/type/xpath/exception_handling/methods_test.py
@@ -2,36 +2,18 @@ from typing import Any
 
 import pytest
 from _helper import internal_url
-from _helper.xpath import XPATH_TESTS, XPATH_TESTS_EXAMPLE_COM
+from _helper.xpath import XPATH_TESTS_EXAMPLE_COM
 
 from browserist import Browser
-from browserist.browser.click.button import click_button
-from browserist.browser.click.button_if_contains_text import click_button_if_contains_text
 from browserist.browser.get.attribute.value import get_attribute_value
 from browserist.browser.get.attribute.value_from_multiple_elements import get_attribute_value_from_multiple_elements
 from browserist.browser.get.text.from_element import get_text_from_element
 from browserist.browser.get.text.from_multiple_elements import get_text_from_multiple_elements
 from browserist.constant import timeout
-from browserist.model.type.callable import BrowserMethodWith2ArgumentsCallable, BrowserMethodWith3ArgumentsCallable
-
-
-@pytest.mark.parametrize("method", [
-    click_button,
-])
-def test_xpath_exception_handling_for_methods_with_2_arguments(
-    method: BrowserMethodWith2ArgumentsCallable,
-    browser_default_headless: Browser
-) -> None:
-
-    browser = browser_default_headless
-    browser.open.url(internal_url.EXAMPLE_COM)
-    for test in XPATH_TESTS:
-        with test.expactation:
-            _ = method(browser.driver, test.xpath) is not None
+from browserist.model.type.callable import BrowserMethodWith3ArgumentsCallable
 
 
 @pytest.mark.parametrize("method, arg3", [
-    (click_button_if_contains_text, "More information..."),
     (get_attribute_value, "href"),
     (get_attribute_value_from_multiple_elements, "href"),
     (get_text_from_element, timeout.VERY_SHORT),
