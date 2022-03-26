6e863c5dd78ca598993e97bc87604d2b88cba13b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 07:51:35 +0100

    If an input already is a validated XPath element, bypass and don't create a new object, including tests.

---
+++ test/type/xpath/bypass_test.py
@@ -0,0 +1,11 @@
+from _helper.xpath import VALID_XPATH
+
+from browserist.model.type.xpath import XPath
+
+
+def test_xpath_type_bypass_if_already_xpath() -> None:
+    """Test that if an input already is a validated XPath element, bypass and don't create a new object."""
+
+    xpath_type = XPath(VALID_XPATH)
+    assert xpath_type is XPath(xpath_type)
+    assert XPath(VALID_XPATH) is not XPath(VALID_XPATH)
