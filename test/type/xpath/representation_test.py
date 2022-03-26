da24f2fff19206c47dcc66096871b9ad2775e84b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 07:52:21 +0100

    Minor update of XPath representation test so it uses test constants.

--- test/type/xpath/representation_test.py
+++ test/type/xpath/representation_test.py
@@ -1,10 +1,12 @@
+from _helper.xpath import VALID_XPATH
+
 from browserist.model.type.xpath import XPath
 
 
 def test_xpath_type_representation() -> None:
     """Test that the XPath tiny type represents itself as a an XPath string."""
 
-    xpath_input = expected_xpath_output = "/valid/xpath"
+    xpath_input = expected_xpath_output = VALID_XPATH
     xpath_type = XPath(xpath_input)
     assert expected_xpath_output == xpath_type
     assert expected_xpath_output == xpath_type.value
