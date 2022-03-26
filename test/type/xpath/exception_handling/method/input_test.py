efb55656997fe95cd660226a4e8a493ee8710089
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 22:27:58 +0100

    Optimise timeout in test of input methods.

--- test/type/xpath/exception_handling/method/input_test.py
+++ test/type/xpath/exception_handling/method/input_test.py
@@ -9,26 +9,24 @@ from browserist.constant import timeout
 from browserist.model.type.callable import BrowserMethodWith3ArgumentsCallable, BrowserMethodWith4ArgumentsCallable
 
 
-@pytest.mark.parametrize("method, timeout", [
-    (input_clear, timeout.VERY_SHORT),
+@pytest.mark.parametrize("method", [
+    input_clear,
 ])
 def test_xpath_exception_handling_for_input_methods_1(
     browser_default_headless: Browser,
-    method: BrowserMethodWith3ArgumentsCallable,
-    timeout: int
+    method: BrowserMethodWith3ArgumentsCallable
 ) -> None:
     exception_handling_for_methods_with_3_arguments_or_more(
-        browser_default_headless, method, timeout, url=internal_url.W3SCHOOLS_COM, tests=XPATH_TESTS_W3SCHOOLS_COM_INPUT)
+        browser_default_headless, method, timeout.VERY_SHORT, url=internal_url.W3SCHOOLS_COM, tests=XPATH_TESTS_W3SCHOOLS_COM_INPUT)
 
 
-@pytest.mark.parametrize("method, value, timeout", [
-    (input_value, "search something", timeout.VERY_SHORT),
+@pytest.mark.parametrize("method, value", [
+    (input_value, "search something"),
 ])
 def test_xpath_exception_handling_for_input_methods_2(
     browser_default_headless: Browser,
     method: BrowserMethodWith4ArgumentsCallable,
-    value: str,
-    timeout: int
+    value: str
 ) -> None:
     exception_handling_for_methods_with_3_arguments_or_more(
-        browser_default_headless, method, value, timeout, url=internal_url.W3SCHOOLS_COM, tests=XPATH_TESTS_W3SCHOOLS_COM_INPUT)
+        browser_default_headless, method, value, timeout.VERY_SHORT, url=internal_url.W3SCHOOLS_COM, tests=XPATH_TESTS_W3SCHOOLS_COM_INPUT)
