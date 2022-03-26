67c5a00c2b4db8cc7d08ce34260f4040e9982232
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 22:12:24 +0100

    Optimise test of click methods so the timeout delay is short.

--- test/type/xpath/exception_handling/method/click_test.py
+++ test/type/xpath/exception_handling/method/click_test.py
@@ -1,29 +1,31 @@
 import pytest
-from _helper.xpath import (exception_handling_for_methods_with_2_arguments,
-                           exception_handling_for_methods_with_3_arguments_or_more)
+from _helper.xpath import exception_handling_for_methods_with_3_arguments_or_more
 
 from browserist import Browser
 from browserist.browser.click.button import click_button
 from browserist.browser.click.button_if_contains_text import click_button_if_contains_text
-from browserist.model.type.callable import BrowserMethodWith2ArgumentsCallable, BrowserMethodWith3ArgumentsCallable
+from browserist.constant import timeout
+from browserist.model.type.callable import BrowserMethodWith3ArgumentsCallable
 
 
-@pytest.mark.parametrize("method", [
-    click_button,
+@pytest.mark.parametrize("method, timeout", [
+    (click_button, timeout.VERY_SHORT),
 ])
 def test_xpath_exception_handling_for_click_methods_1(
     browser_default_headless: Browser,
-    method: BrowserMethodWith2ArgumentsCallable
+    method: BrowserMethodWith3ArgumentsCallable,
+    timeout: int
 ) -> None:
-    exception_handling_for_methods_with_2_arguments(browser_default_headless, method)
+    exception_handling_for_methods_with_3_arguments_or_more(browser_default_headless, method, timeout)
 
 
-@pytest.mark.parametrize("method, text", [
-    (click_button_if_contains_text, "More information..."),
+@pytest.mark.parametrize("method, text, timeout", [
+    (click_button_if_contains_text, "More information...", timeout.VERY_SHORT),
 ])
 def test_xpath_exception_handling_for_click_methods_2(
     browser_default_headless: Browser,
     method: BrowserMethodWith3ArgumentsCallable,
-    text: str
+    text: str,
+    timeout: int
 ) -> None:
-    exception_handling_for_methods_with_3_arguments_or_more(browser_default_headless, method, text)
+    exception_handling_for_methods_with_3_arguments_or_more(browser_default_headless, method, text, timeout)
