2e39524eb1df4773c6d593db8bcbef894fc22189
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-26 07:59:38 +0100

    Convert browser methods to new XPath tiny type (part 3).

--- test/type/xpath/exception_handling/methods_test.py
+++ test/type/xpath/exception_handling/methods_test.py
@@ -14,6 +14,8 @@ from browserist.browser.check_if.is_element_enabled import check_if_is_element_e
 from browserist.browser.check_if.is_image_loaded import check_if_is_image_loaded
 from browserist.browser.click.button import click_button
 from browserist.browser.click.button_if_contains_text import click_button_if_contains_text
+from browserist.browser.get.attribute.value import get_attribute_value
+from browserist.browser.get.attribute.value_from_multiple_elements import get_attribute_value_from_multiple_elements
 from browserist.model.type.callable import BrowserMethodWith2ArgumentsCallable, BrowserMethodWith3ArgumentsCallable
 
 
@@ -41,6 +43,8 @@ def test_xpath_exception_handling_for_methods_with_2_arguments(
 @pytest.mark.parametrize("method, arg3", [
     (check_if_element_contains_text, "More information..."),
     (click_button_if_contains_text, "More information..."),
+    (get_attribute_value, "href"),
+    (get_attribute_value_from_multiple_elements, "href"),
 ])
 def test_xpath_exception_handling_for_methods_with_3_arguments(
     method: BrowserMethodWith3ArgumentsCallable,
