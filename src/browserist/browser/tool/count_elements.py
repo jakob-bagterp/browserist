83b87745ffe77959643595b47f5cf46600951a1b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-27 09:22:40 +0200

    Convert browser methods to new XPath tiny type (part 12).

--- src/browserist/browser/tool/count_elements.py
+++ src/browserist/browser/tool/count_elements.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
 
 from ...constant import timeout
+from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
 
 def tool_count_elements(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> int:
+    xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
     elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
     return len(elements)
