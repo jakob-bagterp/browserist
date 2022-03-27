fa3597954bc4202ac5b6afb58cf4327ca8a95f34
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-27 09:42:46 +0200

    Convert browser methods to new XPath tiny type (part 13).

--- src/browserist/browser/wait/until_element_disappears.py
+++ src/browserist/browser/wait/until_element_disappears.py
@@ -1,7 +1,9 @@
 from ... import helper
 from ...constant import timeout
+from ...model.type.xpath import XPath
 from ..check_if.is_element_displayed import check_if_is_element_displayed
 
 
 def wait_until_element_disappears(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
+    xpath = XPath(xpath)
     helper.retry.until_condition_is_false(driver, xpath, func=check_if_is_element_displayed, timeout=timeout)
--- src/browserist/browser/wait/until_text_changes.py
+++ src/browserist/browser/wait/until_text_changes.py
@@ -1,5 +1,6 @@
 from ... import helper
 from ...constant import timeout
+from ...model.type.xpath import XPath
 from ..get.text.from_element import get_text_from_element
 from .for_element import wait_for_element
 
@@ -8,5 +9,6 @@ def wait_until_text_changes(driver: object, xpath: str, baseline_text: str, time
     def has_text_changed(driver: object, baseline_text: str) -> bool:
         return get_text_from_element(driver, xpath) != baseline_text
 
+    xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
     helper.retry.until_condition_is_true(driver, baseline_text, func=has_text_changed, timeout=timeout)
