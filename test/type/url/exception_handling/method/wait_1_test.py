889ba0666bc2b2e34422b7ec0b1b38e10ed65195
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-28 21:52:26 +0200

    Fix wait for URL tests of tiny type.

--- test/type/url/exception_handling/method/wait_1_test.py
+++ test/type/url/exception_handling/method/wait_1_test.py
@@ -1,19 +1,20 @@
-import pytest
+from _helper.external_url import GOOGLE_COM
 from _helper.url.method_1 import exception_handling_for_methods_with_3_arguments_or_more
+from _helper.url.test_set_1 import VALID_URL
 
 from browserist import Browser
 from browserist.browser.wait.until_url_changes import wait_until_url_changes
 from browserist.browser.wait.until_url_is import wait_until_url_is
 from browserist.constant import timeout
-from browserist.model.type.callable import BrowserMethodWith3ArgumentsCallable
 
 
-@pytest.mark.parametrize("method", [
-    wait_until_url_changes,
-    wait_until_url_is,
-])
-def test_xpath_exception_handling_for_wait_for_url_methods(
-    browser_default_headless: Browser,
-    method: BrowserMethodWith3ArgumentsCallable
-) -> None:
-    exception_handling_for_methods_with_3_arguments_or_more(browser_default_headless, method, timeout.VERY_SHORT)
+def test_xpath_exception_handling_for_wait_for_url_changes_methods(browser_default_headless: Browser) -> None:
+    browser_default_headless.open.url(GOOGLE_COM)
+    exception_handling_for_methods_with_3_arguments_or_more(
+        browser_default_headless, wait_until_url_changes, timeout.VERY_SHORT)
+
+
+def test_xpath_exception_handling_for_wait_for_url_is_method(browser_default_headless: Browser) -> None:
+    browser_default_headless.open.url(VALID_URL)
+    exception_handling_for_methods_with_3_arguments_or_more(
+        browser_default_headless, wait_until_url_is, timeout.VERY_SHORT)
