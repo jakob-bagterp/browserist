b681c1d2018fd48f8a83d988edbce3a6d540689e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-28 17:32:48 +0200

    Add test of URL type's representation as string (based on similar test of XPath tiny type).

---
+++ test/type/url/representation_1_test.py
@@ -0,0 +1,12 @@
+from _helper.url.test_set_1 import VALID_URL
+
+from browserist.model.type.url import URL
+
+
+def test_url_type_representation() -> None:
+    """Test that the XPath tiny type represents itself as a an URL string."""
+
+    url_input = expected_url_output = VALID_URL
+    url_type = URL(url_input)
+    assert expected_url_output == url_type
+    assert expected_url_output == url_type.value
