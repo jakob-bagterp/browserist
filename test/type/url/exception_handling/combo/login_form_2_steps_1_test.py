03a4c247014e26768c1edd6c887f3bb1744740b8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-04-01 09:58:01 +0200

    Minor refactor of test variables for URL in login form.

--- test/type/url/exception_handling/combo/login_form_2_steps_1_test.py
+++ test/type/url/exception_handling/combo/login_form_2_steps_1_test.py
@@ -10,19 +10,19 @@ from browserist.exception.url import URLSyntaxError
 from browserist.model.combo_settings.login_form import LoginForm2Steps
 
 
-@pytest.mark.parametrize("url1, expectation", [
+@pytest.mark.parametrize("url, expectation", [
     (VALID_URL, does_not_raise()),
     (INVALID_URL, pytest.raises(URLSyntaxError)),
 ])
-def test_url_exception_handling_of_login_form_2_steps(url1: str, expectation: Any) -> None:
+def test_url_exception_handling_of_login_form_2_steps(url: str, expectation: Any) -> None:
     with expectation:
         _ = LoginForm2Steps(
             username_input_xpath=VALID_XPATH,
             username_submit_button_xpath=VALID_XPATH,
             password_input_xpath=VALID_XPATH,
             password_submit_button_xpath=VALID_XPATH,
-            url=url1,
+            url=url,
             post_login_wait_seconds=timeout.VERY_SHORT,
-            post_login_url_contains=url1,
+            post_login_url_contains=VALID_URL,
             post_login_element_xpath=VALID_XPATH
         ) is not None
