be298cec09f178cf0d9d6554eab4c3fcb90137a8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-04-04 18:43:20 +0200

    Simplify exception handling for search combo since it's preferable for debugging to crash eventual issues when they happen and not as a group.

--- src/browserist/exception/search.py
+++
@@ -1,7 +0,0 @@
-class SearchException(Exception):
-    def __init__(self, term: str) -> None:
-        self.message = f"Something went wrong when trying to search for \"{term}\". Please try again..."
-        super().__init__(self.message)
-
-    def __str__(self) -> str:
-        return self.message
