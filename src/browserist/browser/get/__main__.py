9c129ac4a3e133ac64dd93479e0ce4fd3c761ae4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-04 23:18:06 +0200

    Add get multiple elements by XPath method.

--- src/browserist/browser/get/__main__.py
+++ src/browserist/browser/get/__main__.py
@@ -3,6 +3,7 @@ from ...model.browser.base.driver import BrowserDriver
 from ...model.browser.base.settings import BrowserSettings
 from ...model.driver_methods import DriverMethods
 from .all_elements_by_tag import get_all_elements_by_tag
+from .all_elements import get_all_elements
 from .attribute.__main__ import GetAttributeDriverMethods
 from .dimensions_of_element import get_dimensions_of_element
 from .element import get_element
@@ -23,6 +24,11 @@ class GetDriverMethods(DriverMethods):
         """"Get all elements by HTML tag. Examples: "img" as tag for all <img> images, "a" for all <a> links, etc."""
 
         return get_all_elements_by_tag(self._driver, tag, timeout)
+    
+    def all_elements(self, xpath: str, timeout: int = timeout.DEFAULT) -> list[object]:
+        """Get all elements by XPath."""
+
+        return get_all_elements(self._driver, xpath, timeout)
 
     def dimensions_of_element(self, xpath: str, timeout: int = timeout.DEFAULT) -> tuple[int, int]:
         """Get width and height of element in pixels. Usage:
