93431bbdf5fccdcfb503c37e96e04dee61c62d4f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-08 00:35:56 +0200

    Rename get attribute values method, including test.

--- src/browserist/browser/get/attribute/values.py
+++ src/browserist/browser/get/attribute/values.py
@@ -5,7 +5,7 @@ from ....model.type.xpath import XPath
 from ...wait.for_element import wait_for_element
 
 
-def get_attribute_value_from_multiple_elements(driver: object, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> list[str]:
+def get_attribute_values(driver: object, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> list[str]:
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
     elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
--- src/browserist/browser/get/url/from_multiple_links.py
+++ src/browserist/browser/get/url/from_multiple_links.py
@@ -1,8 +1,8 @@
 from ....constant import timeout
 from ....model.type.xpath import XPath
-from ..attribute.value_from_multiple_elements import get_attribute_value_from_multiple_elements
+from ..attribute.values import get_attribute_values
 
 
 def get_url_from_multiple_links(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
     xpath = XPath(xpath)
-    return get_attribute_value_from_multiple_elements(driver, xpath, "href", timeout)
+    return get_attribute_values(driver, xpath, "href", timeout)
