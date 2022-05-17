dac99201e3cb75683fc208e9f8fff1ec8f60e45c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-17 23:31:31 +0200

    Refactor check if image element is loaded into helper function, including updated test.

--- src/browserist/browser/wait/until_images_have_loaded.py
+++ src/browserist/browser/wait/until_images_have_loaded.py
@@ -3,13 +3,12 @@ from selenium.webdriver.common.by import By
 from ... import helper
 from ...constant import timeout
 from ...model.type.xpath import XPath
-from ..check_if.is_image_element_loaded import check_if_is_image_element_loaded
 from .for_element import wait_for_element
 
 
 def wait_until_images_have_loaded(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
     def are_all_images_loaded(driver: object, elements: list[object]) -> bool:
-        return all(check_if_is_image_element_loaded(driver, element) is not False for element in elements)
+        return all(helper.image.is_element_loaded(driver, element) is not False for element in elements)
 
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
--- src/browserist/helper/image.py
+++ src/browserist/helper/image.py
@@ -1,4 +1,6 @@
-def check_if_is_image_element_loaded(driver: object, element: object) -> bool:
+def is_element_loaded(driver: object, element: object) -> bool:
+    """Check if image element is loaded and ready in the DOM."""
+
     is_image_loaded: bool = driver.execute_script(  # type: ignore
         "return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0;",
         element)
