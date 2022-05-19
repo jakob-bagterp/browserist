4b4b83f3a03ecb05e469700db45f75a473b0680f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-19 18:50:33 +0200

    Rename get URL from multiple images method, including test.

--- src/browserist/browser/get/url/__main__.py
+++ src/browserist/browser/get/url/__main__.py
@@ -4,8 +4,8 @@ from ....model.browser.base.settings import BrowserSettings
 from ....model.driver_methods import DriverMethods
 from .current import get_current_url
 from .from_image import get_url_from_image
+from .from_images import get_url_from_images
 from .from_link import get_url_from_link
-from .from_multiple_images import get_url_from_multiple_images
 from .from_multiple_links import get_url_from_multiple_links
 
 
@@ -25,17 +25,17 @@ class GetUrlDriverMethods(DriverMethods):
 
         return get_url_from_image(self._driver, xpath, timeout)
 
+    def from_images(self, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
+        """Get array of URLs from images, e.g. <img> tags. Assumes that the XPath targets multiple images."""
+
+        return get_url_from_images(self._driver, xpath, timeout)
+
     def from_link(self, xpath: str, timeout: int = timeout.DEFAULT) -> str:
         """Get URL from link, e.g. <a> tag or button.
 
         This method assumes that the link shouldn't be empty and therefore will retry to get the URL (for better support of single-page apps with extended loading time)."""
 
         return get_url_from_link(self._driver, xpath, timeout)
-
-    def from_multiple_images(self, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
-        """Get array of URLs from images, e.g. <img> tags. Assumes that the XPath targets multiple images."""
-
-        return get_url_from_multiple_images(self._driver, xpath, timeout)
 
     def from_multiple_links(self, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
         """Get array of URLs from links, e.g. <a> tags or buttons. Assumes that the XPath targets multiple links."""
--- src/browserist/browser/get/url/from_images.py
+++ src/browserist/browser/get/url/from_images.py
@@ -3,6 +3,6 @@ from ....model.type.xpath import XPath
 from ..attribute.values import get_attribute_values
 
 
-def get_url_from_multiple_images(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
+def get_url_from_images(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> list[str]:
     xpath = XPath(xpath)
     return get_attribute_values(driver, xpath, "src", timeout)
