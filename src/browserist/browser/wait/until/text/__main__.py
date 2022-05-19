001539b864143dbee99873124cd0090abfa2de76
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-20 00:21:23 +0200

    Add basic driver method.

---
+++ src/browserist/browser/wait/until/text/__main__.py
@@ -0,0 +1,8 @@
+from .....model.browser.base.driver import BrowserDriver
+from .....model.browser.base.settings import BrowserSettings
+from .....model.driver_methods import DriverMethods
+
+
+class WaitUntilTextDriverMethods(DriverMethods):
+    def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
+        super().__init__(browser_driver, settings)
