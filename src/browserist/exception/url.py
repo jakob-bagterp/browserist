eae58ead5a60ffeb505c4f08ae719168034f7cc0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-05-22 15:28:12 +0200

    Refactor and add slots for exceptions.

--- src/browserist/exception/url.py
+++ src/browserist/exception/url.py
@@ -1,4 +1,6 @@
 class URLSyntaxError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, url: str) -> None:
         self.message = f"Invalid URL syntax: {url}"
         super().__init__(self.message)
--- src/browserist/exception/window_handle.py
+++ src/browserist/exception/window_handle.py
@@ -1,4 +1,6 @@
 class WindowHandleIdNotFoundError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, id: str) -> None:
         self.message = f"Window handle ID not found or doesn't exist: {id}"
         super().__init__(self.message)
@@ -8,6 +10,8 @@ class WindowHandleIdNotFoundError(Exception):
 
 
 class WindowHandleNameNotFoundError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, name: str) -> None:
         self.message = f"Window handle name not found or doesn't exist: {name}"
         super().__init__(self.message)
@@ -17,6 +21,8 @@ class WindowHandleNameNotFoundError(Exception):
 
 
 class WindowHandleIdNotUniqueError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, id: str) -> None:
         self.message = f"Window handle ID already exists: {id}"
         super().__init__(self.message)
@@ -26,6 +32,8 @@ class WindowHandleIdNotUniqueError(Exception):
 
 
 class WindowHandleNameNotUniqueError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, name: str) -> None:
         self.message = f"Window handle name already exists: {name}"
         super().__init__(self.message)
@@ -35,6 +43,8 @@ class WindowHandleNameNotUniqueError(Exception):
 
 
 class WindowHandleIdNotValidError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, id: str) -> None:
         self.message = f"Window handle ID has invalid format: {id}"
         super().__init__(self.message)
@@ -44,6 +54,8 @@ class WindowHandleIdNotValidError(Exception):
 
 
 class WindowHandleNameNotValidError(Exception):
+    __slots__ = ["message"]
+
     def __init__(self, name: str) -> None:
         self.message = f"Window handle name is invalid. Try using another name than this: {name}"
         super().__init__(self.message)
