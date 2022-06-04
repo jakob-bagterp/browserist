9338159536b4cd59b9d5a5b44d54f6cd36c63f29
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-04 10:39:25 +0200

    Attempt to fix MyPy issue: "error: Unused "type: ignore" comment"

--- src/browserist/model/browser/chrome.py
+++ src/browserist/model/browser/chrome.py
@@ -11,10 +11,10 @@ class ChromeBrowserDriver(BrowserDriver):
 
     def set_webdriver(self) -> object:
         if self.settings.path_to_executable is None:
-            return webdriver.Chrome(  # type: ignore
+            return webdriver.Chrome(
                 options=self.chrome_options)
         else:
-            return webdriver.Chrome(  # type: ignore
+            return webdriver.Chrome(
                 executable_path=self.settings.path_to_executable,
                 options=self.chrome_options)
 
