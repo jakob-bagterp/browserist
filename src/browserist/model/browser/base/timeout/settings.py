bc1bc2330adb7eb76a5a937d2908832099ae4638
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-10 22:24:01 +0200

    Add docstring to timeout settings class.

--- src/browserist/model/browser/base/timeout/settings.py
+++ src/browserist/model/browser/base/timeout/settings.py
@@ -6,6 +6,12 @@ from .strategy import TimeoutStrategy
 
 @dataclass(slots=True)
 class TimeoutSettings:
+    """Class to configure timeout settings and strategy.
+
+    strategy: If a function times out, should the browser continue or stop? Default is stop.
+
+    seconds: General timeout in seconds to be applied for each function (unless function-specific timeout is given)."""
+
     strategy: TimeoutStrategy = TimeoutStrategy.STOP
     seconds: int = timeout.DEFAULT
     _is_timed_out: bool = False
