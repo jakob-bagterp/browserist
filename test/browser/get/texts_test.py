42ade53a7c2bc2c2acc0ede6537079963b118f5d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-11 11:35:24 +0100

    Rename "expected" to more readable term in tests (part 3).

--- test/browser/get/texts_test.py
+++ test/browser/get/texts_test.py
@@ -4,7 +4,7 @@ from _mock_data.url import internal_url
 from browserist import Browser
 
 
-@pytest.mark.parametrize("url, xpath, expected", [
+@pytest.mark.parametrize("url, xpath, expected_texts", [
     (internal_url.EXAMPLE_COM, "/html/body/div/p[2]/a", ["More information..."]),
     (internal_url.EXAMPLE_COM, "/html/body/div/p",
      ["This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.", "More information..."]),
@@ -12,7 +12,7 @@ from browserist import Browser
     (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[2]/div/div[1]/a",
      ["Learn HTML", "Video Tutorial\nNEW", "HTML Reference"]),
 ])
-def test_get_texts(url: str, xpath: str, expected: str, browser_default_headless: Browser) -> None:
+def test_get_texts(url: str, xpath: str, expected_texts: str, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
     browser.open.url(url)
-    assert browser.get.texts(xpath) == expected
+    assert browser.get.texts(xpath) == expected_texts
