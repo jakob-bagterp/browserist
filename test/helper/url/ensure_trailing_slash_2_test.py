5d8b0ce66e45be120fff6b9731e07de9f6dbca21
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-11 11:44:50 +0100

    Rename "expected" to more readable term in tests (part 5).

--- test/helper/url/ensure_trailing_slash_2_test.py
+++ test/helper/url/ensure_trailing_slash_2_test.py
@@ -3,11 +3,11 @@ import pytest
 from browserist import helper
 
 
-@pytest.mark.parametrize("url, expected", [
+@pytest.mark.parametrize("url, expected_url", [
     ("http://example.com", "http://example.com/"),
     ("http://example.com/", "http://example.com/"),
     ("http://example.com?page=1", "http://example.com?page=1"),
     ("http://example.com/?page=1", "http://example.com/?page=1"),
 ])
-def test_helper_url_ensure_trailing_slash(url: str, expected: str) -> None:
-    assert helper.url.ensure_trailing_slash(url) == expected
+def test_helper_url_ensure_trailing_slash(url: str, expected_url: str) -> None:
+    assert helper.url.ensure_trailing_slash(url) == expected_url
