1637283cbba39cd4da648324d0bccac163b3f2f0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-11 09:45:13 +0100

    Attempt to troubleshoot and fix issue in test of get current URL that suddenly fails for "http://example.com", but not "http://example.com/".

--- test/browser/get/url/current_1_test.py
+++ test/browser/get/url/current_1_test.py
@@ -4,8 +4,8 @@ from browserist import Browser
 
 
 @pytest.mark.parametrize("url, expected", [
-    ("http://example.com", "http://example.com/"),
     ("http://example.com/", "http://example.com/"),
+    ("http://example.com", "http://example.com/"),
 ])
 def test_get_current_url(url: str, expected: str, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
