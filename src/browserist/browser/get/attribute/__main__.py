32c553fe0148a18ece6476fbd07d48b43cb959d1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-13 19:14:55 +0100

    Refactor get_attribute_values so defaults and timeout are handled are handled from the relevant driver method.

--- src/browserist/browser/get/attribute/__main__.py
+++ src/browserist/browser/get/attribute/__main__.py
@@ -1,4 +1,3 @@
-from ....constant import timeout
 from ....model.browser.base.driver import BrowserDriver
 from ....model.browser.base.settings import BrowserSettings
 from ....model.driver_methods import DriverMethods
@@ -22,11 +21,14 @@ class GetAttributeDriverMethods(DriverMethods):
             return get_attribute_value(self._driver, xpath, attribute, timeout)
         return None
 
-    def values(self, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> list[str]:
+    def values(self, xpath: str, attribute: str, timeout: int | None = None) -> list[str] | None:
         """Get values from an attribute of multiple elements. Assumes that the XPath targets multiple links. Examples:
 
         Use "src" as attribute to get the source URL from an <img> image tag.
 
         Use "href" as attribute to get the URL from an <a> link tag."""
 
-        return get_attribute_values(self._driver, xpath, attribute, timeout)
+        if self._timeout_should_continue():
+            timeout = self._mediate_timeout(timeout)
+            return get_attribute_values(self._driver, xpath, attribute, timeout)
+        return None
--- src/browserist/browser/get/attribute/values.py
+++ src/browserist/browser/get/attribute/values.py
@@ -1,11 +1,10 @@
 from selenium.webdriver.common.by import By
 
-from ....constant import timeout
 from ....model.type.xpath import XPath
 from ...wait.for_element import wait_for_element
 
 
-def get_attribute_values(driver: object, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> list[str]:
+def get_attribute_values(driver: object, xpath: str, attribute: str, timeout: int) -> list[str]:
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
     elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
