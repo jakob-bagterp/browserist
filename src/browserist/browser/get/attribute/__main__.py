94e3dc45d0b68a89eebf2869ab1d44eaf2d4ab3c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-13 19:13:23 +0100

    Refactor get_attribute_value so defaults and timeout are handled are handled from the relevant driver method.

--- src/browserist/browser/get/attribute/__main__.py
+++ src/browserist/browser/get/attribute/__main__.py
@@ -10,14 +10,17 @@ class GetAttributeDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
 
-    def value(self, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> str:
+    def value(self, xpath: str, attribute: str, timeout: int | None = None) -> str | None:
         """Get value from an attribute of an element. Examples:
 
         Use "src" as attribute to get the source URL from an <img> image tag.
 
         Use "href" as attribute to get the URL from an <a> link tag."""
 
-        return get_attribute_value(self._driver, xpath, attribute, timeout)
+        if self._timeout_should_continue():
+            timeout = self._mediate_timeout(timeout)
+            return get_attribute_value(self._driver, xpath, attribute, timeout)
+        return None
 
     def values(self, xpath: str, attribute: str, timeout: int = timeout.DEFAULT) -> list[str]:
         """Get values from an attribute of multiple elements. Assumes that the XPath targets multiple links. Examples:
