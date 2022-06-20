c0a139ee551ff16645538a55b3b8946c90505093
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-21 00:11:12 +0200

    Refactor wait_until_text_changes so defaults and timeout are handled are handled from the relevant driver method.

--- src/browserist/browser/wait/until/text/__main__.py
+++ src/browserist/browser/wait/until/text/__main__.py
@@ -11,10 +11,12 @@ class WaitUntilTextDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
 
-    def changes(self, xpath: str, baseline_text: str, timeout: int = timeout.DEFAULT) -> None:
+    def changes(self, xpath: str, baseline_text: str, timeout: int | None = None) -> None:
         """Wait until the text of an element changes from a baseline text, e.g. after a form action. The text is evaluated as an exact match."""
 
-        wait_until_text_changes(self._driver, xpath, baseline_text, timeout)
+        if self._timeout_should_continue():
+            timeout = self._mediate_timeout(timeout)
+            wait_until_text_changes(self._driver, xpath, baseline_text, timeout)
 
     def contains(self, xpath: str, regex: str, timeout: int = timeout.DEFAULT) -> None:
         """Wait until the text of an element has changed, e.g. after a form action."""
