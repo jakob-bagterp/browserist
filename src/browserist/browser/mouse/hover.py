77f613508034cc45f05f1334d10c8aec4e8a59f5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-20 19:46:51 +0200

    Refactor mouse_hover so defaults and timeout are handled are handled from the relevant driver method.

--- src/browserist/browser/mouse/hover.py
+++ src/browserist/browser/mouse/hover.py
@@ -1,12 +1,11 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
-from ...constant import timeout
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
 
-def mouse_hover(driver: object, xpath: str, timeout: int = timeout.DEFAULT) -> None:
+def mouse_hover(driver: object, xpath: str, timeout: int) -> None:
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
     element = driver.find_element(By.XPATH, xpath)  # type: ignore
