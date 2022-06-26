7b265b50b5e5769087159c2e2f6c3d3c3ba7a865
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-26 23:10:57 +0200

    Add unfinished boilerplate for get screenshot of complete page method.

--- src/browserist/browser/get/screenshot/__main__.py
+++ src/browserist/browser/get/screenshot/__main__.py
@@ -1,12 +1,26 @@
 from ....model.browser.base.driver import BrowserDriver
 from ....model.browser.base.settings import BrowserSettings
 from ....model.driver_methods import DriverMethods
+from .complete_page import get_screenshot_complete_page
 from .visible_portion import get_screenshot_visible_portion
 
 
 class GetScreenshotDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
+
+    def complete_page(self, file_name: str | None = None, destination_dir: str | None = None) -> None:
+        """Take screenshot of complete page and save as PNG image. Default destination directory is from where the script is executed. Examples:
+
+        browser.get.screenshot.complete_page() # Default file name and destination
+
+        browser.get.screenshot.complete_page("image.png") # Custom file name and default destination
+
+        browser.get.screenshot.complete_page("image.png", "./screenshots") # Custom file name and destination
+
+        browser.get.screenshot.complete_page(destination_dir = "./screenshots") # Default file name and custom destination"""
+
+        get_screenshot_complete_page(self._driver, self._settings, file_name, destination_dir)
 
     def visible_portion(self, file_name: str | None = None, destination_dir: str | None = None) -> None:
         """Take screenshot and save as PNG image. Default destination directory is from where the script is executed. Examples:
---
+++ src/browserist/browser/get/screenshot/complete_page.py
@@ -0,0 +1,5 @@
+from ....model.browser.base.settings import BrowserSettings
+
+
+def get_screenshot_complete_page(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
+    pass
