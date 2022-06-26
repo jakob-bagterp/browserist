757189f30c25e8a8ea80ee32820e7d26898468d3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-26 23:11:43 +0200

    Minor correction of docstring.

--- src/browserist/browser/get/screenshot/__main__.py
+++ src/browserist/browser/get/screenshot/__main__.py
@@ -9,7 +9,7 @@ class GetScreenshotDriverMethods(DriverMethods):
         super().__init__(browser_driver, settings)
 
     def visible_portion(self, file_name: str | None = None, destination_dir: str | None = None) -> None:
-        """Take screenshot and save as PNG image. Default destination directory is from where the script is executed. Examples:
+        """Take screenshot of visible portion and save as PNG image. Default destination directory is from where the script is executed. Examples:
 
         browser.get.screenshot.visible_portion() # Default file name and destination
 
