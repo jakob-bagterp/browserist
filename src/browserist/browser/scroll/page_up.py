9941fdc293e71901f1856cd0c1b9ce8e0eced2d5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-27 23:18:14 +0200

    First attempt at new scroll page up method.

--- src/browserist/browser/scroll/page_up.py
+++ src/browserist/browser/scroll/page_up.py
@@ -1,2 +1,10 @@
+from ..get.screen.height import get_screen_height
+from .get_position import get_scroll_position
+from .to_position import scroll_to_position
+
+
 def scroll_page_up(driver: object) -> None:
-    pass
+    current_position_x, current_position_y = get_scroll_position(driver)
+    screen_height_x = get_screen_height(driver)
+    page_up_position_x = max(current_position_x - screen_height_x, 0)
+    scroll_to_position(driver, page_up_position_x, current_position_y)
