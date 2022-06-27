5e05d6ad3887a6b2a8d2e1503f5c941844538a04
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-27 23:24:20 +0200

    Minor add comment.

--- src/browserist/browser/scroll/page_up.py
+++ src/browserist/browser/scroll/page_up.py
@@ -6,5 +6,6 @@ from .to_position import scroll_to_position
 def scroll_page_up(driver: object) -> None:
     current_position_x, current_position_y = get_scroll_position(driver)
     screen_height_x = get_screen_height(driver)
+    # Ensure that we don't scroll to negative numbers:
     page_up_position_x = max(current_position_x - screen_height_x - 1, 0)
     scroll_to_position(driver, page_up_position_x, current_position_y)
