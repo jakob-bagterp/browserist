2143ed19deae3fc52d3cf46426965587baacf78e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-29 18:30:53 +0200

    Minor fixes to previous commit.

--- src/browserist/browser/scroll/page/__main__.py
+++ src/browserist/browser/scroll/page/__main__.py
@@ -1,14 +1,14 @@
 from ....model.browser.base.driver import BrowserDriver
 from ....model.browser.base.settings import BrowserSettings
 from ....model.driver_methods import DriverMethods
-from .page_down import scroll_page_down
+from .down import scroll_page_down
 
 
 class ScrollPageDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
 
-    def page_down(self) -> None:
+    def down(self) -> None:
         """If possible, scroll page down."""
 
         scroll_page_down(self._driver)
