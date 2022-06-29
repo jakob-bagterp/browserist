5c604fcc010fdcb0f7ec2882dc4331303462243e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-29 07:44:55 +0200

    Add minor delay to ensure the page is loaded (similar to other scroll methods).

--- src/browserist/browser/scroll/page_up.py
+++ src/browserist/browser/scroll/page_up.py
@@ -1,3 +1,6 @@
+import time
+
+from ...constant import timeout
 from ..get.screen.height import get_screen_height
 from .get_position import get_scroll_position
 from .to_position import scroll_to_position
@@ -9,3 +12,4 @@ def scroll_page_up(driver: object) -> None:
     # Ensure that we don't scroll to negative numbers:
     page_up_position_x = max(current_position_x - screen_height_x - 1, 0)
     scroll_to_position(driver, page_up_position_x, current_position_y)
+    time.sleep(timeout.VERY_SHORT)  # Small delay to ensure the view is updated.
