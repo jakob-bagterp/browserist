4588d9fb88650615c2007af0df07d8eef58817f1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-29 07:54:42 +0200

    Fix issues and simplify method by using scrollBy relative positioning instead of absolute positioning.

--- src/browserist/browser/scroll/page_down.py
+++ src/browserist/browser/scroll/page_down.py
@@ -2,13 +2,9 @@ import time
 
 from ...constant import timeout
 from ..get.screen.height import get_screen_height
-from .get_position import get_scroll_position
-from .to_position import scroll_to_position
 
 
 def scroll_page_down(driver: object) -> None:
-    current_position_x, current_position_y = get_scroll_position(driver)
-    screen_height_x = get_screen_height(driver)
-    page_down_position_x = current_position_x + screen_height_x + 1
-    scroll_to_position(driver, page_down_position_x, current_position_y)
+    screen_height = get_screen_height(driver)
+    driver.execute_script(f"window.scrollBy(0, {screen_height + 1});")  # type: ignore
     time.sleep(timeout.VERY_SHORT)  # Small delay to ensure the view is updated.
