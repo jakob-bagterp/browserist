e59bdcd4a65b9c11bfecae60a4944693a7d175cb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-30 19:59:56 +0200

    Fix evaluation of position so it's based on Y axis only and not X axis.

--- src/browserist/browser/scroll/check_if/is_top_of_page.py
+++ src/browserist/browser/scroll/check_if/is_top_of_page.py
@@ -2,4 +2,5 @@ from ..get.position import get_scroll_position
 
 
 def check_if_scroll_is_top_of_page(driver: object) -> bool:
-    return (0, 0) == get_scroll_position(driver)
+    _, y = get_scroll_position(driver)
+    return y == 0
