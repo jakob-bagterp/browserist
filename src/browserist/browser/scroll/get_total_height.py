575f35e4a2d3743000dd1c5019a8817e515e64a8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-06-30 07:51:02 +0200

    Fix return type of script.

--- src/browserist/browser/scroll/get_total_height.py
+++ src/browserist/browser/scroll/get_total_height.py
@@ -1,5 +1,5 @@
 def get_total_scroll_height(driver: object) -> int:
-    script_get_total_scroll_height = """Math.max(
+    script_get_total_scroll_height = """return Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
         document.body.offsetHeight, document.documentElement.offsetHeight,
         document.body.clientHeight, document.documentElement.clientHeight
