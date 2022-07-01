76670c985c96d8b2d6f8b47359ada87824520de5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-01 23:13:00 +0200

    Refactor scroll_page_down, scroll_page_up, scroll_to_end_of_page, scroll_to_top_of_page so defaults and timeout are handled are handled from the relevant driver method.

--- src/browserist/browser/scroll/page/__main__.py
+++ src/browserist/browser/scroll/page/__main__.py
@@ -14,19 +14,23 @@ class ScrollPageDriverMethods(DriverMethods):
     def down(self) -> None:
         """If possible, scroll page down."""
 
-        scroll_page_down(self._driver)
+        if self._timeout_should_continue():
+            scroll_page_down(self._driver)
 
     def to_end(self) -> None:
         """If possible, scroll to end of page."""
 
-        scroll_to_end_of_page(self._driver)
+        if self._timeout_should_continue():
+            scroll_to_end_of_page(self._driver)
 
     def to_top(self) -> None:
         """If possible, scroll to top of page."""
 
-        scroll_to_top_of_page(self._driver)
+        if self._timeout_should_continue():
+            scroll_to_top_of_page(self._driver)
 
     def up(self) -> None:
         """If possible, scroll page up."""
 
-        scroll_page_up(self._driver)
+        if self._timeout_should_continue():
+            scroll_page_up(self._driver)
