492c4a9fa09d702e38ec585bca0b233825b23a04
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-01 06:17:34 +0200

    Minor rename test variables.

--- test/browser/scroll/down_by_test.py
+++ test/browser/scroll/down_by_test.py
@@ -13,5 +13,5 @@ def test_scroll_down_by(pixels: int, browser_default_headless_scope_function: Br
     browser.open.url(internal_url.W3SCHOOLS_COM)
     browser.scroll.page.to_top()
     browser.scroll.down_by(pixels)
-    _, get_y = browser.scroll.get.position()
-    assert get_y == pixels
+    _, y_down = browser.scroll.get.position()
+    assert y_down == pixels
