0194e708c05659c91e99867f0d9f19da0b0ea4fb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-02 00:18:58 +0200

    Refactor screen size methods into separate main method group – to simplify the get method group – including tests.

--- test/browser/screen/width_headless_test.py
+++ test/browser/screen/width_headless_test.py
@@ -3,9 +3,9 @@ from _mock_data.url import internal_url
 from browserist import Browser
 
 
-def test_get_screen_width_non_headless(browser_default: Browser) -> None:
-    browser = browser_default
+def test_get_screen_width_headless(browser_default_headless: Browser) -> None:
+    browser = browser_default_headless
     browser.open.url(internal_url.EXAMPLE_COM)
-    screen_width = browser.get.screen.width()
+    screen_width = browser.screen.width()
     window_width, _ = browser.window.get.size()
     assert window_width >= screen_width > 0
