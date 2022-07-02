8a2d86e1f55ce5adb1bd9f93ea094237b2bbe4db
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-02 20:02:00 +0200

    Rename "get_screenshot_visible_portion" to "get_screenshot_of_visible_portion" so it's easier to read.

---
+++ src/browserist/browser/screenshot/complete_page.py
@@ -0,0 +1,23 @@
+from ...model.browser.base.settings import BrowserSettings, BrowserType
+from ..scroll.check_if.is_end_of_page import check_if_scroll_is_end_of_page
+from ..scroll.get.position import get_scroll_position
+from ..scroll.page.down import scroll_page_down
+from ..scroll.page.to_top import scroll_to_top_of_page
+from ..scroll.to_position import scroll_to_position
+from .visible_portion import get_screenshot_of_visible_portion
+
+
+def get_screenshot_complete_page(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
+    def default_get_screenshot_of_complete_page(driver: object) -> None:
+        x_inital, y_initial = get_scroll_position(driver)
+        scroll_to_top_of_page(driver)
+        while check_if_scroll_is_end_of_page(driver) is not True:
+            get_screenshot_of_visible_portion(driver, settings, file_name, destination_dir)
+            scroll_page_down(driver)
+        scroll_to_position(driver, x_inital, y_initial)
+
+    match settings.type:
+        case BrowserType.FIREFOX:
+            pass
+        case _:
+            default_get_screenshot_of_complete_page(driver)
--- src/browserist/browser/screenshot/element.py
+++ src/browserist/browser/screenshot/element.py
@@ -2,7 +2,7 @@ from ... import helper
 from ...model.browser.base.settings import BrowserSettings
 
 
-def get_screenshot_of_visible_portion(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
+def get_screenshot_of_element(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
     if file_name is None:
         file_name = helper.screenshot.default_file_name()
     if destination_dir is None:
