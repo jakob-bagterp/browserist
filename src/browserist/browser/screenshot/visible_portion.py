46b7ef470cb56a30b401798a2c633d5fe00e79e2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-02 21:07:25 +0200

    Refactor destination directory logic into controller and method (so it can be shared by other screenshot functions).

--- src/browserist/browser/screenshot/visible_portion.py
+++ src/browserist/browser/screenshot/visible_portion.py
@@ -4,9 +4,5 @@ from ...model.browser.base.settings import BrowserSettings
 
 def get_screenshot_of_visible_portion(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
     file_name = helper.screenshot.controller.file_name(file_name)
-    if destination_dir is None:
-        destination_dir = settings.screenshot_dir
-    else:
-        destination_dir = helper.directory.ensure_trailing_slash(destination_dir)
-        helper.directory.create_if_not_exists(destination_dir)
+    destination_dir = helper.screenshot.controller.destination_dir(settings, destination_dir)
     driver.save_screenshot(f"{destination_dir}{file_name}")  # type: ignore
--- src/browserist/helper/screenshot/controller.py
+++ src/browserist/helper/screenshot/controller.py
@@ -1,5 +1,14 @@
+from ... import helper
+from ...model.browser.base.settings import BrowserSettings
 from .__init__ import default_file_name
 
 
 def file_name(file_name: str | None) -> str:
     return default_file_name() if file_name is None else file_name
+
+
+def destination_dir(settings: BrowserSettings, destination_dir: str | None = None) -> str:
+    if destination_dir is None:
+        return settings.screenshot_dir
+    helper.directory.create_if_not_exists(destination_dir)
+    return helper.directory.ensure_trailing_slash(destination_dir)
