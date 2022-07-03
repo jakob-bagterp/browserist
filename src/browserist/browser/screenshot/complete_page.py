9fdfc4ec2f53a00331e9ce5b055dd92607688973
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-03 14:16:35 +0200

    Add file name controller and screenshot type to main method.

--- src/browserist/browser/screenshot/complete_page.py
+++ src/browserist/browser/screenshot/complete_page.py
@@ -1,5 +1,6 @@
 from ... import helper
 from ...model.browser.base.settings import BrowserSettings, BrowserType
+from ...model.screenshot import ScreenshotType
 from ..scroll.check_if.is_end_of_page import check_if_scroll_is_end_of_page
 from ..scroll.get.position import get_scroll_position
 from ..scroll.page.down import scroll_page_down
@@ -18,6 +19,7 @@ def get_screenshot_of_complete_page(driver: object, settings: BrowserSettings, f
         scroll_to_position(driver, x_inital, y_initial)
 
     destination_dir = helper.screenshot.controller.destination_dir(settings, destination_dir)
+    file_name = helper.screenshot.controller.file_name(file_name, ScreenshotType.COMPLETE_PAGE)
 
     match settings.type:
         case BrowserType.FIREFOX:
