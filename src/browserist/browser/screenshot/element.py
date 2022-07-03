4d1fff2c280f73526dbfdb714ff570331ef3cfe3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-03 13:57:38 +0200

    Apply screenshot type to screenshot methods for element and visible portion.

--- src/browserist/browser/screenshot/element.py
+++ src/browserist/browser/screenshot/element.py
@@ -1,5 +1,6 @@
 from ... import helper
 from ...model.browser.base.settings import BrowserSettings
+from ...model.screenshot import ScreenshotType
 from ...model.type.xpath import XPath
 from ..get.element import get_element
 
@@ -7,6 +8,6 @@ from ..get.element import get_element
 def get_screenshot_of_element(driver: object, xpath: str, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
     xpath = XPath(xpath)
     element = get_element(driver, xpath)
-    file_name = helper.screenshot.controller.file_name(file_name)
+    file_name = helper.screenshot.controller.file_name(file_name, ScreenshotType.ELEMENT)
     destination_dir = helper.screenshot.controller.destination_dir(settings, destination_dir)
     helper.screenshot.save_element(element, destination_dir, file_name)
--- src/browserist/browser/screenshot/visible_portion.py
+++ src/browserist/browser/screenshot/visible_portion.py
@@ -1,8 +1,9 @@
 from ... import helper
 from ...model.browser.base.settings import BrowserSettings
+from ...model.screenshot import ScreenshotType
 
 
 def get_screenshot_of_visible_portion(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None) -> None:
-    file_name = helper.screenshot.controller.file_name(file_name)
+    file_name = helper.screenshot.controller.file_name(file_name, ScreenshotType.VISIBLE_PORTION)
     destination_dir = helper.screenshot.controller.destination_dir(settings, destination_dir)
     helper.screenshot.save(driver, destination_dir, file_name)
