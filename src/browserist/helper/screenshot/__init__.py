4d51e78b99701493db312cb8e6ec222c17edbead
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-10 22:36:46 +0200

    Add minor safety to generate_file_path method.

--- src/browserist/helper/screenshot/__init__.py
+++ src/browserist/helper/screenshot/__init__.py
@@ -27,6 +27,7 @@ def get_temp_file_prefix_without_iterator_and_file_type() -> str:
 def generate_file_path(destination_dir: str, file_name: str) -> str:
     """Merge destination directory and file name into a single path. Assumes that the directory is valid and exists."""
 
+    destination_dir = helper.directory.ensure_trailing_slash(destination_dir)
     return f"{destination_dir}{file_name}"
 
 
