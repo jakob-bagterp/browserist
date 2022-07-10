f9414f73d8670ba179e3d019e8d3a454a8481175
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-10 23:05:01 +0200

    Add comments to default screenshot flow for complete page.

--- src/browserist/helper/screenshot/complete_page.py
+++ src/browserist/helper/screenshot/complete_page.py
@@ -13,12 +13,16 @@ def firefox(driver: object, file_path: str) -> None:
 
 
 def default(driver: object, file_path: str, settings: BrowserSettings, destination_dir: str) -> None:
+    # Save inital scroll position so we can return to it later.
     x_inital, y_initial = get_scroll_position(driver)
+
+    # Prepare for iteration from the top of the page.
     scroll_to_top_of_page(driver)
     temp_dir = helper.screenshot.controller.mediate_temp_dir(destination_dir)
     temp_file_prefix = helper.screenshot.get_temp_file_prefix_without_iterator_and_file_type()
     all_temp_file_paths: list[str] = []
     i = 1
+
     while check_if_scroll_is_end_of_page(driver) is not True:
         temp_file_name = f"{temp_file_prefix}_{i}.png"
         get_screenshot_of_visible_portion(driver, settings, temp_file_name, temp_dir)
@@ -26,7 +30,10 @@ def default(driver: object, file_path: str, settings: BrowserSettings, destinati
         temp_file_path = helper.screenshot.generate_file_path(temp_dir, temp_file_name)
         all_temp_file_paths.append(temp_file_path)
         i += 1
+
     # TODO: Consider refactoring to async methods so it runs faster:
     helper.screenshot.merge_images(all_temp_file_paths, file_path)
+
+    # Return to initial scroll position and tidy up temp files.
     scroll_to_position(driver, x_inital, y_initial)
     helper.file.remove(all_temp_file_paths)
