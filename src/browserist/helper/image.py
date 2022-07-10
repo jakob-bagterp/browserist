daecb97f548afe9884375ee24b202fb63b1c9990
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-10 11:30:31 +0200

    Attempt to add merge image function based on Pillow.

--- src/browserist/helper/image.py
+++ src/browserist/helper/image.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def is_element_loaded(driver: object, element: object) -> bool:
     """Check if image element is loaded and ready in the DOM."""
 
@@ -5,3 +8,14 @@ def is_element_loaded(driver: object, element: object) -> bool:
         "return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0;",
         element)
     return is_image_loaded or False
+
+
+def merge_vertically(image_base: Image, image_add: Image) -> Image:  # type: ignore
+    """Merge two images vertically. Assumes both images have the same width. "image_add" will be added below "image_base"."""
+
+    merged_image_width: int = image_base.width  # type: ignore
+    merged_image_height: int = image_base.height + image_add.height  # type: ignore
+    merged_image = Image.new("RGB", (merged_image_width, merged_image_height))
+    merged_image.paste(image_base, (0, 0))  # type: ignore
+    merged_image.paste(image_add, (0, image_base.height))  # type: ignore
+    return merged_image  # type: ignore
