5152b35425c7e8e36969a82f69e29c44f75f361b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-11 23:15:40 +0200

    Add data class to handle iteration and data operations.

--- src/browserist/model/screenshot.py
+++ src/browserist/model/screenshot.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
 from enum import Enum, unique
+
+from .. import helper
 
 
 @unique
@@ -8,3 +11,28 @@ class ScreenshotType(Enum):
     COMPLETE_PAGE = "complete page"
     ELEMENT = "element"
     VISIBLE_PORTION = "visible portion"
+
+
+@dataclass(kw_only=True, slots=True)
+class ScreenshotTempDataHandler():
+    """Class to handle iteration details and general data for screenshot of complete page, e.g. file and directory names."""
+
+    destination_dir: str
+    all_temp_file_paths: list[str] = []
+    _temp_file_prefix: str = helper.screenshot.get_temp_file_prefix_without_iterator_and_file_type()
+    _iteration: int = 1
+
+    # TODO: Should be static attribute: temp_dir
+    def get_temp_dir(self) -> str:
+        return helper.screenshot.controller.mediate_temp_dir(self.destination_dir)
+
+    def get_temp_file_name(self) -> str:
+        return f"{self._temp_file_prefix}_{self._iteration}.png"
+
+    def get_temp_file_path(self) -> str:
+        return helper.screenshot.generate_file_path(self.get_temp_dir(), self.get_temp_file_name())
+
+    def next_iteration(self) -> None:
+        temp_file_path = self.get_temp_file_path()
+        self.all_temp_file_paths.append(temp_file_path)
+        self._iteration += 1
