dad75261c715ff04544b26b8b7ef78f934435668
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-12 02:32:34 +0100

    Refactor get_temp_dir() into static class value, including handling of __slots__.

--- src/browserist/model/screenshot.py
+++ src/browserist/model/screenshot.py
@@ -13,9 +13,11 @@ class ScreenshotType(Enum):
     VISIBLE_PORTION = "visible portion"
 
 
-@dataclass(kw_only=True, slots=True)
+@dataclass(kw_only=True)
 class ScreenshotTempDataHandler():
     """Class to handle iteration details and general data for screenshot of complete page, e.g. file and directory names."""
+
+    __slots__ = ["destination_dir", "file_path", "_all_temp_file_paths", "_temp_file_prefix", "_iteration", "_temp_dir"]
 
     destination_dir: str
     file_path: str
@@ -23,15 +25,14 @@ class ScreenshotTempDataHandler():
     _temp_file_prefix: str = helper.screenshot.file.get_temp_prefix_without_iterator_and_file_type()
     _iteration: int = 1
 
-    # TODO: Should be static attribute: temp_dir
-    def get_temp_dir(self) -> str:
-        return helper.screenshot.controller.mediate_temp_dir(self.destination_dir)
+    def __post_init__(self) -> None:
+        self._temp_dir: str = helper.screenshot.controller.mediate_temp_dir(self.destination_dir)
 
     def get_temp_file_name(self) -> str:
         return f"{self._temp_file_prefix}_{self._iteration}.png"
 
     def get_temp_file_path(self) -> str:
-        return helper.screenshot.file.get_path(self.get_temp_dir(), self.get_temp_file_name())
+        return helper.screenshot.file.get_path(self._temp_dir, self.get_temp_file_name())
 
     def next_iteration(self) -> None:
         temp_file_path = self.get_temp_file_path()
