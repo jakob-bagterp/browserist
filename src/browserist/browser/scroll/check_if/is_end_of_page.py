65ab03585662b5c8e4ced8240ebad71a879f584f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-17 18:19:00 +0100

    Rename "delay" to "delay_seconds" so it's easier to read (part 2).

--- src/browserist/browser/scroll/check_if/is_end_of_page.py
+++ src/browserist/browser/scroll/check_if/is_end_of_page.py
@@ -9,8 +9,8 @@ def check_if_scroll_is_end_of_page(driver: object) -> bool:
     # Let's assume that we can't scroll further than the end of the page...
     _, y_initial = get_scroll_position(driver)
     # ... and so scrolling down should yield the same position as the initial one.
-    scroll_by(driver, 0, Y_TEST_DISTANCE, delay=timeout.BYPASS)
+    scroll_by(driver, 0, Y_TEST_DISTANCE, delay_seconds=timeout.BYPASS)
     _, y_after = get_scroll_position(driver)
     # Remember to reset the scroll position to initial position:
-    scroll_by(driver, 0, -Y_TEST_DISTANCE, delay=timeout.BYPASS)
+    scroll_by(driver, 0, -Y_TEST_DISTANCE, delay_seconds=timeout.BYPASS)
     return y_initial == y_after
