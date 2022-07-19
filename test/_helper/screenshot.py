0f8d28677abd9df82fb4a6c56201b95fa1d186a7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-19 01:12:04 +0100

    Refactor screenshot test functions so minimum file size is variable and injected as argument – this makes is flexible for extension to tests of the complete page method.

--- test/_helper/screenshot.py
+++ test/_helper/screenshot.py
@@ -2,18 +2,16 @@ import os
 
 from py.path import local
 
-MINIMUM_FILE_SIZE = 1000
 
-
-def image_has_minimum_file_size(directory: str | local, file_name: str) -> bool:
+def image_has_minimum_file_size(directory: str | local, file_name: str, minimum_file_size: int) -> bool:
     directory = str(directory)
-    return os.path.getsize(f"{directory}/{file_name}") > MINIMUM_FILE_SIZE
+    return os.path.getsize(f"{directory}/{file_name}") > minimum_file_size
 
 
-def images_have_minimum_file_size(directory: str | local) -> bool:
+def images_have_minimum_file_size(directory: str | local, minimum_file_size: int) -> bool:
     def absolute_path(file: str) -> str:
         return os.path.join(directory, file)
 
     directory = str(directory)
     files = [absolute_path(file) for file in os.listdir(directory) if os.path.isfile(absolute_path(file))]
-    return all(os.path.getsize(file) > MINIMUM_FILE_SIZE for file in files)
+    return all(os.path.getsize(file) > minimum_file_size for file in files)
