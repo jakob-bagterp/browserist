60060801ecd1ba157ed113b629de1ce696647c69
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-19 01:38:47 +0100

    Add test of Firefox get screenshot of complete page.

--- test/browser/conftest.py
+++ test/browser/conftest.py
@@ -4,4 +4,5 @@ from _fixture.browser import browser_default_headless  # noqa
 from _fixture.browser import browser_default_headless_disable_images  # noqa
 from _fixture.browser import browser_default_headless_scope_function  # noqa
 from _fixture.browser import browser_default_headless_screenshot  # noqa
+from _fixture.browser import browser_firefox_headless_screenshot  # noqa
 from _fixture.window_handle import window_handle_controller  # noqa
