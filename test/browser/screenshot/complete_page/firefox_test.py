ce8bc3d25f0b486973c8f9b36752d29bb4fc210c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-19 01:57:16 +0100

    Add test of Firefox get screenshot of complete page (part 2).

--- test/browser/screenshot/complete_page/firefox_test.py
+++ test/browser/screenshot/complete_page/firefox_test.py
@@ -5,42 +5,42 @@ from py.path import local
 
 from browserist import Browser
 
-MINIMUM_FILE_SIZE = 60_000
+MINIMUM_FILE_SIZE = 1_000_000
 
 
 def test_firefox_get_screenshot_of_complete_page_1(browser_firefox_headless_screenshot: Browser, tmpdir: local) -> None:
-    """Test of browser.screenshot.visible_portion() with default file name and destination."""
+    """Test of browser.screenshot.complete_page() with default file name and destination."""
 
     browser = browser_firefox_headless_screenshot
     browser.open.url(internal_url.W3SCHOOLS_COM)
-    browser.screenshot.visible_portion()
+    browser.screenshot.complete_page()
     assert screenshot.images_have_minimum_file_size(tmpdir, MINIMUM_FILE_SIZE)
 
 
 def test_firefox_get_screenshot_of_complete_page_2(browser_firefox_headless_screenshot: Browser, tmpdir: local) -> None:
-    """Test of browser.screenshot.visible_portion("image.png") with custom file name and default destination."""
+    """Test of browser.screenshot.complete_page("image.png") with custom file name and default destination."""
 
     browser = browser_firefox_headless_screenshot
     browser.open.url(internal_url.W3SCHOOLS_COM)
-    browser.screenshot.visible_portion(CUSTOM_SCREENSHOT_FILENAME)
+    browser.screenshot.complete_page(CUSTOM_SCREENSHOT_FILENAME)
     assert screenshot.images_have_minimum_file_size(tmpdir, MINIMUM_FILE_SIZE)
 
 
 def test_firefox_get_screenshot_of_complete_page_3(browser_firefox_headless_screenshot: Browser, tmpdir: local) -> None:
-    """Test of browser.screenshot.visible_portion("image.png", "./screenshots") with custom file name and destination."""
+    """Test of browser.screenshot.complete_page("image.png", "./screenshots") with custom file name and destination."""
 
     browser = browser_firefox_headless_screenshot
     browser.open.url(internal_url.W3SCHOOLS_COM)
     temp_dir = str(tmpdir.mkdir(CUSTOM_SCREENSHOT_DIRECTORY))
-    browser.screenshot.visible_portion(CUSTOM_SCREENSHOT_FILENAME, temp_dir)
+    browser.screenshot.complete_page(CUSTOM_SCREENSHOT_FILENAME, temp_dir)
     assert screenshot.image_has_minimum_file_size(temp_dir, CUSTOM_SCREENSHOT_FILENAME, MINIMUM_FILE_SIZE)
 
 
 def test_firefox_get_screenshot_of_complete_page_4(browser_firefox_headless_screenshot: Browser, tmpdir: local) -> None:
-    """Test of browser.screenshot.visible_portion(destination_dir = "./screenshots") with default file name and custom destination."""
+    """Test of browser.screenshot.complete_page(destination_dir = "./screenshots") with default file name and custom destination."""
 
     browser = browser_firefox_headless_screenshot
     browser.open.url(internal_url.W3SCHOOLS_COM)
     temp_dir = str(tmpdir.mkdir(CUSTOM_SCREENSHOT_DIRECTORY))
-    browser.screenshot.visible_portion(destination_dir=temp_dir)
+    browser.screenshot.complete_page(destination_dir=temp_dir)
     assert screenshot.images_have_minimum_file_size(temp_dir, MINIMUM_FILE_SIZE)
