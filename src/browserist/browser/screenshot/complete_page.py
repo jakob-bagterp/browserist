5239dbfed57b670d4e99b3980a69add454162a69
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-20 01:16:14 +0100

    Add FilePNG tiny type check to screenshot methods.

--- src/browserist/browser/screenshot/complete_page.py
+++ src/browserist/browser/screenshot/complete_page.py
@@ -2,10 +2,13 @@ from ... import helper
 from ...model.browser.base.settings import BrowserSettings
 from ...model.browser.base.type import BrowserType
 from ...model.screenshot import ScreenshotType
+from ...model.type.file_png import FilePNG
 
 
 def get_screenshot_of_complete_page(driver: object, settings: BrowserSettings, file_name: str | None = None, destination_dir: str | None = None, delay_seconds: float = 1) -> None:
     destination_dir = helper.screenshot.controller.mediate_destination_dir(settings, destination_dir)
+    if file_name is not None:
+        file_name = FilePNG(file_name)
     file_name = helper.screenshot.controller.mediate_file_name(file_name, ScreenshotType.COMPLETE_PAGE)
     destination_file_path = helper.screenshot.file.get_path(destination_dir, file_name)
 
