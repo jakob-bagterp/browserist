de9ad9e05f731d92dd22653c2371d86f3e204fc4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-22 08:40:17 +0100

    Refactor switch_to_iframe so it takes timeout as argument.

--- src/browserist/browser/iframe/__main__.py
+++ src/browserist/browser/iframe/__main__.py
@@ -9,11 +9,12 @@ class IframeDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
         super().__init__(browser_driver, settings)
 
-    def switch_to(self, xpath: str) -> None:
+    def switch_to(self, xpath: str, timeout: int | None = None) -> None:
         """Switch to iframe."""
 
         if self._timeout_should_continue():
-            switch_to_iframe(self._driver, xpath)
+            timeout = self._mediate_timeout(timeout)
+            switch_to_iframe(self._driver, xpath, timeout)
 
     def switch_to_original_page(self) -> None:
         """After switch to iframe, use this to go back to the original page."""
--- src/browserist/browser/iframe/switch_to.py
+++ src/browserist/browser/iframe/switch_to.py
@@ -1,12 +1,11 @@
 from selenium.webdriver.common.by import By
 
-from ...constant import timeout
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
 
-def switch_to_iframe(driver: object, xpath: str) -> None:
+def switch_to_iframe(driver: object, xpath: str, timeout: int) -> None:
     xpath = XPath(xpath)
-    wait_for_element(driver, xpath, timeout.DEFAULT)
+    wait_for_element(driver, xpath, timeout)
     iframe_element = driver.find_element(By.XPATH, xpath)  # type: ignore
     driver.switch_to.frame(iframe_element)  # type: ignore
