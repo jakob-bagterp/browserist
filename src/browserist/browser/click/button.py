5a56516904d9ddd6c74d7905b5075581415e0db0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-23 13:48:54 +0100

    Refactor click_button so timeouts are handled in the wait_for_element method.

--- src/browserist/browser/click/button.py
+++ src/browserist/browser/click/button.py
@@ -1,8 +1,5 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 
-from ...exception.element import NoElementFoundException
-from ...exception.timeout import WaitForElementTimeoutException
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
@@ -10,10 +7,5 @@ from ..wait.for_element import wait_for_element
 def click_button(driver: object, xpath: str, timeout: int) -> None:
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
-    try:
-        element = driver.find_element(By.XPATH, xpath)  # type: ignore
-        element.click()
-    except TimeoutException:
-        raise WaitForElementTimeoutException(driver, xpath) from TimeoutException
-    except NoSuchElementException:
-        raise NoElementFoundException(driver, xpath) from NoSuchElementException
+    element = driver.find_element(By.XPATH, xpath)  # type: ignore
+    element.click()
