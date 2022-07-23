bdd63382a750e251f6967c5cca346cb3034b29ed
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-23 13:50:55 +0100

    Refactor get_dimensions so timeouts are handled in the wait_for_element method.

--- src/browserist/browser/get/dimensions.py
+++ src/browserist/browser/get/dimensions.py
@@ -1,8 +1,5 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 
-from ...exception.element import NoElementDimensionsFoundException
-from ...exception.timeout import WaitForElementTimeoutException
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
@@ -10,11 +7,6 @@ from ..wait.for_element import wait_for_element
 def get_dimensions(driver: object, xpath: str, timeout: int) -> tuple[int, int]:
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
-    try:
-        # Returns dictionary object, e.g. {'height': 598, 'width': 479}:
-        dimensions: dict[str, int] = driver.find_element(By.XPATH, xpath).size  # type: ignore
-        return dimensions.get("width") or 0, dimensions.get("height") or 0
-    except TimeoutException:
-        raise WaitForElementTimeoutException(driver, xpath) from TimeoutException
-    except NoSuchElementException:
-        raise NoElementDimensionsFoundException(driver, xpath) from NoSuchElementException
+    # Returns dictionary object, e.g. {'height': 598, 'width': 479}:
+    dimensions: dict[str, int] = driver.find_element(By.XPATH, xpath).size  # type: ignore
+    return dimensions.get("width") or 0, dimensions.get("height") or 0
