b63b54df49215426e7cfee72f67214133344f44a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-23 13:37:34 +0100

    Refactor click_button_if_contains_text so timeouts are handled in the wait_for_element method.

--- src/browserist/browser/click/button_if_contains_text.py
+++ src/browserist/browser/click/button_if_contains_text.py
@@ -1,7 +1,4 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
-from ...exception.element import NoElementFoundException, NoElementFoundWithTextConditionException
-from ...exception.timeout import WaitForElementTimeoutException
+from ...exception.element import NoElementFoundWithTextConditionException
 from ...model.type.xpath import XPath
 from ..check_if.contains_text import check_if_contains_text
 from ..wait.for_element import wait_for_element
@@ -10,13 +7,8 @@ from .button import click_button
 
 def click_button_if_contains_text(driver: object, xpath: str, regex: str, ignore_case: bool, timeout: int) -> None:
     xpath = XPath(xpath)
-    try:
-        wait_for_element(driver, xpath, timeout)
-        if check_if_contains_text(driver, xpath, regex, ignore_case, timeout):
-            click_button(driver, xpath, timeout)
-        else:
-            raise NoElementFoundWithTextConditionException(driver, xpath, regex)
-    except TimeoutException:
-        raise WaitForElementTimeoutException(driver, xpath) from TimeoutException
-    except NoSuchElementException:
-        raise NoElementFoundException(driver, xpath) from NoSuchElementException
+    wait_for_element(driver, xpath, timeout)
+    if check_if_contains_text(driver, xpath, regex, ignore_case, timeout):
+        click_button(driver, xpath, timeout)
+    else:
+        raise NoElementFoundWithTextConditionException(driver, xpath, regex)
