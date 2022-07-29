a9772fd25e7c5a000ab3366e825014c12653af00
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-07-29 17:12:42 +0100

    Refactor retry helper method with shared retry method.

--- src/browserist/helper/retry.py
+++ src/browserist/helper/retry.py
@@ -22,28 +22,27 @@ def get_text(driver: object, settings: BrowserSettings, input: str, func: Driver
     return text
 
 
+def retry(retries_left: int, wait_interval_seconds: float, func: DriverGetBoolCallable) -> int:
+    time.sleep(wait_interval_seconds)
+    retries_left -= 1
+    if retries_left == 0:
+        raise RetryTimeoutException(func)
+    return retries_left
+
+
 def until_condition_is_true(driver: object, settings: BrowserSettings, *args: str | list[object], func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func(driver, settings, *args) is False and retries_left > 0:
-        time.sleep(wait_interval_seconds)
-        retries_left -= 1
-        if retries_left == 0:
-            raise RetryTimeoutException(func)
+        retries_left = retry(retries_left, wait_interval_seconds, func)
 
 
 def until_condition_is_false(driver: object, settings: BrowserSettings, *args: str | list[object], func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func(driver, settings, *args) is True and retries_left > 0:
-        time.sleep(wait_interval_seconds)
-        retries_left -= 1
-        if retries_left == 0:
-            raise RetryTimeoutException(func)
+        retries_left = retry(retries_left, wait_interval_seconds, func)
 
 
 def until_condition_is_false_without_browser_settings(driver: object, *args: str | list[object], func: DriverGetBoolCallable, timeout: int = timeout.DEFAULT, wait_interval_seconds: float = interval.DEFAULT) -> None:
     retries_left = calculate_number_of_retries(timeout, wait_interval_seconds)
     while func(driver, *args) is True and retries_left > 0:
-        time.sleep(wait_interval_seconds)
-        retries_left -= 1
-        if retries_left == 0:
-            raise RetryTimeoutException(func)
+        retries_left = retry(retries_left, wait_interval_seconds, func)
