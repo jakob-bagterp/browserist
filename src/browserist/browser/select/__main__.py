843d4ee459a6ad5b8a8e638c0ea465d20e9771a6
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-05 15:44:59 +0100

    Part 14: Refactor select input methods to use BrowserDriver consistently instead of driver and settings arguments.

--- src/browserist/browser/select/__main__.py
+++ src/browserist/browser/select/__main__.py
@@ -11,10 +11,10 @@ class SelectDriverMethods(DriverMethods):
     def input_field(self, xpath: str, timeout: int | None = None) -> None:
         """Select input field, similar to clicking the mouse on a form field."""
 
-        if self._settings.headless:
+        if self._browser_driver.settings.headless:
             raise MethodNotSupportedInHeadlessModeException(
                 "select.input_field", "headless mode doesn't support interactions")
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
-            select_input_field(self._driver, self._settings, xpath, timeout)
+            select_input_field(self._browser_driver, xpath, timeout)
--- src/browserist/browser/select/input_field.py
+++ src/browserist/browser/select/input_field.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 
-from ...model.browser.base.settings import BrowserSettings
+from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
 
 
-def select_input_field(driver: object, settings: BrowserSettings, xpath: str, timeout: int) -> None:
+def select_input_field(browser_driver: BrowserDriver, xpath: str, timeout: int) -> None:
     xpath = XPath(xpath)
-    wait_for_element(driver, settings, xpath, timeout)
+    wait_for_element(browser_driver.webdriver, browser_driver.settings, xpath, timeout)
+    driver = browser_driver.get_webdriver()
     driver.find_element(By.XPATH, xpath).click()  # type: ignore
