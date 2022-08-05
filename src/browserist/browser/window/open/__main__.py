c49f20a59cb8a25666f699340a05214fb55e6d36
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-05 19:48:07 +0100

    Refactor open new tab or window methods to use timeout handling and mediation of timeout settings.

--- src/browserist/browser/window/open/__main__.py
+++ src/browserist/browser/window/open/__main__.py
@@ -12,18 +12,20 @@ class WindowOpenDriverMethods(DriverMethods):
         super().__init__(browser_driver)
         self._controller = controller
 
-    def new_tab(self, url: str | None = None, name: str | None = None) -> None:
+    def new_tab(self, url: str | None = None, name: str | None = None, timeout: int | None = None) -> None:
         """Open and switch to new tab in current window. The URL and name of the tab are optional arguments.
 
         name: Can be used to switch to this tab with the "browser.window.switch_to(name)" method."""
 
         if self._timeout_should_continue():
-            open_new_tab_or_window(self._browser_driver, self._controller, TabOrWindow.TAB, url, name)
+            timeout = self._mediate_timeout(timeout)
+            open_new_tab_or_window(self._browser_driver, self._controller, TabOrWindow.TAB, timeout, url, name)
 
-    def new_window(self, url: str | None = None, name: str | None = None) -> None:
+    def new_window(self, url: str | None = None, name: str | None = None, timeout: int | None = None) -> None:
         """Open and switch to new window. The URL and name of the window are optional arguments.
 
         name: Can be used to switch to this window with the "browser.window.switch_to(name)" method."""
 
         if self._timeout_should_continue():
-            open_new_tab_or_window(self._browser_driver, self._controller, TabOrWindow.WINDOW, url, name)
+            timeout = self._mediate_timeout(timeout)
+            open_new_tab_or_window(self._browser_driver, self._controller, TabOrWindow.WINDOW, timeout, url, name)
--- src/browserist/browser/window/open/new_tab_or_window.py
+++ src/browserist/browser/window/open/new_tab_or_window.py
@@ -11,14 +11,14 @@ from ..handle.current import get_current_window_handle
 def open_new_tab_or_window(browser_driver: BrowserDriver,
                            controller: WindowHandleController,
                            tab_or_window: TabOrWindow,
+                           timeout: int,
                            url: str | None = None,
                            name: str | None = None) -> None:
     url = helper.url.mediate_conversion_to_tiny_type_or_none(url)
     current_number_of_window_handles = count_window_handles(browser_driver, controller, selenium=True)
     driver = browser_driver.get_webdriver()
     driver.switch_to.new_window(tab_or_window.value)  # type: ignore
-    wait_until_number_of_window_handles_is(browser_driver, current_number_of_window_handles + 1,
-                                           browser_driver.settings.timeout.seconds)
+    wait_until_number_of_window_handles_is(browser_driver, current_number_of_window_handles + 1, timeout)
     new_handle_id = get_current_window_handle(browser_driver)
     controller.add_handle(new_handle_id, name)
     if url:
