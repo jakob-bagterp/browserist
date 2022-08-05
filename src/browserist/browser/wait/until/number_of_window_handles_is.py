ae89ec7f8506f78c254c07b796200ea9818e89b3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-05 19:45:14 +0100

    Add timeout exception handling to wait_until_number_of_window_handles_is.

--- src/browserist/browser/wait/until/number_of_window_handles_is.py
+++ src/browserist/browser/wait/until/number_of_window_handles_is.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 
+from .... import helper
 from ....exception.timeout import WaitForWindowTimeoutException
 from ....model.browser.base.driver import BrowserDriver
 
@@ -11,4 +12,5 @@ def wait_until_number_of_window_handles_is(browser_driver: BrowserDriver, expect
         driver = browser_driver.get_webdriver()
         WebDriverWait(driver, timeout).until(EC.number_of_windows_to_be(expected_handles))  # type: ignore
     except TimeoutException:
+        browser_driver.settings = helper.timeout.set_is_timed_out(browser_driver.settings)
         raise WaitForWindowTimeoutException() from TimeoutException
