40fd030c33e5f1094a2b35ae2381f11228c48bdc
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-05 17:53:48 +0100

    Part 17: Refactor combo methods to use BrowserDriver consistently instead of driver and settings arguments.

--- src/browserist/browser/combo/log_in.py
+++ src/browserist/browser/combo/log_in.py
@@ -1,7 +1,7 @@
 import time
 
 from ...constant import timeout
-from ...model.browser.base.settings import BrowserSettings
+from ...model.browser.base.driver import BrowserDriver
 from ...model.combo_settings.login_credentials import LoginCredentials
 from ...model.combo_settings.login_form import LoginForm1Step, LoginForm2Steps
 from ..click.button import click_button
@@ -11,32 +11,32 @@ from ..wait.for_element import wait_for_element
 from ..wait.until.url.contains import wait_until_url_contains
 
 
-def combo_log_in(driver: object, settings: BrowserSettings, login_credentials: LoginCredentials, login_form: LoginForm1Step | LoginForm2Steps) -> None:
+def combo_log_in(browser_driver: BrowserDriver, login_credentials: LoginCredentials, login_form: LoginForm1Step | LoginForm2Steps) -> None:
     # TODO: Incorporate timeout strategy and settings
     # TODO: Handle timeout.DEFAULT in alignment with timeout strategy and settings
 
-    def login_form_1_step(driver: object, settings: BrowserSettings, login_form: LoginForm1Step) -> None:
-        input_value(driver, settings, login_form.username_input_xpath, login_credentials.username, timeout.DEFAULT)
-        input_value(driver, settings, login_form.password_input_xpath, login_credentials.password, timeout.DEFAULT)
-        click_button(driver, settings, login_form.submit_button_xpath, timeout.DEFAULT)
+    def login_form_1_step(browser_driver: BrowserDriver, login_form: LoginForm1Step) -> None:
+        input_value(browser_driver, login_form.username_input_xpath, login_credentials.username, timeout.DEFAULT)
+        input_value(browser_driver, login_form.password_input_xpath, login_credentials.password, timeout.DEFAULT)
+        click_button(browser_driver, login_form.submit_button_xpath, timeout.DEFAULT)
 
-    def login_form_2_steps(driver: object, settings: BrowserSettings, login_form: LoginForm2Steps) -> None:
-        input_value(driver, settings, login_form.username_input_xpath, login_credentials.username, timeout.DEFAULT)
-        click_button(driver, settings, login_form.username_submit_button_xpath, timeout.DEFAULT)
-        input_value(driver, settings, login_form.password_input_xpath, login_credentials.password, timeout.DEFAULT)
-        click_button(driver, settings, login_form.password_submit_button_xpath, timeout.DEFAULT)
+    def login_form_2_steps(browser_driver: BrowserDriver, login_form: LoginForm2Steps) -> None:
+        input_value(browser_driver, login_form.username_input_xpath, login_credentials.username, timeout.DEFAULT)
+        click_button(browser_driver, login_form.username_submit_button_xpath, timeout.DEFAULT)
+        input_value(browser_driver, login_form.password_input_xpath, login_credentials.password, timeout.DEFAULT)
+        click_button(browser_driver, login_form.password_submit_button_xpath, timeout.DEFAULT)
 
     if login_form.url is not None:
-        open_url_if_not_current(driver, login_form.url)
+        open_url_if_not_current(browser_driver, login_form.url)
 
     if type(login_form) is LoginForm1Step:
-        login_form_1_step(driver, settings, login_form)
+        login_form_1_step(browser_driver, login_form)
     elif type(login_form) is LoginForm2Steps:
-        login_form_2_steps(driver, settings, login_form)
+        login_form_2_steps(browser_driver, login_form)
 
     if login_form.post_login_wait_seconds is not None:
         time.sleep(login_form.post_login_wait_seconds)
     if login_form.post_login_url_contains is not None:
-        wait_until_url_contains(driver, login_form.post_login_url_contains, timeout.DEFAULT)
+        wait_until_url_contains(browser_driver, login_form.post_login_url_contains, timeout.DEFAULT)
     if login_form.post_login_element_xpath is not None:
-        wait_for_element(driver, settings, login_form.post_login_element_xpath, timeout.DEFAULT)
+        wait_for_element(browser_driver, login_form.post_login_element_xpath, timeout.DEFAULT)
--- src/browserist/browser/combo/search.py
+++ src/browserist/browser/combo/search.py
@@ -1,5 +1,5 @@
 from ...constant import timeout
-from ...model.browser.base.settings import BrowserSettings
+from ...model.browser.base.driver import BrowserDriver
 from ...model.combo_settings.search import SearchSettings
 from ..click.button import click_button
 from ..input.value import input_value
@@ -8,15 +8,15 @@ from ..wait.for_element import wait_for_element
 from ..wait.until.url.contains import wait_until_url_contains
 
 
-def combo_search(driver: object, settings: BrowserSettings, term: str, search: SearchSettings) -> None:
+def combo_search(browser_driver: BrowserDriver, term: str, search: SearchSettings) -> None:
     # TODO: Incorporate timeout strategy and settings
     # TODO: Handle timeout.DEFAULT in alignment with timeout strategy and settings
 
     if search.url is not None:
-        open_url_if_not_current(driver, search.url)
-    input_value(driver, settings, search.input_xpath, term, timeout.DEFAULT)
-    click_button(driver, settings, search.button_xpath, timeout.DEFAULT)
+        open_url_if_not_current(browser_driver, search.url)
+    input_value(browser_driver, search.input_xpath, term, timeout.DEFAULT)
+    click_button(browser_driver, search.button_xpath, timeout.DEFAULT)
     if search.await_search_results_url_contains is not None:
-        wait_until_url_contains(driver, search.await_search_results_url_contains, timeout.DEFAULT)
+        wait_until_url_contains(browser_driver, search.await_search_results_url_contains, timeout.DEFAULT)
     if search.await_search_results_xpath is not None:
-        wait_for_element(driver, settings, search.await_search_results_xpath, timeout.DEFAULT)
+        wait_for_element(browser_driver, search.await_search_results_xpath, timeout.DEFAULT)
