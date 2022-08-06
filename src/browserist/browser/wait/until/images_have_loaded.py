d0e87aa728f4b457e1f23ab3fb097ebfa968d1a4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 01:24:03 +0200

    Refactor and move image helper back into main helper section.

--- src/browserist/browser/wait/until/images_have_loaded.py
+++ src/browserist/browser/wait/until/images_have_loaded.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-from .... import image_helper, iteration_helper
+from .... import helper, iteration_helper
 from ....model.browser.base.driver import BrowserDriver
 from ....model.type.xpath import XPath
 from ..for_element import wait_for_element
@@ -8,7 +8,7 @@ from ..for_element import wait_for_element
 
 def wait_until_images_have_loaded(browser_driver: BrowserDriver, xpath: str, timeout: float) -> None:
     def are_all_images_loaded(browser_driver: BrowserDriver, elements: list[object]) -> bool:
-        return all(image_helper.is_element_loaded(browser_driver.webdriver, element) is not False for element in elements)
+        return all(helper.image.is_element_loaded(browser_driver.webdriver, element) is not False for element in elements)
 
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
--- test/helper/image/is_element_loaded_test.py
+++ test/helper/image/is_element_loaded_test.py
@@ -1,7 +1,7 @@
 import pytest
 from _mock_data.url import internal_url
 
-from browserist import Browser, image_helper
+from browserist import Browser, helper
 
 
 @pytest.mark.parametrize("xpath, expected", [
@@ -14,4 +14,4 @@ def test_is_element_loaded(xpath: str, expected: bool, browser_default_headless:
     browser = browser_default_headless
     browser.open.url(internal_url.W3SCHOOLS_COM)
     element = browser.get.element(xpath)
-    assert image_helper.is_element_loaded(browser.driver, element) is expected
+    assert helper.image.is_element_loaded(browser.driver, element) is expected
