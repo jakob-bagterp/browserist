36518654f9daf5ff1a1434625062cb43e47c2353
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-06 13:14:40 +0200

    Fix test of retry get text helper.

--- test/helper/retry/get_text_from_element_test.py
+++ test/helper/retry/get_text_from_element_test.py
@@ -19,5 +19,5 @@ def return_input(_: object, input: str) -> str:
 def test_helper_retry_get_text(input: str, expectation: Any, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
     with expectation:
-        _ = iteration_helper.retry.get_text(browser.driver, browser._browser_driver.settings,
-                                            input, return_input, timeout.VERY_SHORT) is not None
+        _ = iteration_helper.retry.get_text(browser._browser_driver, input,
+                                            return_input, timeout.VERY_SHORT) is not None
