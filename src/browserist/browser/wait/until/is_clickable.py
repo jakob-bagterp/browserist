267ee5fd9db5098b2b1c4c9f02e6b40e0c1aa0b2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 17:12:57 +0200

    Update wait_until_element_contains_any_text() for BrowserDriver and timeout strategy.

--- src/browserist/browser/wait/until/is_clickable.py
+++ src/browserist/browser/wait/until/is_clickable.py
@@ -1,9 +1,8 @@
-from .... import helper
-from ....constant import timeout
+from .... import helper_iteration
 from ....model.type.xpath import XPath
 from ...check_if.is_clickable import check_if_is_clickable
 
 
-def wait_until_element_is_clickable(driver: object, xpath: str, timeout: float = timeout.DEFAULT) -> None:
+def wait_until_element_is_clickable(driver: object, xpath: str, timeout: float) -> None:
     xpath = XPath(xpath)
-    helper.retry.until_condition_is_true(driver, xpath, func=check_if_is_clickable, timeout=timeout)
+    helper_iteration.retry.until_condition_is_true(driver, xpath, func=check_if_is_clickable, timeout=timeout)
