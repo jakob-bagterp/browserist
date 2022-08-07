a6c99acfc4a8cd179e5aa358a4d74b9822f8a8b7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 15:31:41 +0200

    Add wait_until_element_contains_any_text() to test of XPath exception handling.

--- test/type/xpath/exception_handling/method/wait_2_test.py
+++ test/type/xpath/exception_handling/method/wait_2_test.py
@@ -1,9 +1,11 @@
 import pytest
 from _mock_data.xpath.method_3 import exception_handling_for_methods_with_3_arguments_or_more
-from _mock_data.xpath.test_set_3 import XPATH_TEST_SET_EXAMPLE_COM_LINK, XPATH_TEST_SET_W3SCHOOLS_COM_IMAGE
+from _mock_data.xpath.test_set_3 import (XPATH_TEST_SET_EXAMPLE_COM_LINK, XPATH_TEST_SET_W3SCHOOLS_COM_HEADLINE,
+                                         XPATH_TEST_SET_W3SCHOOLS_COM_IMAGE)
 
 from browserist import Browser
 from browserist.browser.wait.for_element import wait_for_element
+from browserist.browser.wait.until.contains_any_text import wait_until_element_contains_any_text
 from browserist.browser.wait.until.element_disappears import wait_until_element_disappears
 from browserist.browser.wait.until.text.changes import wait_until_text_changes
 from browserist.browser.wait.until.text.contains import wait_until_text_contains
@@ -36,3 +38,14 @@ def test_xpath_exception_handling_for_wait_methods_2(
 ) -> None:
     exception_handling_for_methods_with_3_arguments_or_more(
         browser_default_headless, method, text, timeout.VERY_SHORT, test_set=XPATH_TEST_SET_EXAMPLE_COM_LINK)
+
+
+@pytest.mark.parametrize("method", [
+    wait_until_element_contains_any_text,
+])
+def test_xpath_exception_handling_for_wait_methods_3(
+    browser_default_headless: Browser,
+    method: BrowserMethodWith3ArgumentsCallable
+) -> None:
+    exception_handling_for_methods_with_3_arguments_or_more(
+        browser_default_headless, method, timeout.VERY_SHORT, test_set=XPATH_TEST_SET_W3SCHOOLS_COM_HEADLINE)
