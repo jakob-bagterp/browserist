f9d82f0b5b7d786ee6e5eb5c9fac83d719356046
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-08 00:07:17 +0200

    Refactor get text driver methods so they contain a single instruction per line and are more readable.

--- src/browserist/browser/get/text.py
+++ src/browserist/browser/get/text.py
@@ -8,7 +8,8 @@ from ..wait.for_element import wait_for_element
 
 def get_text(driver: object, xpath: str, timeout: float = timeout.DEFAULT) -> str:
     def get_inner_text_of_element(driver: object, xpath: str) -> str:
-        return str(driver.find_element(By.XPATH, xpath).text)  # type: ignore
+        element = driver.find_element(By.XPATH, xpath)  # type: ignore
+        return str(element.text)  # type: ignore
 
     xpath = XPath(xpath)
     wait_for_element(driver, xpath, timeout)
