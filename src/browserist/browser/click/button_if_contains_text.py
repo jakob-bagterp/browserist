889d2ab4858b4b729e9fed019f8116f4c3e58c71
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 18:39:14 +0200

    Fix issue in click_button_if_contains_text() by removing timeout from check_if_contains_text() method.

--- src/browserist/browser/click/button_if_contains_text.py
+++ src/browserist/browser/click/button_if_contains_text.py
@@ -9,7 +9,7 @@ from .button import click_button
 def click_button_if_contains_text(browser_driver: BrowserDriver, xpath: str, regex: str, ignore_case: bool, timeout: float) -> None:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
-    if check_if_contains_text(browser_driver, xpath, regex, ignore_case, timeout):
+    if check_if_contains_text(browser_driver, xpath, regex, ignore_case):
         click_button(browser_driver, xpath, timeout)
     else:
         raise NoElementFoundWithTextConditionException(browser_driver, xpath, regex)
