22df3804ddddd9e1c665b690e2f7f01dad59d97d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 23:45:20 +0200

    Increase coverage of check_if_contains_text() method to 100%.

--- test/browser/check_if/contains_text_test.py
+++ test/browser/check_if/contains_text_test.py
@@ -9,6 +9,7 @@ from browserist import Browser
     ("//*[@id='main']/div[1]/div/h1", "code", False, False),
     ("//*[@id='main']/div[1]/div/h1", r"^learn", True, True),
     ("//*[@id='main']/div[1]/div/h1", r"^learn", False, False),
+    ("/does/not/exist", "does not matter", True, False),
 ])
 def test_check_if_contains_text(xpath: str, regex: str, ignore_case: bool, expected: bool, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
