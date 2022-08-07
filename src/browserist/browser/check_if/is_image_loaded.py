3611097ef1f5502eb6d26e9323e13a0f0a6a3e9c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 17:35:40 +0200

    Add try/except clause to check_if_is_image_loaded() and remove dependency to wait_for_element() method.

--- src/browserist/browser/check_if/is_image_loaded.py
+++ src/browserist/browser/check_if/is_image_loaded.py
@@ -1,13 +1,15 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from ... import helper
 from ...constant import timeout
 from ...model.type.xpath import XPath
-from ..wait.for_element import wait_for_element
 
 
 def check_if_is_image_loaded(driver: object, xpath: str, timeout: float = timeout.DEFAULT) -> bool:
     xpath = XPath(xpath)
-    wait_for_element(driver, xpath, timeout)
-    element: object = driver.find_element(By.XPATH, xpath)  # type: ignore
-    return helper.image.is_element_loaded(driver, element)
+    try:
+        element: object = driver.find_element(By.XPATH, xpath)  # type: ignore
+        return helper.image.is_element_loaded(driver, element)
+    except (NoSuchElementException, Exception):
+        return False
