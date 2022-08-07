c5a741f11617e0dbb284b3944ca1f771a3c45a84
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 23:49:05 +0200

    Refactor and rename valid, yet non-existing XPath elements to something more readable.

--- test/browser/check_if/is_disabled_test.py
+++ test/browser/check_if/is_disabled_test.py
@@ -6,7 +6,7 @@ from browserist import Browser
 
 @pytest.mark.parametrize("xpath, expected", [
     ("/html/body/div/p[2]/a", False),
-    ("/html/body/div/h1/div", True),  # Element doesn't exist.
+    ("/does/not/exist", True),
 ])
 def test_check_if_is_disabled(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
     browser = browser_default_headless
