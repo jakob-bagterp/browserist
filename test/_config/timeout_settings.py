6ee20520ee16e88254c7f5d98ce767e0deb51c95
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 13:28:12 +0200

    Add browser fixtures specifically for testing timeout strategy and settings.

--- test/_config/timeout_settings.py
+++ test/_config/timeout_settings.py
@@ -3,3 +3,7 @@ from browserist import TimeoutSettings, TimeoutStrategy
 DEFAULT_CONTINUE = TimeoutSettings(
     strategy=TimeoutStrategy.CONTINUE
 )
+
+DEFAULT_STOP = TimeoutSettings(
+    strategy=TimeoutStrategy.STOP
+)
--- test/_fixture/browser.py
+++ test/_fixture/browser.py
@@ -1,6 +1,7 @@
 from typing import Generator
 
 import pytest
+from _config import timeout_settings
 from _config.browser_settings import default
 from py.path import local
 
@@ -32,6 +33,28 @@ def browser_default_headless() -> Generator[Browser, None, None]:
 @pytest.fixture(scope="session")
 def browser_default_headless_disable_images() -> Generator[Browser, None, None]:
     with Browser(default.HEADLESS_AND_DISABLE_IMAGES) as browser:
+        yield browser
+
+
+@pytest.fixture(scope="session")
+def browser_default_headless_timeout_strategy_continue() -> Generator[Browser, None, None]:
+    browser_settings = BrowserSettings(
+        headless=True,
+        disable_images=True,
+        timeout=timeout_settings.DEFAULT_CONTINUE
+    )
+    with Browser(browser_settings) as browser:
+        yield browser
+
+
+@pytest.fixture(scope="session")
+def browser_default_headless_timeout_strategy_stop() -> Generator[Browser, None, None]:
+    browser_settings = BrowserSettings(
+        headless=True,
+        disable_images=True,
+        timeout=timeout_settings.DEFAULT_STOP
+    )
+    with Browser(browser_settings) as browser:
         yield browser
 
 
