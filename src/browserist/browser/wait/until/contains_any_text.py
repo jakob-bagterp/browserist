82cbdcb9f0c6cd8ce4177e3f2b5c7d34ef80843c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-07 17:13:41 +0200

    Revert "Update wait_until_element_contains_any_text() for BrowserDriver and timeout strategy."
    This reverts commit 267ee5fd9db5098b2b1c4c9f02e6b40e0c1aa0b2.

--- src/browserist/browser/wait/until/contains_any_text.py
+++ src/browserist/browser/wait/until/contains_any_text.py
@@ -1,10 +1,11 @@
 from .... import helper_iteration
+from ....constant import timeout
 from ....model.browser.base.driver import BrowserDriver
 from ....model.type.xpath import XPath
 from ...check_if.contains_any_text import check_if_contains_any_text
 
 
-def wait_until_element_contains_any_text(browser_driver: BrowserDriver, xpath: str, timeout: float) -> None:
+def wait_until_element_contains_any_text(browser_driver: BrowserDriver, xpath: str, timeout: float = timeout.DEFAULT) -> None:
     xpath = XPath(xpath)
     helper_iteration.retry.until_condition_is_true(
         browser_driver, xpath, func=check_if_contains_any_text, timeout=timeout)
