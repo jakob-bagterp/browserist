3102bb8a89ba0072279c08af14d21fd6f656533c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-12 17:55:09 +0200

    Also add text and URL to "does not exist" mock data constants.

--- test/_mock_data/does_not_exist.py
+++ test/_mock_data/does_not_exist.py
@@ -1,3 +1,7 @@
 # All valid values from a syntax perspective, but they don't exist.
 
+TEXT = "does not exist"
+
+URL = "https://www.does-not-exist.xyz"
+
 XPATH = "/does/not/exist"
--- test/timeout/set_test.py
+++ test/timeout/set_test.py
@@ -22,15 +22,15 @@ browser = Browser(browser_settings)
 @pytest.mark.parametrize("browser, browser_function, args", [
     (browser, browser.wait.for_element, (does_not_exist.XPATH, timeout.VERY_SHORT)),
     (browser, browser.wait.until.number_of_window_handles_is, (2, timeout.VERY_SHORT)),
-    (browser, browser.wait.until.page_title.contains, ("does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.page_title.changes, ("does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.page_title.equals, ("does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.text.contains, (does_not_exist.XPATH, "does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.text.changes, (does_not_exist.XPATH, "does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.text.equals, (does_not_exist.XPATH, "does not exist", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.url.contains, ("https://www.does-not-exist.com", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.url.changes, ("https://www.does-not-exist.com", timeout.VERY_SHORT)),
-    (browser, browser.wait.until.url.equals, ("https://www.does-not-exist.com", timeout.VERY_SHORT)),
+    (browser, browser.wait.until.page_title.contains, (does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.page_title.changes, (does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.page_title.equals, (does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.text.contains, (does_not_exist.XPATH, does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.text.changes, (does_not_exist.XPATH, does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.text.equals, (does_not_exist.XPATH, does_not_exist.TEXT, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.url.contains, (does_not_exist.URL, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.url.changes, (does_not_exist.URL, timeout.VERY_SHORT)),
+    (browser, browser.wait.until.url.equals, (does_not_exist.URL, timeout.VERY_SHORT)),
 ])
 def test_set_timeout(browser: Browser, browser_function: BrowserCallable, args: Any) -> None:
     browser.open.url(internal_url.EXAMPLE_COM)
