db5d1e0282e2e2d9c85b64d6b7e7c4a214a1f740
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-15 00:16:59 +0200

    Add tests.

---
+++ test/browser/window/get/width_1_test.py
@@ -0,0 +1,17 @@
+import pytest
+from _helper.timeout import reset_to_not_timed_out
+from _mock_data.url import internal_url
+
+from browserist import Browser
+
+
+@pytest.mark.parametrize("width, height", [
+    (520, 520),  # 500 is minimum width and height for Chrome, 513 on Edge.
+    (600, 600),
+])
+def test_window_get_width(width: int, height: int, browser_default: Browser) -> None:
+    browser = reset_to_not_timed_out(browser_default)
+    browser.open.url(internal_url.EXAMPLE_COM)
+    browser.window.set.size(width, height)
+    get_width = browser.window.get.width()
+    assert width <= get_width
