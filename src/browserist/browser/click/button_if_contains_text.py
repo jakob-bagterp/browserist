ce8a3c0ecc4568d36f87a0d6c96147590cb3ad49
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-08-14 11:43:32 +0200

    Remove the try/except clause with no element and timeout exceptions as this is already handled in the wait_for_element() method.

--- src/browserist/browser/click/button_if_contains_text.py
+++ src/browserist/browser/click/button_if_contains_text.py
@@ -1,9 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-
 from ... import constant
 from ...constant import timeout
-from ...exception.element import NoElementFoundException, NoElementFoundWithTextConditionException
-from ...exception.timeout import WaitForElementTimeoutException
+from ...exception.element import NoElementFoundWithTextConditionException
 from ...model.type.xpath import XPath
 from ..check_if.contains_text import check_if_contains_text
 from ..wait.for_element import wait_for_element
@@ -12,14 +9,9 @@ from .button import click_button
 
 def click_button_if_contains_text(driver: object, xpath: str, regex: str, ignore_case: bool = True, timeout: float = timeout.DEFAULT) -> None:
     xpath = XPath(xpath)
-    try:
-        wait_for_element(driver, xpath, timeout)
-        if check_if_contains_text(driver, xpath, regex, ignore_case):
-            # We bypass the timeout since we know the element is present following the wait_for_element() check:
-            click_button(driver, xpath, constant.timeout.BYPASS)
-        else:
-            raise NoElementFoundWithTextConditionException(driver, xpath, regex)
-    except TimeoutException:
-        raise WaitForElementTimeoutException(driver, xpath) from TimeoutException
-    except NoSuchElementException:
-        raise NoElementFoundException(driver, xpath) from NoSuchElementException
+    wait_for_element(driver, xpath, timeout)
+    if check_if_contains_text(driver, xpath, regex, ignore_case):
+        # We bypass the timeout since we know the element is present following the wait_for_element() check:
+        click_button(driver, xpath, constant.timeout.BYPASS)
+    else:
+        raise NoElementFoundWithTextConditionException(driver, xpath, regex)
