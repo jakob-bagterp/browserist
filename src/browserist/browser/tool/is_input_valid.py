a1228074e9fd6df165f6e328fd3b9bdb01ccd982
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-14 10:25:25 +0200

    Minor refactor of tool_is_input_valid() so if/else expression is on a single line.

--- src/browserist/browser/tool/is_input_valid.py
+++ src/browserist/browser/tool/is_input_valid.py
@@ -2,8 +2,5 @@ import re
 
 
 def tool_is_input_valid(text: str, regex: str, ignore_case: bool = True) -> bool:
-    if ignore_case:
-        match = re.fullmatch(regex, text, re.IGNORECASE)
-    else:
-        match = re.fullmatch(regex, text)
+    match = re.fullmatch(regex, text, re.IGNORECASE) if ignore_case else re.fullmatch(regex, text)
     return bool(match)
