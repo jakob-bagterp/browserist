dd79ad1ca627247aa217d6a6ce0f9289cf616168
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-15 10:32:26 +0200

    Add basic method.

--- src/browserist/browser/screen/__main__.py
+++ src/browserist/browser/screen/__main__.py
@@ -1,6 +1,7 @@
 from ...model.browser.base.driver import BrowserDriver
 from ...model.driver_methods import DriverMethods
 from .height import get_screen_height
+from .set import set_screen_size
 from .size import get_screen_size
 from .width import get_screen_width
 
@@ -14,6 +15,12 @@ class ScreenSizeDriverMethods(DriverMethods):
 
         if self._timeout_should_continue():
             return get_screen_height(self._browser_driver)
+
+    def set(self, width: int, height: int) -> None:
+        """When the inner screen size doesn't have the same dimensions as the outer window size, this attempts to set the screen size."""
+
+        if self._timeout_should_continue():
+            return set_screen_size(self._browser_driver, width, height)
 
     def size(self) -> tuple[int, int]:  # type: ignore
         """Get inner width and height of the screen in pixels. Usage:
---
+++ src/browserist/browser/screen/set.py
@@ -0,0 +1,15 @@
+from ...model.browser.base.driver import BrowserDriver
+from ..window.set.size import set_window_size
+from .size import get_screen_size
+
+
+def set_screen_size(browser_driver: BrowserDriver, width: int, height: int) -> None:
+    # 1st pass:
+    set_window_size(browser_driver, width, height)
+
+    # 2nd pass if 1st pass doesn't match:
+    width_check, height_check = get_screen_size(browser_driver)
+    if width != width_check and height != height_check:
+        width_adjusted = width + (width - width_check)
+        height_adjusted = height + (height - width_check)
+        set_window_size(browser_driver, width_adjusted, height_adjusted)
