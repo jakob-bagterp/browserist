cd7ef8f10b91c9b0c8604e117a847b4579cd965f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-15 11:47:06 +0200

    Rename get screen size method from "size" to "get_size".

--- src/browserist/browser/screen/__main__.py
+++ src/browserist/browser/screen/__main__.py
@@ -1,7 +1,7 @@
 from ...model.browser.base.driver import BrowserDriver
 from ...model.driver_methods import DriverMethods
+from .get_size import get_screen_size
 from .height import get_screen_height
-from .size import get_screen_size
 from .width import get_screen_width
 
 
@@ -15,10 +15,10 @@ class ScreenSizeDriverMethods(DriverMethods):
         if self._timeout_should_continue():
             return get_screen_height(self._browser_driver)
 
-    def size(self) -> tuple[int, int]:  # type: ignore
+    def get_size(self) -> tuple[int, int]:  # type: ignore
         """Get inner width and height of the screen in pixels. Usage:
 
-        width, height = browser.screen.size()"""
+        width, height = browser.screen.get_size()"""
 
         if self._timeout_should_continue():
             return get_screen_size(self._browser_driver)
