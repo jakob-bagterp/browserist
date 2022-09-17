a06e567e12799d1cce0dd0a69b5de138f329a653
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-17 10:35:59 +0200

    Minor update comment.

--- src/browserist/browser/viewport/set_size.py
+++ src/browserist/browser/viewport/set_size.py
@@ -7,7 +7,7 @@ def set_viewport_size(browser_driver: BrowserDriver, width: int, height: int) ->
     # 1st pass:
     set_window_size(browser_driver, width, height)
 
-    # 2nd pass if 1st pass doesn't match. When the inner screen size doesn't have the same dimensions as the outer window size, this attempts to set the viewport size.
+    # 2nd pass if 1st pass doesn't match. When the inner viewport size doesn't have the same dimensions as the outer window size, this attempts to set the viewport size.
     width_check, height_check = get_viewport_size(browser_driver)
     if width != width_check or height != height_check:
         width_adjusted = width + (width - width_check)
