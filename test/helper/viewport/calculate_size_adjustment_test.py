224486ab058d8fe8bcdb1b60d214eceb8209bbed
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-17 16:21:59 +0200

    Minor rename test function.

--- test/helper/viewport/calculate_size_adjustment_test.py
+++ test/helper/viewport/calculate_size_adjustment_test.py
@@ -8,5 +8,5 @@ from browserist import helper
     (200, 206, 194),
     (100, 92, 108),
 ])
-def test_helper_calculate_size_adjustment(size_to_be: int, size_current: int, expected_size: int) -> None:
+def test_helper_viewport_calculate_size_adjustment(size_to_be: int, size_current: int, expected_size: int) -> None:
     assert helper.viewport.calculate_size_adjustment(size_to_be, size_current) == expected_size
