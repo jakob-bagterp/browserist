b6b8bca8d7dfac873a69f5143be37500684a6318
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-17 04:24:52 +0200

    Correct screen size of common Apple devices to UIKit size instead of native resolution.

--- src/browserist/model/screen_size/apple.py
+++ src/browserist/model/screen_size/apple.py
@@ -7,25 +7,25 @@ from .device import DeviceScreenSize
 class AppleDevices:
     """Reference: https://developer.apple.com/library/archive/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/Displays/Displays.html"""
 
-    IPHONE_X: DeviceScreenSize = DeviceScreenSize(1125, 2436)
+    IPHONE_X: DeviceScreenSize = DeviceScreenSize(375, 812)
 
-    IPHONE_8_PLUS: DeviceScreenSize = DeviceScreenSize(1080, 1920)
-    IPHONE_8: DeviceScreenSize = DeviceScreenSize(750, 1334)
+    IPHONE_8_PLUS: DeviceScreenSize = DeviceScreenSize(414, 736)
+    IPHONE_8: DeviceScreenSize = DeviceScreenSize(375, 667)
 
-    IPHONE_7_PLUS: DeviceScreenSize = DeviceScreenSize(1080, 1920)
-    IPHONE_7: DeviceScreenSize = DeviceScreenSize(750, 1334)
+    IPHONE_7_PLUS: DeviceScreenSize = DeviceScreenSize(414, 736)
+    IPHONE_7: DeviceScreenSize = DeviceScreenSize(375, 667)
 
-    IPHONE_6S_PLUS: DeviceScreenSize = DeviceScreenSize(1080, 1920)
-    IPHONE_6S: DeviceScreenSize = DeviceScreenSize(1080, 1920)
-    IPHONE_6_PLUS: DeviceScreenSize = DeviceScreenSize(1080, 1920)
-    IPHONE_6: DeviceScreenSize = DeviceScreenSize(750, 1334)
+    IPHONE_6S_PLUS: DeviceScreenSize = DeviceScreenSize(375, 667)
+    IPHONE_6S: DeviceScreenSize = DeviceScreenSize(375, 667)
+    IPHONE_6_PLUS: DeviceScreenSize = DeviceScreenSize(375, 667)
+    IPHONE_6: DeviceScreenSize = DeviceScreenSize(375, 667)
 
-    IPHONE_SE: DeviceScreenSize = DeviceScreenSize(640, 1136)
+    IPHONE_SE: DeviceScreenSize = DeviceScreenSize(320, 568)
 
-    IPAD_PRO_12_9_INCH_2ND_GEN: DeviceScreenSize = DeviceScreenSize(2048, 2732)
-    IPAD_PRO_12_9_INCH: DeviceScreenSize = DeviceScreenSize(2048, 2732)
-    IPAD_PRO_10_5_INCH: DeviceScreenSize = DeviceScreenSize(2224, 1668)
-    IPAD_PRO_9_7_INCH: DeviceScreenSize = DeviceScreenSize(1536, 2048)
+    IPAD_PRO_12_9_INCH_2ND_GEN: DeviceScreenSize = DeviceScreenSize(1024, 1366)
+    IPAD_PRO_12_9_INCH: DeviceScreenSize = DeviceScreenSize(1024, 1366)
+    IPAD_PRO_10_5_INCH: DeviceScreenSize = DeviceScreenSize(1112, 834)
+    IPAD_PRO_9_7_INCH: DeviceScreenSize = DeviceScreenSize(768, 1024)
 
-    IPAD_AIR_2: DeviceScreenSize = DeviceScreenSize(1536, 2048)
-    IPAD_MINI_4: DeviceScreenSize = DeviceScreenSize(1536, 2048)
+    IPAD_AIR_2: DeviceScreenSize = DeviceScreenSize(768, 1024)
+    IPAD_MINI_4: DeviceScreenSize = DeviceScreenSize(768, 1024)
