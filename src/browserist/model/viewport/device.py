7330a9cdab2068d31bead6dcda3515314c6d1d5e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-17 17:00:56 +0200

    Add docstring.

--- src/browserist/model/viewport/device.py
+++ src/browserist/model/viewport/device.py
@@ -3,5 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(slots=True)
 class DeviceViewport:
+    """Class to define viewport size (width and height) of common devices."""
+
     width: int
     height: int
