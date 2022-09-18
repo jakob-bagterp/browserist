e04a51a9fd188e82b51b2ba1eaa30b318d942c37
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-18 22:57:36 +0200

    Refactor and simplify several methods to use new internal get_element_without_wait() method.

--- src/browserist/browser/check_if/is_clickable.py
+++ src/browserist/browser/check_if/is_clickable.py
@@ -1,15 +1,14 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
+from ..get.element import get_element_without_wait
 
 
 def check_if_is_clickable(browser_driver: BrowserDriver, xpath: str) -> bool:
     xpath = XPath(xpath)
     try:
-        driver = browser_driver.get_webdriver()
-        element = driver.find_element(By.XPATH, xpath)  # type: ignore
+        element = get_element_without_wait(browser_driver, xpath)
         return bool(EC.element_to_be_clickable(element))  # type: ignore
     except Exception:
         return False
