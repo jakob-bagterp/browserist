961bbe33d8b0c77f8dc259cfc78a9d060db6a0e0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-18 09:44:09 +0200

    Fix issue: "TypeError: element_to_be_clickable() takes 1 positional argument but 2 were given" (reference: https://github.com/SeleniumHQ/selenium/blob/trunk/py/selenium/webdriver/support/expected_conditions.py#L299)

--- src/browserist/browser/check_if/is_clickable.py
+++ src/browserist/browser/check_if/is_clickable.py
@@ -1,8 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 
-from ...constant import timeout
 from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
 
@@ -11,8 +9,7 @@ def check_if_is_clickable(browser_driver: BrowserDriver, xpath: str) -> bool:
     xpath = XPath(xpath)
     try:
         driver = browser_driver.get_webdriver()
-        element = WebDriverWait(driver, timeout.BYPASS).until(
-            EC.element_to_be_clickable(By.XPATH, xpath))  # type: ignore
-        return element is not None
+        element = driver.find_element(By.XPATH, xpath)  # type: ignore
+        return bool(EC.element_to_be_clickable(element))  # type: ignore
     except Exception:
         return False
