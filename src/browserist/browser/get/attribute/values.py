326be228cffdfaf754dfc845b0c87985054ca414
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-20 19:44:16 +0200

    Refactor and replace with new get_elements_without_wait() method.

--- src/browserist/browser/get/attribute/values.py
+++ src/browserist/browser/get/attribute/values.py
@@ -1,13 +1,11 @@
-from selenium.webdriver.common.by import By
-
 from ....model.browser.base.driver import BrowserDriver
 from ....model.type.xpath import XPath
+from ...get.elements import get_elements_without_wait
 from ...wait.for_element import wait_for_element
 
 
 def get_attribute_values(browser_driver: BrowserDriver, xpath: str, attribute: str, timeout: float) -> list[str]:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
-    driver = browser_driver.get_webdriver()
-    elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
+    elements = get_elements_without_wait(browser_driver, xpath)
     return [element.get_attribute(attribute) for element in elements]  # type: ignore
--- src/browserist/browser/tool/count_elements.py
+++ src/browserist/browser/tool/count_elements.py
@@ -1,13 +1,11 @@
-from selenium.webdriver.common.by import By
-
 from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
+from ..get.elements import get_elements_without_wait
 from ..wait.for_element import wait_for_element
 
 
 def tool_count_elements(browser_driver: BrowserDriver, xpath: str, timeout: float) -> int:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
-    driver = browser_driver.get_webdriver()
-    elements: list[object] = driver.find_elements(By.XPATH, xpath)  # type: ignore
+    elements = get_elements_without_wait(browser_driver, xpath)
     return len(elements)
