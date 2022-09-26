2a7c7b1cc49c6bbd2a04bbf32ed617a26456ffb2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-09-26 06:47:42 +0200

    Attempt to fix failed GitHub Actions for Windows run by handling special case.

--- test/_mock_data/path/test_set_4.py
+++ test/_mock_data/path/test_set_4.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
+from browserist.helper.directory import operating_system
 from browserist.model.type.path import FilePath
 
 VALID_FILE_PATH = FilePath(Path.cwd())
 
-VALID_ROOT_DIR_AS_STRING = "/"
+VALID_ROOT_DIR_AS_STRING = "\\" if operating_system.is_windows() else "/"
 
 VALID_PATH = Path(VALID_ROOT_DIR_AS_STRING)
