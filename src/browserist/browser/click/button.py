a21ea31d603256baa46dc8af30ceede623ca91c4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-11 23:20:20 +0100

    Refactor click button method and add no wait variant for internal use.

--- src/browserist/browser/click/button.py
+++ src/browserist/browser/click/button.py
@@ -7,5 +7,11 @@ from ..wait.for_element import wait_for_element
 def click_button(browser_driver: BrowserDriver, xpath: str, timeout: float) -> None:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
+    click_button_without_wait(browser_driver, xpath)
+
+
+def click_button_without_wait(browser_driver: BrowserDriver, xpath: XPath) -> None:
+    """Variation of the click_button() method for internal use only. It assumes that the XPath has already been validated."""
+
     element = get_element_without_wait(browser_driver, xpath)
     element.click()  # type: ignore
