f2605af0025e8428cefc6ca1a520656b573ae2d9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-26 11:15:14 +0100

    Rename wait function for "random time" to "random seconds".

--- test/browser/wait/random_seconds_test.py
+++ test/browser/wait/random_seconds_test.py
@@ -10,10 +10,10 @@ from browserist import Browser
     (1, 2),
     (2, 4),
 ])
-def test_wait_random_time(min_seconds: int, max_seconds: int, browser_default_headless: Browser) -> None:
+def test_wait_random_seconds(min_seconds: int, max_seconds: int, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     start_time_ns = time.perf_counter_ns()
-    browser.wait.random_time(min_seconds, max_seconds)
+    browser.wait.random_seconds(min_seconds, max_seconds)
     stop_time_ns = time.perf_counter_ns()
     elapsed_time_seconds = (stop_time_ns - start_time_ns) / 1_000_000_000
     assert min_seconds <= elapsed_time_seconds
