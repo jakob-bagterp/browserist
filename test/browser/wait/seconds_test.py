cca06a69978c590c8a2d9203fce5b1de59ad6d10
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-26 11:32:42 +0100

    Add test.

---
+++ test/browser/wait/seconds_test.py
@@ -0,0 +1,23 @@
+import time
+
+import pytest
+from _helper.timeout import reset_to_not_timed_out
+
+from browserist import Browser
+
+TIMING_MARGIN = 10 / 100  # 10%
+
+
+@pytest.mark.parametrize("seconds", [
+    1,
+    5.5,
+])
+def test_wait_seconds(seconds: float, browser_default_headless: Browser) -> None:
+    browser = reset_to_not_timed_out(browser_default_headless)
+    start_time_ns = time.perf_counter_ns()
+    browser.wait.seconds(seconds)
+    stop_time_ns = time.perf_counter_ns()
+    elapsed_time_seconds = (stop_time_ns - start_time_ns) / 1_000_000_000
+    max_timing_margin_seconds = seconds + seconds * TIMING_MARGIN
+    assert seconds <= elapsed_time_seconds
+    assert elapsed_time_seconds <= max_timing_margin_seconds
