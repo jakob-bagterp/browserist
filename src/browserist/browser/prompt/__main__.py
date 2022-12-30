9f000e057cd9eed736c17a2365f591e6f74d2b3e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-30 17:11:05 +0100

    Add new method to prompt user whether the process should proceed or not.

--- src/browserist/browser/prompt/__main__.py
+++ src/browserist/browser/prompt/__main__.py
@@ -1,6 +1,7 @@
 from ...model.browser.base.driver import BrowserDriver
 from ...model.driver_methods import DriverMethods
 from .input_value import prompt_and_input_value
+from .proceed_yes_or_no import prompt_proceed_yes_or_no
 
 
 class PromptDriverMethods(DriverMethods):
@@ -13,3 +14,11 @@ class PromptDriverMethods(DriverMethods):
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
             prompt_and_input_value(self._browser_driver, xpath, prompt_message, validate_input_regex, timeout)
+
+    def prompt_proceed_yes_or_no(self) -> bool:
+        """Prompt user whether to proceed or not through the terminal."""
+
+        if self._timeout_should_continue():
+            return prompt_proceed_yes_or_no()
+        else:
+            return False
