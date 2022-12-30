da842bf3513707f172eba546ffd187b8c6b15723
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-30 16:51:58 +0100

    Add new method to prompt user for input through the terminal, including helper functions.

--- src/browserist/browser/prompt/__main__.py
+++ src/browserist/browser/prompt/__main__.py
@@ -1,7 +1,15 @@
 from ...model.browser.base.driver import BrowserDriver
 from ...model.driver_methods import DriverMethods
+from .input_value import prompt_and_input_value
 
 
 class PromptDriverMethods(DriverMethods):
     def __init__(self, browser_driver: BrowserDriver) -> None:
         super().__init__(browser_driver)
+
+    def input_value(self, xpath: str, prompt_message: str, validate_input_regex: str | None = None, timeout: float | None = None) -> None:
+        """Prompt user for value through the terminal and insert this value into form field. Optional to validate input by regex."""
+
+        if self._timeout_should_continue():
+            timeout = self._mediate_timeout(timeout)
+            prompt_and_input_value(self._browser_driver, xpath, prompt_message, validate_input_regex, timeout)
