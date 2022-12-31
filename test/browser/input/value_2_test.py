2f92626905a9cd6a0af9ef85429a25236a9a40d1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-12-31 11:03:54 +0100

    Refactor XPath to constant for input value test.

--- test/browser/input/value_2_test.py
+++ test/browser/input/value_2_test.py
@@ -9,10 +9,12 @@ from browserist import Browser
 from browserist.constant import timeout
 from browserist.exception.timeout import WaitForElementTimeoutException
 
+SEARCH_INPUT_XPATH = "//input[@id='search2']"
+
 
 @pytest.mark.parametrize("url, xpath, expectation", [
-    (internal_url.W3SCHOOLS_COM, "//input[@id='search2']", does_not_raise()),
-    (internal_url.W3SCHOOLS_COM, "//input[@id='search2']/div", pytest.raises(WaitForElementTimeoutException)),
+    (internal_url.W3SCHOOLS_COM, SEARCH_INPUT_XPATH, does_not_raise()),
+    (internal_url.W3SCHOOLS_COM, f"{SEARCH_INPUT_XPATH}/div", pytest.raises(WaitForElementTimeoutException)),
 ])
 def test_input_value_exceptions(url: str, xpath: str, expectation: Any, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
