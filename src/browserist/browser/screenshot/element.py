0d63bcd2525fdadec576ff5ea139d9b821285b68
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-03-13 15:37:55 +0100

    Add domain to existing default naming method of screenshots.

--- src/browserist/browser/screenshot/element.py
+++ src/browserist/browser/screenshot/element.py
@@ -13,7 +13,7 @@ def get_screenshot_of_element(browser_driver: BrowserDriver, xpath: str, file_na
     xpath = XPath(xpath)
     if file_name is not None:
         file_name = FilePNG(file_name)
-    file_name = helper_screenshot.controller.mediate_file_name(file_name, ScreenshotType.ELEMENT)
+    file_name = helper_screenshot.controller.mediate_file_name(browser_driver, file_name, ScreenshotType.ELEMENT)
     destination_dir = helper_screenshot.controller.mediate_destination_dir(browser_driver.settings, destination_dir)
     file_path = helper_screenshot.file.get_path(file_name, destination_dir)
     element = get_element(browser_driver, xpath, timeout.DEFAULT)
