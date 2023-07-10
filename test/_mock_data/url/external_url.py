12f25f2d1460d1cff7ca7d4bc1b20c32f3736559
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-07-10 17:34:56 +0200

    WIP: Rename URL for IANA.

--- test/_mock_data/url/external_url.py
+++ test/_mock_data/url/external_url.py
@@ -8,6 +8,6 @@ GOOGLE_COM: str = "https://www.google.com/"
 
 JYLLANDSPOSTEN_DK: str = "https://jyllands-posten.dk/"
 
-IANA_ORG: str = "https://www.iana.org/domains/reserved"
+IANA_ORG_RESERVED_DOMAINS: str = "https://www.iana.org/domains/reserved"
 
 W3SCHOOLS_COM: str = "https://www.w3schools.com/"
