973702dfa531bcf1963ed2beed3939514e1fa647
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-07-28 14:46:43 +0200

    Update docstrings (part 5).

--- src/browserist/browser/get/__main__.py
+++ src/browserist/browser/get/__main__.py
@@ -20,23 +20,50 @@ class GetDriverMethods(DriverMethods):
         self.url: GetUrlDriverMethods = GetUrlDriverMethods(browser_driver)
 
     def dimensions(self, xpath: str, timeout: float | None = None) -> tuple[int, int]:  # type: ignore
-        """Get width and height of element in pixels. Usage:
+        """Get width and height of element in pixels.
 
-        width, height = browser.get.dimensions("/element/xpath")"""
+        Args:
+            xpath (str): XPath of the element.
+            timeout (float | None, optional): Timeout in seconds. If `None`, the global timeout setting is used.
+
+        Returns:
+            tuple[int, int]: Width and height in pixels.
+
+        Example:
+            ```python
+            width, height = browser.get.dimensions("/element/xpath")
+            ```
+        """
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
             return get_dimensions(self._browser_driver, xpath, timeout)
 
     def element(self, xpath: str, timeout: float | None = None) -> object:  # type: ignore
-        """Get single web element by XPath."""
+        """Get single web element by XPath.
+
+        Args:
+            xpath (str): XPath of the element.
+            timeout (float | None, optional): Timeout in seconds. If `None`, the global timeout setting is used.
+
+        Returns:
+            object: Web element.
+        """
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
             return get_element(self._browser_driver, xpath, timeout)
 
     def elements(self, xpath: str, timeout: float | None = None) -> list[object]:  # type: ignore
-        """Get multiple web elements by XPath."""
+        """Get multiple web elements by XPath.
+
+        Args:
+            xpath (str): XPath of the elements.
+            timeout (float | None, optional): Timeout in seconds. If `None`, the global timeout setting is used.
+
+        Returns:
+            list[object]: List of web elements.
+        """
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
@@ -50,7 +77,11 @@ class GetDriverMethods(DriverMethods):
             return get_elements_by_tag(self._browser_driver, tag, timeout)
 
     def page_title(self) -> str:  # type: ignore
-        """Get page title of the current page."""
+        """Get page title of the current page.
+
+        Returns:
+            str: Page title.
+        """
 
         if self._timeout_should_continue():
             return get_page_title(self._browser_driver)
@@ -58,16 +89,31 @@ class GetDriverMethods(DriverMethods):
     def text(self, xpath: str, timeout: float | None = None) -> str:  # type: ignore
         """Get text from element.
 
-        This method assumes that the text field shouldn't be empty and therefore will retry to get the text (for better support of single-page apps with extended loading time)."""
+        Note:
+            This method assumes that the text field shouldn't be empty and therefore will retry to get the text (for better support of single-page apps with extended loading time).
+
+        Args:
+            xpath (str): XPath of the element.
+            timeout (float | None, optional): Timeout in seconds. If `None`, the global timeout setting is used.
+
+        Returns:
+            str: Text from element.
+        """
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
             return get_text(self._browser_driver, xpath, timeout)
 
     def texts(self, xpath: str, timeout: float | None = None) -> list[str]:  # type: ignore
-        """Get array of texts from elements.
+        """Get array of texts from elements. Assumes that the XPath targets multiple elements.
 
-        Assumes that the XPath targets multiple elements."""
+        Args:
+            xpath (str): XPath of the elements.
+            timeout (float | None, optional): _description_. Defaults to None.
+
+        Returns:
+            list[str]: _description_
+        """
 
         if self._timeout_should_continue():
             timeout = self._mediate_timeout(timeout)
