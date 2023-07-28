9f71f6fec6dc8b1df30a796fbdd855b242871f20
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-07-28 15:22:55 +0200

    Attempt to fix failed Windows tests by reducing fixture scope.

--- test/browser/scroll/page/to_top_test.py
+++ test/browser/scroll/page/to_top_test.py
@@ -4,8 +4,8 @@ from _mock_data.url import internal_url
 from browserist import Browser
 
 
-def test_scroll_to_top_of_page(browser_default_headless: Browser) -> None:
-    browser = reset_to_not_timed_out(browser_default_headless)
+def test_scroll_to_top_of_page(browser_default_headless_scope_function: Browser) -> None:
+    browser = reset_to_not_timed_out(browser_default_headless_scope_function)
     browser.open.url(internal_url.W3SCHOOLS_COM)
     browser.scroll.page.to_end()
     x_end, y_end = browser.scroll.get.position()
