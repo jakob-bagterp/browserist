aa5046b6af34d2bebf495ca2db571e4961c0704f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-07-31 11:58:01 +0200

    Add info about CookieBannerSettings and update docstring.

--- docs/reference/browser/combo.md
+++ docs/reference/browser/combo.md
@@ -1,3 +1,7 @@
 # browser.combo
 
 ::: browserist.browser.combo.__main__.ComboDriverMethods
+
+## CookieBannerSettings
+
+::: browserist.model.combo_settings.cookie_banner.CookieBannerSettings
--- src/browserist/model/combo_settings/cookie_banner.py
+++ src/browserist/model/combo_settings/cookie_banner.py
@@ -8,15 +8,13 @@ from ...model.type.xpath import XPath
 class CookieBannerSettings:
     """Object with data needed to accept or decline cookies from a banner.
 
-    button_xpath: Can be for either accept or decline cookies.
-
-    url: Optional URL from where to handle the cookie banner.
-
-    has_loaded_wait_seconds: Minor grace time to make sure the cookie banner has loaded.
-
-    has_loaded_xpath: Check if banner element has loaded so the cookie banner is ready.
-
-    has_disappeared_wait_seconds: Minor grace time to make sure the cookie banner has disappeared and that the cookie information has been saved before proceeding."""
+    Args:
+        url (str | None, optional): URL from where to handle the cookie banner.
+        has_loaded_wait_seconds (float | None, optional): Minor grace time to ensure the cookie banner has loaded. Often due an fade-in animation or similar transition.
+        has_loaded_xpath (str | None , optional): Check if cookie banner has loaded so it's ready for interaction.
+        button_xpath (str): Can be for either accept or decline cookies.
+        has_disappeared_wait_seconds (float | None, optional): Minor grace time to ensure the cookie banner has disappeared – often due an animation – and that the cookie information has been saved before proceeding.
+    """
 
     url: str | None = None
     has_loaded_wait_seconds: float | None = None
