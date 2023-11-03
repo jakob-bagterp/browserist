b6f601eab5652ae89954aead05b258cb4df7ee19
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-11-03 11:25:58 +0100

    WIP: First attempt to add iframe option in combo and settings.

--- src/browserist/browser/combo/cookie_banner.py
+++ src/browserist/browser/combo/cookie_banner.py
@@ -6,6 +6,8 @@ from ...model.combo_settings.cookie_banner import CookieBannerSettings
 from ...model.driver_methods import DriverMethods
 from ...model.type.callable import TimeoutShouldContinueCallable
 from ..click.button import click_button
+from ..iframe.switch_to import switch_to_iframe
+from ..iframe.switch_to_original_page import switch_to_original_page
 from ..open.url_if_not_current import open_url_if_not_current
 from ..wait.for_element import wait_for_element
 from ..wait.until.element_disappears import wait_until_element_disappears
@@ -17,6 +19,8 @@ def combo_cookie_banner(driver_method: DriverMethods, cookie_banner: CookieBanne
 
     if cookie_banner.url is not None and timeout_should_continue():
         open_url_if_not_current(browser_driver, cookie_banner.url)
+    if cookie_banner.iframe_xpath is not None and timeout_should_continue():
+        switch_to_iframe(browser_driver, cookie_banner.iframe_xpath, timeout)
     if cookie_banner.has_loaded_wait_seconds is not None:
         time.sleep(cookie_banner.has_loaded_wait_seconds)
     if cookie_banner.has_loaded_xpath is not None and timeout_should_continue():
@@ -29,3 +33,5 @@ def combo_cookie_banner(driver_method: DriverMethods, cookie_banner: CookieBanne
         time.sleep(constant.timeout.VERY_SHORT)
     if timeout_should_continue():
         wait_until_element_disappears(browser_driver, cookie_banner.button_xpath, timeout)
+    if cookie_banner.iframe_xpath is not None and timeout_should_continue():
+        switch_to_original_page(browser_driver)
