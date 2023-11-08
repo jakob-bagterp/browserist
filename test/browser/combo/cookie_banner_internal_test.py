bc50b203f2086f3467b22dff2ca0d9e65f29d186
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-11-08 08:51:42 +0100

    Fix issue.

--- test/browser/combo/cookie_banner_internal_test.py
+++ test/browser/combo/cookie_banner_internal_test.py
@@ -12,13 +12,13 @@ ACCEPT_BUTTON_XPATH = "//button[@id='accept-cookies']"
 
 COOKIE_BANNER_SETTINGS_WITH_IFRAME = CookieBannerSettings(
     url=internal_url.COOKIE_BANNER_WITH_IFRAME,
+    iframe_xpath="//iframe[@id='cookie-banner-iframe']",
     has_loaded_xpath=HAS_LOADED_XPATH,
     button_xpath=ACCEPT_BUTTON_XPATH,
 )
 
 COOKIE_BANNER_SETTINGS_WITHOUT_IFRAME = CookieBannerSettings(
     url=internal_url.COOKIE_BANNER_WITHOUT_IFRAME,
-    iframe_xpath="//iframe[@id='cookie-banner-iframe']",
     has_loaded_xpath=HAS_LOADED_XPATH,
     button_xpath=ACCEPT_BUTTON_XPATH,
 )
