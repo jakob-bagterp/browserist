c035febb622a2f262a6fede31526e4add12820ec
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-11-21 19:11:48 +0100

    Minor reorder of parameter assignments so it's aligned with the flow.

--- test/_config/combo/search.py
+++ test/_config/combo/search.py
@@ -3,7 +3,7 @@ from _mock_data.url import internal_url
 from browserist import SearchSettings
 
 SEARCH_SETTINGS = SearchSettings(
+    url=internal_url.SEARCH,
     input_xpath="//*[@id='search-input']",
     button_xpath="//*[@id='submit']",
-    url=internal_url.SEARCH,
 )
