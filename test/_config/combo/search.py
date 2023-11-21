8f52da8ded9d24af3c441033b6cb7c474edbfce9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-11-21 19:14:07 +0100

    Add extra parameter to test search combo settings.

--- test/_config/combo/search.py
+++ test/_config/combo/search.py
@@ -6,4 +6,5 @@ SEARCH_SETTINGS = SearchSettings(
     url=internal_url.SEARCH,
     input_xpath="//*[@id='search-input']",
     button_xpath="//*[@id='submit']",
+    await_search_results_xpath="//*[@id='search-results']/div[@class='search-result-item']",
 )
