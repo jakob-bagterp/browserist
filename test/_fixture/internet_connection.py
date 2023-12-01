589a3a0e66dac98becdbe2d9600724852c9ce3d9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-01 13:41:50 +0100

    Minor rename variable and add docstring.

--- test/_fixture/internet_connection.py
+++ test/_fixture/internet_connection.py
@@ -2,15 +2,15 @@ import socket
 
 import pytest
 
-_original_connection = socket.socket.connect
+ORIGINAL_SOCKET_CONNECTION = socket.socket.connect
 
 
 def no_internet_connection(*args, **kwargs):
-    pass
+    """When mocked to `socket.socket.connect`, this emulates no network connection."""
 
 
 @pytest.fixture(scope="function")
 def disable_network():
     socket.socket.connect = no_internet_connection
     yield
-    socket.socket.connect = _original_connection
+    socket.socket.connect = ORIGINAL_SOCKET_CONNECTION
