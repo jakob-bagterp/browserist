14822c0bc8bfe639972c0f2cb5cc68f9d56349b8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-07 23:16:13 +0100

    Change default directory for screenshots to the user's "downloads" folder, which should be working across Linux, Windows, Mac.

--- src/browserist/constant/directory.py
+++ src/browserist/constant/directory.py
@@ -1,3 +1,8 @@
+import os
 from pathlib import Path
 
 PROJECT_WORKING_DIR: Path = Path(__file__).parent.parent.parent.parent
+
+USER_DIR = Path.home()
+
+DOWNLOADS_DIR = os.path.join(USER_DIR, "downloads")
