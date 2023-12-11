894d00869d6bbcdc6a3520b194ef56d1ff411a11
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-11 20:53:39 +0100

    Add test of helper_download.get_absolute_file_path()

---
+++ test/helper/download/get_absolute_file_path_test.py
@@ -0,0 +1,17 @@
+import os
+
+import pytest
+
+from browserist import Browser, BrowserSettings, helper_download
+from browserist.constant.directory import DOWNLOADS_DIR
+
+TEST_FILE_NAME = "test.txt"
+
+
+@pytest.mark.parametrize("download_dir, file_name, expected_file_path", [
+    (DOWNLOADS_DIR, TEST_FILE_NAME, os.path.join(DOWNLOADS_DIR, TEST_FILE_NAME)),
+])
+def test_helper_download_get_absolute_file_path(download_dir: str, file_name: str, expected_file_path: str) -> None:
+    brower_settings = BrowserSettings(headless=True, download_dir=download_dir)
+    with Browser(brower_settings) as browser:
+        assert helper_download.get_absolute_file_path(browser._browser_driver, file_name) == expected_file_path
