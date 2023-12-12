4fffe05e5bfa1c4e607d480fe7fd2c8b7cf354d0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 09:41:28 +0100

    Minor rename variable for consistency.

--- test/helper/file/exists_2_test.py
+++ test/helper/file/exists_2_test.py
@@ -13,9 +13,9 @@ from browserist.model.type.path import FilePath
     (True, True),
 ])
 def test_helper_file_exists(has_file: bool, expected_file_exists: bool, tmpdir: local) -> None:
-    downloads_dir = os.path.join(str(tmpdir), "downloads")
-    os.mkdir(downloads_dir)
-    file_path = os.path.join(downloads_dir, "file.txt")
+    download_dir = os.path.join(str(tmpdir), "downloads")
+    os.mkdir(download_dir)
+    file_path = os.path.join(download_dir, "file.txt")
     if has_file:
         _helper.file.create(file_path)
     file_path = FilePath(file_path)
