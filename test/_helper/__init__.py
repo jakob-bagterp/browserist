52e1003d8cb8662a66c109237b1e39e845305846
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 13:07:18 +0100

    Clean-up of imports.

--- test/_helper/__init__.py
+++ test/_helper/__init__.py
@@ -1,5 +1,5 @@
 __all__: list[str] = []
 
-from .file import create  # noqa: F401
+from . import file  # noqa: F401
 from .time import get_difference  # noqa: F401
 from .tolerance import add, deduct  # noqa: F401
