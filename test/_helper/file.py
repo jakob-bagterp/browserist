f509f13a07d3239eda17e805b474e4deac117e4a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 14:19:36 +0100

    Ensure that test file is always removed.

--- test/_helper/file.py
+++ test/_helper/file.py
@@ -14,6 +14,8 @@ def download_dir_and_file_path_controller(has_file: bool, temp_dir: local) -> tu
     download_dir = os.path.join(str(temp_dir), "downloads")
     os.mkdir(download_dir)
     file_path = os.path.join(download_dir, "file.txt")
+    if os.path.exists(file_path):
+        os.remove(file_path)
     if has_file:
         create(file_path)
     return download_dir, file_path
