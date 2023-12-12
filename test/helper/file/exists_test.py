cea879372ee8df6939d10d5701cf610f15888743
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 08:38:20 +0100

    Refactor and simplify test.

--- test/helper/file/exists_test.py
+++ test/helper/file/exists_test.py
@@ -7,18 +7,16 @@ from browserist import helper
 from browserist.model.type.path import FilePath
 
 
-@pytest.mark.parametrize("directory, file_name, expected", [
-    ("downloads", "file.txt", True),
-    ("downloads", None, False),
+@pytest.mark.parametrize("has_file, expected_file_exists", [
+    (False, False),
+    (True, True),
 ])
-def test_helper_file_exists(directory: str, file_name: str | None, expected: bool, tmpdir: local) -> None:
-    base_dir = os.path.join(str(tmpdir), directory)
-    os.mkdir(base_dir)
-    if file_name:
-        file_path = os.path.join(base_dir, file_name)
+def test_helper_file_exists(has_file: bool, expected_file_exists: bool, tmpdir: local) -> None:
+    downloads_dir = os.path.join(str(tmpdir), "downloads")
+    os.mkdir(downloads_dir)
+    file_path = os.path.join(downloads_dir, "file.txt")
+    if has_file:
         with open(file_path, "w") as file:
             file.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-    else:
-        file_path = "non_existent_file_path"
     file_path = FilePath(file_path)
-    assert helper.file.exists(file_path) == expected
+    assert helper.file.exists(file_path) == expected_file_exists
