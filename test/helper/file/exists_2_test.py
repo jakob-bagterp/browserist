776b527854f1f2e9f0d02e7078f0f0cf8acacac0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 14:30:00 +0100

    Fix FliePath input type issue.

--- test/helper/file/exists_2_test.py
+++ test/helper/file/exists_2_test.py
@@ -3,6 +3,7 @@ import pytest
 from py.path import local
 
 from browserist import helper
+from browserist.model.type.path import FilePath
 
 
 @pytest.mark.parametrize("has_file, expected_file_exists", [
@@ -11,4 +12,5 @@ from browserist import helper
 ])
 def test_helper_file_exists(has_file: bool, expected_file_exists: bool, tmpdir: local) -> None:
     _, file_path = _helper.file.download_dir_and_file_path_controller(has_file, tmpdir)
+    file_path = FilePath(file_path)
     assert helper.file.exists(file_path) == expected_file_exists
