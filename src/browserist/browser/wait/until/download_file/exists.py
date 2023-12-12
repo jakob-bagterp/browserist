2fa012c4f50d545a6e834002ff7cc1f0f223d61e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-12 10:01:45 +0100

    Attempt to fix issue with retry iteration helper: "TypeError: exists() takes 1 positional argument but 2 were given"

--- src/browserist/browser/wait/until/download_file/exists.py
+++ src/browserist/browser/wait/until/download_file/exists.py
@@ -2,9 +2,13 @@ from pathlib import Path
 
 from ..... import helper, helper_download, helper_iteration
 from .....model.browser.base.driver import BrowserDriver
+from .....model.type.path import FilePath
 
 
 def wait_until_download_file_exists(browser_driver: BrowserDriver, file_name: str | Path, timeout: float) -> None:
+    def file_exists(browser_driver: BrowserDriver, file_path: FilePath) -> bool:
+        return helper.file.exists(file_path)
+
     file_path = helper_download.get_absolute_file_path(browser_driver, file_name)
     helper_iteration.retry.until_condition_is_true(
-        browser_driver, file_path, func=helper.file.exists, timeout=timeout)
+        browser_driver, file_path, func=file_exists, timeout=timeout)
