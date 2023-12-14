cd94535cc1c036cf140c002a92dfe036a9eca30f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-14 09:25:18 +0100

    WIP: Add check of entries before download to main flows.

--- src/browserist/browser/click/download.py
+++ src/browserist/browser/click/download.py
@@ -1,3 +1,4 @@
+from ... import helper
 from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
@@ -7,8 +8,12 @@ from .button import click_button_without_wait
 def click_download_button(browser_driver: BrowserDriver, xpath: str, timeout: float, await_download: bool, expected_file_name: str | None, idle_download_timeout: float) -> None:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
+    if await_download and not expected_file_name:
+        download_dir_entries_before_download = helper.directory.get_entries(browser_driver.settings._download_dir)
     click_button_without_wait(browser_driver, xpath)
     if await_download:
-        pass  # TODO: Add watcher flow for file download completion.
+        if download_dir_entries_before_download:
+            pass
+        # TODO: Add watcher flow for file download completion.
 
     # TODO: Temporary solution. To be updated.
--- src/browserist/browser/click/download_and_get_file_path.py
+++ src/browserist/browser/click/download_and_get_file_path.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from ... import helper
 from ...model.browser.base.driver import BrowserDriver
 from ...model.type.xpath import XPath
 from ..wait.for_element import wait_for_element
@@ -9,6 +10,9 @@ from .button import click_button_without_wait
 def click_download_button_and_get_file_path(browser_driver: BrowserDriver, xpath: str, timeout: float, idle_download_timeout: float) -> Path:
     xpath = XPath(xpath)
     wait_for_element(browser_driver, xpath, timeout)
+    download_dir_entries_before_download = helper.directory.get_entries(browser_driver.settings._download_dir)
     click_button_without_wait(browser_driver, xpath)
+    if download_dir_entries_before_download:
+        pass
 
     return Path(browser_driver.settings._download_dir)  # TODO: Temporary solution. To be updated.
