e9c98c8662e1c3b2554fef47dc398843290e0c6f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-14 08:58:57 +0100

    Add test of helper.directory.get_entries() method.

---
+++ test/helper/directory/get_entries_test.py
@@ -0,0 +1,19 @@
+import os
+
+import _helper
+
+from browserist import helper
+
+
+def test_helper_directory_get_entries(tmpdir) -> None:
+    NUMBER_OF_DIRECTORIES = 4
+    NUMBER_OF_FILES = 3
+    download_dir = os.path.join(str(tmpdir), "downloads")
+
+    _helper.directory.create_multiple(download_dir, NUMBER_OF_DIRECTORIES)
+    download_dir_entries = helper.directory.get_entries(download_dir)
+    assert len(download_dir_entries) == NUMBER_OF_DIRECTORIES
+
+    _helper.file.create_multiple(download_dir, "txt", NUMBER_OF_FILES)
+    download_dir_entries = helper.directory.get_entries(download_dir)
+    assert len(download_dir_entries) == NUMBER_OF_DIRECTORIES + NUMBER_OF_FILES
