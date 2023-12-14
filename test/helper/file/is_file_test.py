1e43f592a30cf32bbf9d9ebd73998de2c1a33224
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-14 08:54:54 +0100

    Minor refactor of test.

--- test/helper/file/is_file_test.py
+++ test/helper/file/is_file_test.py
@@ -11,11 +11,12 @@ def test_helper_file_is_file(tmpdir) -> None:
     download_dir = os.path.join(str(tmpdir), "downloads")
 
     _helper.directory.create_multiple(download_dir, NUMBER_OF_DIRECTORIES)
-    assert len(helper.directory.get_entries(download_dir)) == NUMBER_OF_DIRECTORIES
+    download_dir_entries = helper.directory.get_entries(download_dir)
+    assert len(download_dir_entries) == NUMBER_OF_DIRECTORIES
 
     _helper.file.create_multiple(download_dir, "txt", NUMBER_OF_FILES)
-    assert len(helper.directory.get_entries(download_dir)) == NUMBER_OF_DIRECTORIES + NUMBER_OF_FILES
-
     download_dir_entries = helper.directory.get_entries(download_dir)
+    assert len(download_dir_entries) == NUMBER_OF_DIRECTORIES + NUMBER_OF_FILES
+
     download_dir_files = [file_name for file_name in download_dir_entries if helper.file.is_file(download_dir, file_name)]
     assert len(download_dir_files) == NUMBER_OF_FILES
