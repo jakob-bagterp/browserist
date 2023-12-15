77a16adbf9d63cbfefb5b912efad2a9b0c20ac57
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-15 19:41:13 +0100

    Rename helper_download.get_absolute_file_path() to helper_download.get_file_path().

--- src/browserist/helper_download/__init__.py
+++ src/browserist/helper_download/__init__.py
@@ -7,7 +7,7 @@ from ..model.browser.base.driver import BrowserDriver
 from ..model.type.path import FilePath
 
 
-def get_absolute_file_path(browser_driver: BrowserDriver, file_name: str | Path) -> FilePath:
+def get_file_path(browser_driver: BrowserDriver, file_name: str | Path) -> FilePath:
     file_path = os.path.join(browser_driver.settings._download_dir, file_name)
     return FilePath(file_path)
 
