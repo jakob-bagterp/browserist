cc7f784dcad841e1adf819f5804304bda98271c7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-18 10:02:53 +0100

    Minor refactor of browser as parameter in helper method.

--- test/browser/wait/until/download_file/size_does_not_increase_test.py
+++ test/browser/wait/until/download_file/size_does_not_increase_test.py
@@ -15,7 +15,7 @@ FILE_NAME = "file.txt"
 def test_wait_until_download_file_size_does_not_increase_by_timing(tmpdir: local) -> None:
     """Assume that it's faster to await a non-existing file than an existing"""
 
-    def get_time_for_wait_until_download_file_size_does_not_increase() -> float:
+    def get_time_for_wait_until_download_file_size_does_not_increase(browser: Browser) -> float:
         start_time = time.perf_counter_ns()
         browser.wait.until.download_file.size_does_not_increase(FILE_NAME, idle_timeout.VERY_SHORT)
         stop_time = time.perf_counter_ns()
@@ -24,10 +24,10 @@ def test_wait_until_download_file_size_does_not_increase_by_timing(tmpdir: local
     download_dir = os.path.join(str(tmpdir), "downloads")
     brower_settings = BrowserSettings(headless=True, download_dir=download_dir)
     with Browser(brower_settings) as browser:
-        time_without_file = get_time_for_wait_until_download_file_size_does_not_increase()
+        time_without_file = get_time_for_wait_until_download_file_size_does_not_increase(browser)
         file_path = os.path.join(download_dir, FILE_NAME)
         _helper.file.create(file_path)
-        time_with_file = get_time_for_wait_until_download_file_size_does_not_increase()
+        time_with_file = get_time_for_wait_until_download_file_size_does_not_increase(browser)
         assert time_without_file < time_with_file
 
 
