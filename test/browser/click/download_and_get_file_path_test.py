64be9603f0bf86f6fa4d936414e6e03774ea51f9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-18 11:03:23 +0100

    Add test of download_and_get_file_path (thought not fully working).

---
+++ test/browser/click/download_and_get_file_path_test.py
@@ -0,0 +1,16 @@
+import os
+
+from _helper.timeout import reset_to_not_timed_out
+from _mock_data.url import internal_url
+from py.path import local
+
+from browserist import Browser, BrowserSettings
+
+
+def test_click_download_and_get_file_path(tmpdir: local) -> None:
+    download_dir = os.path.join(str(tmpdir), "downloads")
+    brower_settings = BrowserSettings(headless=True, download_dir=download_dir)
+    with Browser(brower_settings) as browser:
+        reset_to_not_timed_out(browser)
+        browser.open.url(internal_url.DOWNLOAD)
+        assert browser.click.download_and_get_file_path("//button[@id='download']") == "file.zip"
