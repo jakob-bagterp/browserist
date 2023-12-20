876248b6d268d0a831ffb4d87af069cd8f96056d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-20 23:49:39 +0100

    Update remaining DownloadHandler details for Edge.

--- src/browserist/model/download/edge.py
+++ src/browserist/model/download/edge.py
@@ -1,4 +1,4 @@
-from ... import helper, helper_download
+from ... import helper
 from .handler import DownloadHandler
 
 
@@ -9,34 +9,21 @@ class EdgeDownloadHandler(DownloadHandler):
 
     @property
     def _temporary_file_predicts_final_file(self) -> bool:
-        return True
-
-        # TODO: To be verified.
+        return False
 
     @property
     def _temporary_file_extension(self) -> str:
         return ".crdownload"
 
-        # TODO: To be verified.
-
     def _is_temporary_file(self, file_name: str) -> bool:
         """When Edge starts a download, it uses `.crdownload` as extension for temporary files.
 
-        For example, it creates the temporary file `file.zip.crdownload` until fully downloaded and then renames it to `file.zip`."""
+        For example, it creates the temporary file `Unconfirmed 123456.crdownload` (the prepend `Unconfirmed` may differ from language to language version, and therefore can't be part of the logic) until fully downloaded and then renames it to `file.zip`."""
 
         return file_name.endswith(self._temporary_file_extension) and helper.file.is_file(self._download_dir, file_name)
 
-        # TODO: To be verified.
-
     def _get_temporary_file_from_expected_file(self, expected_file_name: str) -> str:
-        return helper_download.add_file_extension(expected_file_name, self._temporary_file_extension)
-
-        # TODO: To be verified.
+        return ""  # Not supported by Edge.
 
     def _get_temporary_file_without_extension(self) -> str:
-        if self._temporary_file is not None:
-            return helper_download.remove_file_extension(self._temporary_file.name, self._temporary_file_extension)
-        else:
-            return ""
-
-        # TODO: To be verified.
+        return ""  # Not supported by Edge.
