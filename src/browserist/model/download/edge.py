25867b1e2cb294e69ea7ec7c07f6093190233ed1
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-12-30 19:10:05 +0100

    Following observation of download with Edge, it seems not to use preliminary temporary file for downloads.

--- src/browserist/model/download/edge.py
+++ src/browserist/model/download/edge.py
@@ -18,8 +18,6 @@ class EdgeDownloadHandler(DownloadHandler):
     def _is_preliminary_temporary_file(self, file_name: str) -> bool:
         return False  # Not supported by Edge.
 
-        # TODO: To be verified.
-
     def _is_temporary_file(self, file_name: str) -> bool:
         """When Edge starts a download, it uses `.crdownload` as extension for temporary files.
 
