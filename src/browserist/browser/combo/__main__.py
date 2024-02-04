3ca1b0daaa5efbc7ee73d36ae82d5070eb9b3560
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-02-04 10:40:14 +0100

    Minor update docstring.

--- src/browserist/browser/combo/__main__.py
+++ src/browserist/browser/combo/__main__.py
@@ -38,7 +38,7 @@ class ComboDriverMethods(DriverMethods):
                 browser.click.button("//xpath/to/button")
             ```
 
-            Or use succesfull handling of the cookie banner as conditional to continue by setting `return_bool` to `True` as parameter in the settings class:
+            Or use succesfull handling of the cookie banner with a conditional `if` statement by setting `return_bool` to `True` as parameter in the settings class:
 
             ```python title="" linenums="1"
             from browserist import Browser, CookieBannerSettings
