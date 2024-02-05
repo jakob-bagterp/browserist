202c4a910905043115dc19ae26adc3eabf2c8863
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-02-05 17:18:15 +0100

    Simplify parameters and variables in flow.

--- src/browserist/browser/combo/log_in.py
+++ src/browserist/browser/combo/log_in.py
@@ -1,6 +1,5 @@
 import time
 
-from ...model.browser.base.driver import BrowserDriver
 from ...model.combo_settings.login_credentials import LoginCredentials
 from ...model.combo_settings.login_form import LoginForm1Step, LoginForm2Steps
 from ...model.driver_methods import DriverMethods
@@ -13,7 +12,10 @@ from ..wait.until.url.contains import wait_until_url_contains
 
 
 def combo_log_in(driver_method: DriverMethods, login_credentials: LoginCredentials, login_form: LoginForm1Step | LoginForm2Steps, timeout: float) -> None:
-    def login_form_1_step(browser_driver: BrowserDriver, timeout_should_continue: TimeoutShouldContinueCallable, login_form: LoginForm1Step, timeout: float) -> None:
+    timeout_should_continue: TimeoutShouldContinueCallable = driver_method._timeout_should_continue
+    browser_driver = driver_method._browser_driver
+
+    def login_form_1_step(login_form: LoginForm1Step) -> None:
         if timeout_should_continue():
             input_value(browser_driver, login_form.username_input_xpath, login_credentials.username, timeout)
         if timeout_should_continue():
@@ -21,7 +23,7 @@ def combo_log_in(driver_method: DriverMethods, login_credentials: LoginCredentia
         if timeout_should_continue():
             click_button(browser_driver, login_form.submit_button_xpath, timeout)
 
-    def login_form_2_steps(browser_driver: BrowserDriver, timeout_should_continue: TimeoutShouldContinueCallable, login_form: LoginForm2Steps, timeout: float) -> None:
+    def login_form_2_steps(login_form: LoginForm2Steps) -> None:
         if timeout_should_continue():
             input_value(browser_driver, login_form.username_input_xpath, login_credentials.username, timeout)
         if timeout_should_continue():
@@ -31,16 +33,13 @@ def combo_log_in(driver_method: DriverMethods, login_credentials: LoginCredentia
         if timeout_should_continue():
             click_button(browser_driver, login_form.password_submit_button_xpath, timeout)
 
-    timeout_should_continue: TimeoutShouldContinueCallable = driver_method._timeout_should_continue
-    browser_driver = driver_method._browser_driver
-
     if login_form.url is not None and timeout_should_continue():
         open_url_if_not_current(browser_driver, login_form.url)
 
     if type(login_form) is LoginForm1Step:
-        login_form_1_step(browser_driver, timeout_should_continue, login_form, timeout)
+        login_form_1_step(login_form)
     elif type(login_form) is LoginForm2Steps:
-        login_form_2_steps(browser_driver, timeout_should_continue, login_form, timeout)
+        login_form_2_steps(login_form)
 
     if login_form.post_login_wait_seconds is not None:
         time.sleep(login_form.post_login_wait_seconds)
