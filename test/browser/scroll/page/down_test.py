8f70bcec2be708a6b2d16f337a7a5d80d70c8105
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-06 19:24:01 +0200

    Add tests of scrolling up or down multiple pages.

--- test/browser/scroll/page/down_test.py
+++ test/browser/scroll/page/down_test.py
@@ -1,15 +1,23 @@
+import pytest
 from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
 
 from browserist import Browser
 
 
-def test_scroll_page_down(browser_default_headless: Browser) -> None:
+@pytest.mark.parametrize("pages", [
+    1,
+    2,
+    3,
+    4,
+    5,
+])
+def test_scroll_page_down(pages: int, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.SCROLL_LONG_VERTICAL)
     browser.scroll.page.to_top()
     _, y_top = browser.scroll.get.position()
     y_screen_height = browser.viewport.get.height()
-    browser.scroll.page.down()
+    browser.scroll.page.down(pages)
     _, y_page_down = browser.scroll.get.position()
-    assert y_page_down == y_top + y_screen_height + 1
+    assert y_page_down == y_top + (y_screen_height + 1) * pages
--- test/browser/scroll/page/up_test.py
+++ test/browser/scroll/page/up_test.py
@@ -1,4 +1,5 @@
 import _helper
+import pytest
 from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
 
@@ -6,15 +7,22 @@ from browserist import Browser
 from browserist.helper import operating_system
 
 
-def test_scroll_page_up(browser_default_headless: Browser) -> None:
+@pytest.mark.parametrize("pages", [
+    1,
+    2,
+    3,
+    4,
+    5,
+])
+def test_scroll_page_up(pages: int, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.SCROLL_LONG_VERTICAL)
     browser.scroll.page.to_end()
     _, y_end = browser.scroll.get.position()
     y_screen_height = browser.viewport.get.height()
-    browser.scroll.page.up()
+    browser.scroll.page.up(pages)
     _, y_page_up = browser.scroll.get.position()
-    expected_exact_position = y_end - y_screen_height - 1
+    expected_exact_position = y_end - (y_screen_height - 1) * pages
     match _:  # Sometimes the scroll position is not calculated exactly on Windows nor macOS, and so we just do an approximation.
         case _ if operating_system.is_windows():
             assert y_page_up < y_end
