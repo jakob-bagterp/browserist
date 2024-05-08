6dd92d48312d6d494e84aefcaf7172e77d9669bd
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-08 20:36:08 +0200

    Update test_get_total_scroll_height with internal page.

--- test/browser/scroll/get/total_height_test.py
+++ test/browser/scroll/get/total_height_test.py
@@ -9,7 +9,7 @@ MARGIN = 0.12  # 12%
 
 @pytest.mark.parametrize("url, expected_total_scroll_height", [
     (internal_url.NOT_SCROLLABLE, 600),
-    (internal_url.W3SCHOOLS_COM, 16465),
+    (internal_url.SCROLL_LONG_VERTICAL, 40802),
 ])
 def test_get_total_scroll_height(url: str, expected_total_scroll_height: int, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
