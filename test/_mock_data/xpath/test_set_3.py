317fd4b0f4dc6e9995ac28ad9899aaabcbe699be
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-26 13:17:11 +0200

    Refactor and simplify XPaths for test sets.

--- test/_mock_data/xpath/test_set_3.py
+++ test/_mock_data/xpath/test_set_3.py
@@ -19,52 +19,42 @@ XPATH_TEST_SET_MINI_SITE_HOMEPAGE_DEFAULT = XPathTestSet(
     ]
 )
 
-VALID_XPATH_MINI_SITE_HOMEPAGE_LINK = "/html/body/section[2]/div[1]/a"
-
 XPATH_TEST_SET_MINI_SITE_HOMEPAGE_LINK = XPathTestSet(
     url=internal_url.MINI_SITE_HOMEPAGE,
     tests=[
-        XPathExpectation(VALID_XPATH_MINI_SITE_HOMEPAGE_LINK, does_not_raise()),
+        XPathExpectation("/html/body/section[2]/div[1]/a", does_not_raise()),
         XPathExpectation(INVALID_XPATH, pytest.raises(XPathSyntaxError)),
     ]
 )
-
-VALID_XPATH_MINI_SITE_FEATURE_1_IMAGE = "//*[@id='main']/img[1]"
 
 XPATH_TEST_SET_MINI_SITE_FEATURE_1_IMAGE = XPathTestSet(
     url=internal_url.MINI_SITE_FEATURE_1,
     tests=[
-        XPathExpectation(VALID_XPATH_MINI_SITE_FEATURE_1_IMAGE, does_not_raise()),
+        XPathExpectation("//*[@id='main']/img[1]", does_not_raise()),
         XPathExpectation(INVALID_XPATH, pytest.raises(XPathSyntaxError)),
     ]
 )
-
-VALID_XPATH_MINI_SITE_HOMEPAGE_HEADLINE = "/html/body/section[1]/div/h1"
 
 XPATH_TEST_SET_W3SCHOOLS_COM_HEADLINE = XPathTestSet(
     url=internal_url.MINI_SITE_HOMEPAGE,
     tests=[
-        XPathExpectation(VALID_XPATH_MINI_SITE_HOMEPAGE_HEADLINE, does_not_raise()),
+        XPathExpectation("/html/body/section[1]/div/h1", does_not_raise()),
         XPathExpectation(INVALID_XPATH, pytest.raises(XPathSyntaxError)),
     ]
 )
-
-VALID_XPATH_COOKIE_BANNER_IFRAME = "//iframe[@id='cookie-banner-iframe']"
 
 XPATH_TEST_SET_COOKIE_BANNER_IFRAME = XPathTestSet(
     url=internal_url.COOKIE_BANNER_WITH_IFRAME,
     tests=[
-        XPathExpectation(VALID_XPATH_COOKIE_BANNER_IFRAME, does_not_raise()),
+        XPathExpectation("//iframe[@id='cookie-banner-iframe']", does_not_raise()),
         XPathExpectation(INVALID_XPATH, pytest.raises(XPathSyntaxError)),
     ]
 )
 
-VALID_XPATH_W3SCHOOLS_COM_INPUT = "//*[@id='search2']"
-
 XPATH_TEST_SET_W3SCHOOLS_COM_INPUT = XPathTestSet(
     url=internal_url.W3SCHOOLS_COM,
     tests=[
-        XPathExpectation(VALID_XPATH_W3SCHOOLS_COM_INPUT, does_not_raise()),
+        XPathExpectation("//*[@id='search2']", does_not_raise()),
         XPathExpectation(INVALID_XPATH, pytest.raises(XPathSyntaxError)),
     ]
 )
