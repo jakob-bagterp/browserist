de2dd599800a6df91b74541eed8115d70666bfc0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-26 18:45:26 +0200

    Fix issue in test_get_dimensions and refactor to use internal mini site.

--- test/browser/get/dimensions_1_test.py
+++ test/browser/get/dimensions_1_test.py
@@ -6,12 +6,13 @@ from browserist import Browser
 
 
 @pytest.mark.parametrize("url, xpath, expected_width, expected_height", [
-    (internal_url.EXAMPLE_COM, "/html/body/div", 600, 167),
-    (internal_url.EXAMPLE_COM, "/html/body/div/h1", 600, 38),
-    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[7]/div/div[1]", 444, 265),
-    (internal_url.W3SCHOOLS_COM, "//*[@id='main']/div[8]/div/div[1]", 192, 113),
+    (internal_url.MINI_SITE_HOMEPAGE, "//section[@class='hero hero__background-image__office-ideas-on-board']", 784, 400),
+    (internal_url.MINI_SITE_HOMEPAGE, "//div[@class='hero__text-box']", 340, 160),
+    (internal_url.MINI_SITE_FEATURE_1, "//*[@id='main']/p[1]", 600, 185),
+    (internal_url.MINI_SITE_FEATURE_1, "//*[@id='main']/img", 600, 400),
 ])
 def test_get_dimensions(url: str, xpath: str, expected_width: int, expected_height: int, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(url)
-    assert expected_width, expected_height == browser.get.dimensions(xpath)
+    measured_width, measured_height = browser.get.dimensions(xpath)
+    assert expected_width == measured_width and expected_height == measured_height
