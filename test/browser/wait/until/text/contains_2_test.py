0cabf8da5c8f379cd5a02152ea1e9f3b3f6a3dee
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-30 07:31:25 +0200

    Refactor various tests to use new pages of internal mini site.

--- test/browser/wait/until/text/contains_2_test.py
+++ test/browser/wait/until/text/contains_2_test.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import pytest
 from _helper.timeout import reset_to_not_timed_out
+from _mock_data import does_not_exist
 from _mock_data.url import internal_url
 
 from browserist import Browser
@@ -12,15 +13,15 @@ from browserist.exception.timeout import WaitForElementTimeoutException
 
 
 @pytest.mark.parametrize("xpath, regex, expectation", [
-    ("/html/body/div/p[2]/a", "More information...", does_not_raise()),
-    ("/html/body/div/p[2]/a", "mOrE iNfoRmaTion...", does_not_raise()),
-    ("/html/body/div/p[2]/a", "information", does_not_raise()),
-    ("/html/body/div/p[2]/a", r"^information", pytest.raises(RetryTimeoutException)),
-    ("/html/body/div/p[2]/a", "no valid text", pytest.raises(RetryTimeoutException)),
-    ("/html/body/div/p[2]/a/div", "element doesn't exist", pytest.raises(WaitForElementTimeoutException)),
+    ("/html/body/section[2]/div[1]/a", "Learn more", does_not_raise()),
+    ("/html/body/section[2]/div[1]/a", "LeARn mOrE", pytest.raises(RetryTimeoutException)),
+    ("/html/body/section[2]/div[1]/a", "more", pytest.raises(RetryTimeoutException)),
+    ("/html/body/section[2]/div[1]/a", r"^more", pytest.raises(RetryTimeoutException)),
+    ("/html/body/section[2]/div[1]/a", "no valid text", pytest.raises(RetryTimeoutException)),
+    (does_not_exist.XPATH, "element doesn't exist", pytest.raises(WaitForElementTimeoutException)),
 ])
 def test_wait_until_text_contains(xpath: str, regex: str, expectation: Any, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     with expectation:
-        browser.open.url(internal_url.EXAMPLE_COM)
+        browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
         _ = browser.wait.until.text.contains(xpath, regex, timeout.VERY_SHORT) is not None
