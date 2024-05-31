f57c1d98bdc50cc34ef52c1452f64086197c56c4
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-31 10:13:01 +0200

    Minor correction of underscore.

--- test/helper/regex/is_value_valid_test.py
+++ test/helper/regex/is_value_valid_test.py
@@ -4,10 +4,10 @@ from browserist import helper
 
 
 @pytest.mark.parametrize("value, regex, expected", [
-    ("is_valid", "is_valid", True),
-    ("is_valid", r"is_valid", True),
+    ("is valid", "is valid", True),
+    ("is valid", r"is valid", True),
     ("123456789", r"\d", True),
-    ("is_not_valid", "no_match_regex", False),
+    ("is not valid", "no match regex", False),
 ])
 def test_is_value_valid(value: str, regex: str, expected: bool) -> None:
     assert helper.regex.is_value_valid(value, regex) is expected
