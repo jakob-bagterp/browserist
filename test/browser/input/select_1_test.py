3739c0f1e17704d0a1022a85d229066670358dd0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-31 08:17:51 +0200

    Refactor test_select_input_field() to use parameters.

--- test/browser/input/select_1_test.py
+++ test/browser/input/select_1_test.py
@@ -1,17 +1,18 @@
-from _config.browser_settings import default
+import pytest
+from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
-from _mock_data.xpath.drop_down_seletor import DROPDOWN_OPTIONS_XPATH, OPTION_2_XPATH, OPTION_3_XPATH, OPTION_4_XPATH
+from _mock_data.xpath.drop_down_seletor import OPTION_1_XPATH, OPTION_2_XPATH, OPTION_3_XPATH, OPTION_4_XPATH
 
 from browserist import Browser
 
 
-def test_select_input_field() -> None:
-    with Browser(default.DEFAULT) as browser:
-        browser.open.url(internal_url.DROPDOWN_SELECTOR)
-        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option1"
-        browser.input.select(OPTION_2_XPATH)
-        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option2"
-        browser.input.select(OPTION_3_XPATH)
-        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option3"
-        browser.input.select(OPTION_4_XPATH)
-        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option4"
+@pytest.mark.parametrize("xpath, expected_value", [
+    (OPTION_1_XPATH, "option1"),
+    (OPTION_2_XPATH, "option2"),
+    (OPTION_3_XPATH, "option3"),
+    (OPTION_4_XPATH, "option4"),
+])
+def test_select_input_field(xpath: str, expected_value: str, browser_default_headless: Browser) -> None:
+    browser = reset_to_not_timed_out(browser_default_headless)
+    browser.open.url(internal_url.DROPDOWN_SELECTOR)
+    assert browser.get.attribute.value(xpath, "value") == expected_value
