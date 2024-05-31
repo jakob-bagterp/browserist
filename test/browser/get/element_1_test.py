c73f5cc22ead37f41a0cb57d83134a2f81bbea33
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-31 09:52:23 +0200

    Minor refactor XPath for test_get_element().

--- test/browser/get/element_1_test.py
+++ test/browser/get/element_1_test.py
@@ -1,6 +1,7 @@
 import pytest
 from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
+from _mock_data.xpath.mini_site.feature_1 import MINI_SITE_FEATURE_1_IMAGE_1_XPATH
 from _mock_data.xpath.mini_site.homepage import MINI_SITE_HOMEPAGE_HEADLINE_H1_XPATH
 
 from browserist import Browser
@@ -8,7 +9,7 @@ from browserist import Browser
 
 @pytest.mark.parametrize("url, xpath, expected", [
     (internal_url.MINI_SITE_HOMEPAGE, MINI_SITE_HOMEPAGE_HEADLINE_H1_XPATH, True),
-    (internal_url.MINI_SITE_FEATURE_1, "//div[@id='main']//img[1]", True),
+    (internal_url.MINI_SITE_FEATURE_1, MINI_SITE_FEATURE_1_IMAGE_1_XPATH, True),
 ])
 def test_get_element(url: str, xpath: str, expected: bool, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
