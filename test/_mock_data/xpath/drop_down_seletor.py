abdca3c32e0eb25f0635bebc1e8aeb651aa57905
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-05-31 08:07:43 +0200

    Refactor XPath constants for test_select_input_field().

---
+++ test/_mock_data/xpath/drop_down_seletor.py
@@ -0,0 +1,9 @@
+DROPDOWN_OPTIONS_XPATH = "//select[@id='options']"
+
+OPTION_1_XPATH = f"{DROPDOWN_OPTIONS_XPATH}/option[@value='option1']"
+
+OPTION_2_XPATH = f"{DROPDOWN_OPTIONS_XPATH}/option[@value='option2']"
+
+OPTION_3_XPATH = f"{DROPDOWN_OPTIONS_XPATH}/option[@value='option3']"
+
+OPTION_4_XPATH = f"{DROPDOWN_OPTIONS_XPATH}/option[@value='option4']"
--- test/browser/input/select_1_test.py
+++ test/browser/input/select_1_test.py
@@ -1,6 +1,6 @@
 from _config.browser_settings import default
 from _mock_data.url import internal_url
-from _mock_data.xpath import xpath
+from _mock_data.xpath.drop_down_seletor import DROPDOWN_OPTIONS_XPATH, OPTION_2_XPATH, OPTION_3_XPATH, OPTION_4_XPATH
 
 from browserist import Browser
 
@@ -8,10 +8,10 @@ from browserist import Browser
 def test_select_input_field() -> None:
     with Browser(default.DEFAULT) as browser:
         browser.open.url(internal_url.DROPDOWN_SELECTOR)
-        assert browser.get.attribute.value(xpath.DropDownSeletor.DROPDOWN_OPTIONS, "value") == "option1"
-        browser.input.select(xpath.DropDownSeletor.OPTION_2)
-        assert browser.get.attribute.value(xpath.DropDownSeletor.DROPDOWN_OPTIONS, "value") == "option2"
-        browser.input.select(xpath.DropDownSeletor.OPTION_3)
-        assert browser.get.attribute.value(xpath.DropDownSeletor.DROPDOWN_OPTIONS, "value") == "option3"
-        browser.input.select(xpath.DropDownSeletor.OPTION_4)
-        assert browser.get.attribute.value(xpath.DropDownSeletor.DROPDOWN_OPTIONS, "value") == "option4"
+        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option1"
+        browser.input.select(OPTION_2_XPATH)
+        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option2"
+        browser.input.select(OPTION_3_XPATH)
+        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option3"
+        browser.input.select(OPTION_4_XPATH)
+        assert browser.get.attribute.value(DROPDOWN_OPTIONS_XPATH, "value") == "option4"
