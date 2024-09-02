08d845deb4c7033a7e939fec20e0bebbfad05f95
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-02 23:44:48 +0200

    Removed URL type conversion from tests.

--- test/helper/url/compile_comparison_to_regex_pattern_test.py
+++ test/helper/url/compile_comparison_to_regex_pattern_test.py
@@ -1,7 +1,6 @@
 import pytest
 
 from browserist import helper
-from browserist.model.type.url import URL
 
 IGNORE_TRAILING_SLASH_DEFAULT = True
 IGNORE_PARAMETERS_DEFAULT = False
@@ -15,7 +14,6 @@ IGNORE_HTTPS_DEFAULT = False
     ("https://example.com?page=1", "https://example.com/?page=1", True),
 ])
 def test_helper_url_compile_comparison_to_regex_pattern_default(url: str, url_comparison: str, expected: bool) -> None:
-    url = URL(url)
     url_pattern = helper.url.compile_comparison_to_regex_pattern(url, IGNORE_TRAILING_SLASH_DEFAULT, IGNORE_PARAMETERS_DEFAULT, IGNORE_HTTPS_DEFAULT)
     assert bool(url_pattern.match(url_comparison)) is expected
 
@@ -29,6 +27,5 @@ def test_helper_url_compile_comparison_to_regex_pattern_default(url: str, url_co
     ("https://example.com", "https://example.com", False, True),
 ])
 def test_helper_url_compile_comparison_to_regex_pattern_ignore_https(url: str, url_comparison: str, ignore_https: bool, expected: bool) -> None:
-    url = URL(url)
     url_pattern = helper.url.compile_comparison_to_regex_pattern(url, IGNORE_TRAILING_SLASH_DEFAULT, IGNORE_PARAMETERS_DEFAULT, ignore_https)
     assert bool(url_pattern.match(url_comparison)) is expected
