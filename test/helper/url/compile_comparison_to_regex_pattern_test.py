f2aa09a63d608e2565e6ecf82214ba9a79c0dfab
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-02 23:53:13 +0200

    Added tests.

--- test/helper/url/compile_comparison_to_regex_pattern_test.py
+++ test/helper/url/compile_comparison_to_regex_pattern_test.py
@@ -18,6 +18,21 @@ def test_helper_url_compile_comparison_to_regex_pattern_default(url: str, url_co
     assert bool(url_pattern.fullmatch(url_comparison)) is expected
 
 
+@pytest.mark.parametrize("url, url_comparison, ignore_trailing_slash, expected", [
+    ("https://example.com", "https://example.com/", True, True),
+    ("https://example.com/", "https://example.com/", True, True),
+    ("https://example.com", "https://example.com/", False, False),
+    ("https://example.com/", "https://example.com/", False, True),
+    ("https://example.com?page=1", "https://example.com?page=1", True, True),
+    ("https://example.com?page=1", "https://example.com?page=1", False, True),
+    ("https://example.com?page=1", "https://example.com/?page=1", True, False),
+    ("https://example.com?page=1", "https://example.com/?page=1", False, False),
+])
+def test_helper_url_compile_comparison_to_regex_pattern_ignore_trailing_slash(url: str, url_comparison: str, ignore_trailing_slash: bool, expected: bool) -> None:
+    url_pattern = helper.url.compile_comparison_to_regex_pattern(url, ignore_trailing_slash, IGNORE_PARAMETERS_DEFAULT, IGNORE_HTTPS_DEFAULT)
+    assert bool(url_pattern.fullmatch(url_comparison)) is expected
+
+
 @pytest.mark.parametrize("url, url_comparison, ignore_https, expected", [
     ("http://example.com", "https://example.com", True, True),
     ("http://example.com", "https://example.com", False, False),
