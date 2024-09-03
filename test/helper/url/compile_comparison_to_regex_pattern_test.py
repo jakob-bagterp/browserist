d6c0576611db72add61eeb69be0a2cf80c81de85
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 15:49:49 +0200

    Refactored URLs into constants.

--- test/helper/url/compile_comparison_to_regex_pattern_test.py
+++ test/helper/url/compile_comparison_to_regex_pattern_test.py
@@ -33,13 +33,21 @@ def test_helper_url_compile_comparison_to_regex_pattern_ignore_trailing_slash(ur
     assert bool(url_pattern.fullmatch(url_comparison)) is expected
 
 
+URL_WITHOUT_HTTPS = "http://example.com"
+URL_WITHOUT_HTTPS_TRAILING_SLASH = f"{URL_WITHOUT_HTTPS}/"
+URL_WITH_HTTPS = "https://example.com"
+URL_WITH_HTTPS_TRAILING_SLASH = f"{URL_WITH_HTTPS}/"
+
+
 @pytest.mark.parametrize("url, url_comparison, ignore_https, expected", [
-    ("http://example.com", "https://example.com", True, True),
-    ("http://example.com", "https://example.com", False, False),
-    ("http://example.com/", "https://example.com", True, True),
-    ("http://example.com/", "https://example.com", False, False),
-    ("https://example.com", "https://example.com", True, True),
-    ("https://example.com", "https://example.com", False, True),
+    (URL_WITHOUT_HTTPS, URL_WITH_HTTPS, True, True),
+    (URL_WITHOUT_HTTPS, URL_WITH_HTTPS, False, False),
+    (URL_WITHOUT_HTTPS_TRAILING_SLASH, URL_WITH_HTTPS, True, True),
+    (URL_WITHOUT_HTTPS_TRAILING_SLASH, URL_WITH_HTTPS, False, False),
+    (URL_WITH_HTTPS, URL_WITH_HTTPS, True, True),
+    (URL_WITH_HTTPS, URL_WITH_HTTPS, False, True),
+    (URL_WITH_HTTPS_TRAILING_SLASH, URL_WITH_HTTPS, True, True),
+    (URL_WITH_HTTPS_TRAILING_SLASH, URL_WITH_HTTPS, False, True),
 ])
 def test_helper_url_compile_comparison_to_regex_pattern_ignore_https(url: str, url_comparison: str, ignore_https: bool, expected: bool) -> None:
     url_pattern = helper.url.compile_comparison_to_regex_pattern(url, IGNORE_TRAILING_SLASH_DEFAULT, IGNORE_PARAMETERS_DEFAULT, ignore_https)
