4f3574993778b0d88417e72eb94d3f793b7f3897
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 17:32:56 +0200

    Added tests.

--- test/helper/url/split_url_and_parameters_test.py
+++ test/helper/url/split_url_and_parameters_test.py
@@ -11,6 +11,8 @@ from browserist import helper
     ("https://example.com/?page=1", "https://example.com/", "?page=1"),
     ("https://example.com/?page=1&test=True", "https://example.com/", "?page=1&test=True"),
     ("https://example.com/?page=1?test=True", "https://example.com/", "?page=1?test=True"),
+    ("https://example.com/search?query=test&page=1", "https://example.com/search", "?query=test&page=1"),
+    ("https://example.com/search/?query=test&page=1", "https://example.com/search/", "?query=test&page=1"),
 ])
 def test_helper_url_split_url_and_parameters(url: str, expected_url: str, expected_parameters: str) -> None:
     url, parameters = helper.url.split_url_and_parameters(url)
