fcc8f24e1a0ce2e5b6e95d3865cc16293b2f8b67
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 23:08:27 +0200

    Fixed tests for test_set_viewport_by_device_headless() on Mac by adding tolerances to checks.

--- test/browser/viewport/set/size_by_device_headless_test.py
+++ test/browser/viewport/set/size_by_device_headless_test.py
@@ -1,8 +1,10 @@
+import _helper
 import pytest
 from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
 
 from browserist import Browser, DeviceViewportSize, common_devices
+from browserist.helper import operating_system
 
 
 @pytest.mark.parametrize("device", [
@@ -24,5 +26,13 @@ def test_set_viewport_by_device_headless(device: DeviceViewportSize, browser_def
     browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
     browser.viewport.set.size_by_device(device)
     width_check, height_check = browser.viewport.get.size()
-    assert device.width == width_check
-    assert device.height == height_check
+    if operating_system.is_mac_os():
+        minimum_width = _helper.tolerance.deduct(device.width, 50)
+        maximum_width = _helper.tolerance.add(device.width, 70)
+        assert minimum_width < width_check < maximum_width
+        minimum_height = _helper.tolerance.deduct(device.height, 50)
+        maximum_height = _helper.tolerance.add(device.height, 50)
+        assert minimum_height < height_check < maximum_height
+    else:
+        assert device.width == width_check
+        assert device.height == height_check
