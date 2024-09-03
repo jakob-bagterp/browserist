575674fc6334e901af0dedb0901e2d54a27a8097
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 16:10:40 +0200

    Fixed tests for test_open_url_if_not_current_ignore_https().

--- test/browser/open/url_if_not_current_test.py
+++ test/browser/open/url_if_not_current_test.py
@@ -53,20 +53,26 @@ def test_open_url_if_not_current_ignore_parameters(url1: str, url2: str, ignore_
     assert (get_url1 == get_url2) is expected
 
 
-@pytest.mark.parametrize("url1, url2, ignore_https, expected", [
-    ("http://example.com", "http://example.com", True, True),
-    ("http://example.com", "https://example.com", True, True),
-    ("http://example.com", "http://example.com", False, True),
-    ("http://example.com", "https://example.com", False, False),
-    ("https://example.com", "http://example.com", True, True),
-    ("https://example.com", "https://example.com", True, True),
-    ("https://example.com", "http://example.com", False, False),
-    ("https://example.com", "https://example.com", False, True),
+URL_WITHOUT_HTTPS = "http://httpbin.org/"
+URL_WITH_HTTPS = "https://httpbin.org/"
+
+
+@pytest.mark.parametrize("url1, url2, ignore_https, expected_url", [
+    (URL_WITHOUT_HTTPS, URL_WITHOUT_HTTPS, True, URL_WITHOUT_HTTPS),
+    (URL_WITHOUT_HTTPS, URL_WITH_HTTPS, True, URL_WITHOUT_HTTPS),
+    (URL_WITHOUT_HTTPS, URL_WITHOUT_HTTPS, False, URL_WITHOUT_HTTPS),
+    (URL_WITHOUT_HTTPS, URL_WITH_HTTPS, False, URL_WITH_HTTPS),
+    (URL_WITH_HTTPS, URL_WITHOUT_HTTPS, True, URL_WITH_HTTPS),
+    (URL_WITH_HTTPS, URL_WITH_HTTPS, True, URL_WITH_HTTPS),
+    (URL_WITH_HTTPS, URL_WITHOUT_HTTPS, False, URL_WITHOUT_HTTPS),
+    (URL_WITH_HTTPS, URL_WITH_HTTPS, False, URL_WITH_HTTPS),
 ])
-def test_open_url_if_not_current_ignore_https(url1: str, url2: str, ignore_https: bool, expected: bool, browser_default_headless: Browser) -> None:
+def test_open_url_if_not_current_ignore_https(url1: str, url2: str, ignore_https: bool, expected_url: str, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
-    browser.open.url_if_not_current(url1)
+    browser.open.url(url1)
+    browser.wait.until.url.equals(url1, timeout=20)
     get_url1 = browser.get.url.current()
+    assert get_url1 == url1
     browser.open.url_if_not_current(url2, ignore_https=ignore_https)
     get_url2 = browser.get.url.current()
-    assert (get_url1 == get_url2) is expected
+    assert get_url2 == expected_url
