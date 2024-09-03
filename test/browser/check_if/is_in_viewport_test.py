921134140dd21e16e1c9408fae170c7d49fd4316
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 22:51:04 +0200

    Revert "Fixed issues in test_check_if_is_in_viewport() by using different page with longer scroll: "AssertionError: assert True is False +  where True = is_in_viewport('//footer')""
    This reverts commit 31525175c4ccb278c0e096610c319bdba628376a.

--- test/browser/check_if/is_in_viewport_test.py
+++ test/browser/check_if/is_in_viewport_test.py
@@ -2,19 +2,18 @@ import pytest
 from _helper.timeout import reset_to_not_timed_out
 from _mock_data import does_not_exist
 from _mock_data.url import internal_url
-from _mock_data.xpath.mini_site.feature_3 import MINI_SITE_FEATURE_3_HEADLINE_H1_XPATH
+from _mock_data.xpath.mini_site.homepage import MINI_SITE_HOMEPAGE_HEADLINE_H1_XPATH
 
 from browserist import Browser
 
 
 @pytest.mark.parametrize("xpath, expected", [
-    (MINI_SITE_FEATURE_3_HEADLINE_H1_XPATH, True),
-    ("//header", True),
+    (MINI_SITE_HOMEPAGE_HEADLINE_H1_XPATH, True),
     ("//footer", False),
     ("//*[@name=\"can't handle mix of single and double quotes\"]", False),
     (does_not_exist.XPATH, False),
 ])
 def test_check_if_is_in_viewport(xpath: str, expected: bool, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
-    browser.open.url(internal_url.MINI_SITE_FEATURE_3)
+    browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
     assert browser.check_if.is_in_viewport(xpath) is expected
