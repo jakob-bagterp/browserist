bc1bfb639901a07dd200f570320d54095460b088
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-09-03 16:38:04 +0200

    Fixed and refactored tests for test_open_url_if_not_current_ignore_trailing_slash().

--- test/browser/open/url_if_not_current_test.py
+++ test/browser/open/url_if_not_current_test.py
@@ -23,19 +23,23 @@ def test_open_url_if_not_current(url1: str, url2: str, browser_default_headless:
     assert get_url1 != get_url2
 
 
-@pytest.mark.parametrize("url1, url2, ignore_trailing_slash, expected", [
-    (internal_url.MINI_SITE_HOMEPAGE, internal_url.MINI_SITE_HOMEPAGE, True, True),
-    (internal_url.MINI_SITE_HOMEPAGE, internal_url.MINI_SITE_HOMEPAGE, False, False),
-    (internal_url.MINI_SITE_HOMEPAGE, f"{internal_url.MINI_SITE_HOMEPAGE}/", True, True),
-    (internal_url.MINI_SITE_HOMEPAGE, f"{internal_url.MINI_SITE_HOMEPAGE}/", False, False),
+MINI_SITE_HOMEPAGE_WITH_TRAILING_SLASH = f"{internal_url.MINI_SITE_HOMEPAGE}/"
+
+
+@pytest.mark.parametrize("url1, url2, ignore_trailing_slash, expected_url", [
+    (internal_url.MINI_SITE_HOMEPAGE, internal_url.MINI_SITE_HOMEPAGE, True, internal_url.MINI_SITE_HOMEPAGE),
+    (internal_url.MINI_SITE_HOMEPAGE, internal_url.MINI_SITE_HOMEPAGE, False, internal_url.MINI_SITE_HOMEPAGE),
+    (internal_url.MINI_SITE_HOMEPAGE, MINI_SITE_HOMEPAGE_WITH_TRAILING_SLASH, True, internal_url.MINI_SITE_HOMEPAGE),
+    (internal_url.MINI_SITE_HOMEPAGE, MINI_SITE_HOMEPAGE_WITH_TRAILING_SLASH, False, MINI_SITE_HOMEPAGE_WITH_TRAILING_SLASH),
 ])
-def test_open_url_if_not_current_ignore_trailing_slash(url1: str, url2: str, ignore_trailing_slash: bool, expected: bool, browser_default_headless: Browser) -> None:
+def test_open_url_if_not_current_ignore_trailing_slash(url1: str, url2: str, ignore_trailing_slash: bool, expected_url: str, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
-    browser.open.url_if_not_current(url1)
+    browser.open.url(url1)
     get_url1 = browser.get.url.current()
+    assert get_url1 == url1
     browser.open.url_if_not_current(url2, ignore_trailing_slash=ignore_trailing_slash)
     get_url2 = browser.get.url.current()
-    assert (get_url1 == get_url2) is expected
+    assert get_url2 == expected_url
 
 
 MINI_SITE_HOMEPAGE_WITH_PARAMETERS = f"{internal_url.MINI_SITE_HOMEPAGE}?foo=bar"
