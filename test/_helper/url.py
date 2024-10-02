2b4b7c9fb02d939ea8f7fb8324473a6d4a34c5fe
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-02 08:24:08 +0200

    Fixed issues for failed tests on Windows: "OSError: [Errno 22] Invalid argument: '/D:/a/browserist/browserist/test/_mock_data/web/mini_site/about.html'"

--- test/_helper/url.py
+++ test/_helper/url.py
@@ -1,2 +1,5 @@
-def convert_internal_url_to_file_path(url: str) -> str:
-    return url.replace("file://", "")
+from pathlib import Path
+
+
+def convert_internal_url_to_file_path(url: str) -> Path:
+    return Path(url.replace("file://", ""))
