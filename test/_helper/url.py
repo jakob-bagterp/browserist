5ee9f9913635f76d878a831ae268879d64a24c72
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-02 22:17:31 +0200

    Revert "Another attempted fix of Windows issue: "OSError: [Errno 22] Invalid argument: '\\D:\\a\\browserist\\browserist\\test\\_mock_data\\web\\mini_site\\homepage.html'""
    This reverts commit f48cb24ac217c26c96cfc458b379fd14ff3d986b.

--- test/_helper/url.py
+++ test/_helper/url.py
@@ -3,4 +3,5 @@ from pathlib import Path
 
 def convert_internal_url_to_file_path(url: str) -> Path:
     url_as_file_path = url.replace("file://", "", 1)
-    return Path(rf"{url_as_file_path}")
+    root_dir = Path(__file__).parent.parent.parent.parent
+    return Path.joinpath(root_dir, url_as_file_path)
