e08fc173173c34e85701cb8c7c706af7be18edad
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-02 23:27:38 +0200

    Another attempted fix of Windows issue: "OSError: [Errno 22] Invalid argument: 'D:\\D:\\a\\browserist\\browserist\\test\\_mock_data\\web\\mini_site\\homepage.html'"

--- test/_helper/url.py
+++ test/_helper/url.py
@@ -1,7 +1,9 @@
-from pathlib import Path
+from browserist.helper import operating_system
 
 
-def convert_internal_url_to_file_path(url: str) -> Path:
+def convert_internal_url_to_file_path(url: str) -> str:
     url_as_file_path = url.replace("file://", "", 1)
-    root_dir = Path(__file__).parent.parent.parent.resolve()
-    return Path.joinpath(root_dir, url_as_file_path)
+    if operating_system.is_windows():
+        url_as_file_path = url_as_file_path.replace("/", "\\")
+        url_as_file_path = url_as_file_path.replace("\\", "", 1)
+    return url_as_file_path
