3d6795161ee23c1bd7cd8c612a0dc3f91dce3fb3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-02 04:05:25 +0200

    Updated flow in test.

--- test/browser/get/page_source_test.py
+++ test/browser/get/page_source_test.py
@@ -13,8 +13,9 @@ from browserist import Browser
 ])
 def test_get_page_source(url: str, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
-    with open(_helper.url.convert_internal_url_to_file_path(url)) as file:
-        expected_page_source = file.read().strip()
     browser.open.url(url)
-    loaded_page_source = browser.get.page_source().strip()
+    loaded_page_source = browser.get.page_source()
+    url_as_file_path = _helper.url.convert_internal_url_to_file_path(url)
+    with open(url_as_file_path) as file:
+        expected_page_source = file.read()
     assert loaded_page_source == expected_page_source
