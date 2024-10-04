b53456887380fdeeb7c4f40a00fa8df37ed43eeb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-04 22:35:28 +0200

    Updated tests that cover exceptions as well.

--- test/browser/user_agent/set_1_test.py
+++ test/browser/user_agent/set_1_test.py
@@ -1,13 +1,12 @@
+from contextlib import nullcontext as does_not_raise
+from typing import Any
+
 import pytest
 from _constant.user_agent import DEFAULT_USER_AGENT_SNIPPETS
-from _helper.timeout import reset_to_not_timed_out
 from _mock_data.url import internal_url
 
-from browserist import Browser, BrowserSettings
-
-# Test initiation of Browser with BrowserSettings
-# Test custom user agent on the fly
-# TEST VArious browsers + evetual not support errors
+from browserist import Browser, BrowserSettings, BrowserType
+from browserist.exception.user_agent import ChangeUserAgentOnTheFlyNotSupportedException
 
 
 @pytest.mark.parametrize("user_agent", [
@@ -21,14 +20,17 @@ def test_user_agent_set_on_initiation(user_agent: str) -> None:
         assert user_agent_checked == user_agent
 
 
-@pytest.mark.parametrize("user_agent", [
-    ("test"),
+@pytest.mark.parametrize("browser_settings, user_agent, expectation", [
+    (BrowserSettings(type=BrowserType.CHROME, headless=True), "test", does_not_raise()),
+    (BrowserSettings(type=BrowserType.EDGE, headless=True), "test", does_not_raise()),
+    (BrowserSettings(type=BrowserType.FIREFOX, headless=True), "test", pytest.raises(ChangeUserAgentOnTheFlyNotSupportedException)),
 ])
-def test_user_agent_set_on_the_fly(user_agent: str, browser_default_headless_scope_function: Browser) -> None:
-    browser = reset_to_not_timed_out(browser_default_headless_scope_function)  # Use and discard the browser, so it doesn't pollute other tests.
-    browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
-    user_agent_default = browser.user_agent.get()
-    assert any(expected_user_agent_snippet in user_agent_default for expected_user_agent_snippet in DEFAULT_USER_AGENT_SNIPPETS)
-    browser.user_agent.set(user_agent)
-    user_agent_custom = browser.user_agent.get()
-    assert user_agent_custom == user_agent
+def test_user_agent_set_on_the_fly(browser_settings: BrowserSettings, user_agent: str, expectation: Any) -> None:
+    with Browser(browser_settings) as browser:
+        browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
+        user_agent_default = browser.user_agent.get()
+        assert any(expected_user_agent_snippet in user_agent_default for expected_user_agent_snippet in DEFAULT_USER_AGENT_SNIPPETS)
+        with expectation:
+            browser.user_agent.set(user_agent)
+            user_agent_custom = browser.user_agent.get()
+            assert user_agent_custom == user_agent
