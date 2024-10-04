7089c354c7ed9166c6c7f9445e93e3a2be06a61e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-04 07:44:10 +0200

    Remove language from user agent model since we aren't going to use it.

--- src/browserist/model/user_agent.py
+++ src/browserist/model/user_agent.py
@@ -8,9 +8,7 @@ class UserAgentSettings:
     Args:
         user_agent (str | None, optional): The user agent to set as `User-Agent` in the request header. If `None`, the default or current user agent is used.
         platform (str | None, optional): The platform to set as `Platform` in the request header. If `None`, the default or current platform is used.
-        accept_language (str | None, optional): The language to set as `Accept-Language` in the request header. If `None`, the default or current language is used.
     """
 
     user_agent: str | None = None
     platform: str | None = None
-    accept_language: str | None = None
