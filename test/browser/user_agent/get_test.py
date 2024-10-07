0d70def4214c719974d06827c47f89d4781f173d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-07 07:34:24 +0200

    Updated test assertion.

--- test/browser/user_agent/get_test.py
+++ test/browser/user_agent/get_test.py
@@ -9,4 +9,5 @@ def test_user_agent_get(browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.MINI_SITE_HOMEPAGE)
     user_agent = browser.user_agent.get()
+    assert user_agent is not None
     assert any(expected_user_agent_snippet in user_agent for expected_user_agent_snippet in DEFAULT_USER_AGENT_SNIPPETS)
