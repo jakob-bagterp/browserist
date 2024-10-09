bf7456369f6ea76899685b2e261d1907c23cf87d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-09 22:01:45 +0200

    Another attempt to fix connection issues.

--- test/_fixture/internet_connection.py
+++ test/_fixture/internet_connection.py
@@ -1,5 +1,6 @@
 import socket
 from collections.abc import Generator
+from types import TracebackType
 
 import pytest
 
@@ -10,12 +11,22 @@ def no_internet_connection(*args, **kwargs):
     """When mocked to `socket.socket.connect`, this emulates no network connection."""
 
 
+class NetworkDisabler:
+    """Context manager to disable the network connection to emulate no internet connection."""
+
+    def __enter__(self):
+        socket.socket.connect = no_internet_connection
+
+    def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None) -> None:
+        socket.socket.connect = ORIGINAL_SOCKET_CONNECTION
+
+
 @pytest.fixture(scope="function")
 def disable_network() -> Generator[None, None, None]:
     """Disables the network connection to emulate no internet connection."""
 
-    socket.socket.connect = no_internet_connection
-    yield
+    with NetworkDisabler():
+        yield
 
 
 @pytest.fixture(scope="function")
