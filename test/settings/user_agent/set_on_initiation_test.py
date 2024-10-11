aa9a2bab9336d76c568d193d64ba608f849bd980
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-10-11 09:40:48 +0200

    Rearranged test files and added placeholder in settings folder after giving up on finding the cause of the issues.

---
+++ test/settings/user_agent/set_on_initiation_test.py
@@ -0,0 +1,4 @@
+def test_user_agent_set_on_initiation_with_various_browsers_temporarily_disabled() -> None:
+    # TODO: Refer to test in browser folder. For unknown reasons, the same test fails if executed from this folder.
+
+    assert True
