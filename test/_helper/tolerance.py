5291a8e4484351f7e478735d71c50d3c7909bb0e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2024-12-12 15:50:50 +0100

    Updated comments.

--- test/_helper/tolerance.py
+++ test/_helper/tolerance.py
@@ -1,8 +1,12 @@
 def add_percent(value: float, tolerance_percent: float) -> float:
+    """Increases a value by a given percentage."""
+
     return value * (1 + convert_percent_to_float(tolerance_percent))
 
 
 def deduct_percent(value: float, tolerance_percent: float) -> float:
+    """Decreases a value by a given percentage."""
+
     return value * (1 - convert_percent_to_float(tolerance_percent))
 
 
