bbd85925d8a001e08e70968974a8512f94b85bc9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-02-10 18:24:11 +0100

    Another attempt to fix Pytest issue: "AssertionError: assert 'file.txt.crdownload' == 'file.txt'"

--- test/download_handler/simulate_timing_test.py
+++ test/download_handler/simulate_timing_test.py
@@ -53,7 +53,7 @@ class DownloadHandlerThread(Thread):
     def run(self) -> None:
         download_handler = get_download_handler(self.browser, self.download_dir_entries_before_download, self.uses_temporary_file)
         if operating_system.is_windows() and is_python_version(3, 13):
-            time.sleep(0.2)  # Add a delay for Python 3.13 running on Windows as its file creation is less responsive.
+            time.sleep(0.5)  # Add a delay for Python 3.13 running on Windows as its file creation is less responsive.
         assert download_handler.await_and_get_final_file().name == FINAL_FILE_NAME
         assert download_handler._final_file is not None
         assert download_handler._final_file.name == FINAL_FILE_NAME
