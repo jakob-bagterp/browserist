5ca125a3e78460d7b39326b11173aa77606a101c
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-02-10 20:59:13 +0100

    Another attempt to fix Pytest issue: "AssertionError: assert 'file.txt.crdownload' == 'file.txt'"

--- test/download_handler/simulate_timing_test.py
+++ test/download_handler/simulate_timing_test.py
@@ -6,10 +6,12 @@ from threading import Thread
 import pytest
 from _fixture.download_handler import get as get_download_handler
 from _helper import directory, file
+from _helper.python import is_python_version
 from _helper.timeout import reset_to_not_timed_out
 from py.path import local
 
 from browserist import Browser, BrowserSettings, BrowserType
+from browserist.helper import operating_system
 
 FINAL_FILE_NAME = "file.txt"
 PRELIMINARY_TEMPORARY_FILE_NAME = ".com.google.Chrome.1a2b3c"
@@ -83,6 +85,8 @@ def test_simulate_file_download_in_timed_stage_scenarios_for_download_handler(pr
     browser_settings = BrowserSettings(headless=True, download_dir=download_dir, check_connection=False)
     if browser_settings.type not in [BrowserType.CHROME, BrowserType.EDGE]:
         pytest.skip(f"Timing tests for DownloadHandler are only supported by Chrome and Edge, not {browser_settings.type}.")
+    if operating_system.is_windows() and is_python_version(3, 13):
+        pytest.skip("When Python 3.13 runs on Windows there are inconstencies in the timing of temporary and final file names.")
 
     with Browser(browser_settings) as browser:
         with expectation_of_no_exceptions_raised():
