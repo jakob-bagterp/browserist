934596b64a2cd842fc67e57126617cfac860a584
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-03-15 19:41:38 +0100

    Minor updated position of height test.

--- test/browser/scroll/page/to_end_test.py
+++ test/browser/scroll/page/to_end_test.py
@@ -14,12 +14,12 @@ def test_scroll_to_end_of_page(browser_default_headless: Browser) -> None:
 
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.SCROLL_LONG_VERTICAL)
+    total_srcroll_height = browser.scroll.get.total_height()
+    assert total_srcroll_height > 0
     browser.scroll.page.to_top()
     x_default, y_default = browser.scroll.get.position()
     assert x_default == 0
     assert y_default == 0
-    total_srcroll_height = browser.scroll.get.total_height()
-    assert total_srcroll_height > 0
     browser.scroll.page.to_end()
     x_end, y_end = browser.scroll.get.position()
     assert x_end == 0
