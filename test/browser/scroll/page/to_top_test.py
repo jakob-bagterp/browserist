6709374fed00d0e5b07fd58cce0970187135a9a2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-03-15 19:42:31 +0100

    Updated assertions for test_scroll_to_top_of_page.

--- test/browser/scroll/page/to_top_test.py
+++ test/browser/scroll/page/to_top_test.py
@@ -7,9 +7,13 @@ from browserist import Browser
 def test_scroll_to_top_of_page(browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.SCROLL_LONG_VERTICAL)
+    total_srcroll_height = browser.scroll.get.total_height()
+    assert total_srcroll_height > 0
     browser.scroll.page.to_end()
     x_end, y_end = browser.scroll.get.position()
+    assert x_end == 0
+    assert y_end > 0
     browser.scroll.page.to_top()
     x_top, y_top = browser.scroll.get.position()
-    assert x_top == x_end == 0
+    assert x_top == 0
     assert y_top < y_end
