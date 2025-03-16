c99f583495677c8fe11b0c90a8a1400a3906ccd9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-03-16 18:14:26 +0100

    Refactored URL in test.

--- test/settings/proxy/set_proxy_test.py
+++ test/settings/proxy/set_proxy_test.py
@@ -25,13 +25,15 @@ ORIGIN_IP_ADDRESS_PATTERN = re.compile(r'"origin":\s*"\d{1,3}\.\d{1,3}\.\d{1,3}\
     (BrowserType.FIREFOX, PROXY_SETTINGS),
 ])
 def test_set_proxy(browser_type: BrowserType, proxy: ProxySettings | str) -> None:
+    HTTPBIN_IP_URL = "https://httpbin.io/ip"
+
     browser_settings_without_proxy = BrowserSettings(
         type=browser_type,
         headless=True,
         check_connection=False,
     )
     with Browser(browser_settings_without_proxy) as browser_without_proxy:
-        browser_without_proxy.open.url("https://httpbin.io/ip")
+        browser_without_proxy.open.url(HTTPBIN_IP_URL)
         page_source_without_proxy = browser_without_proxy.get.html.page_source()
         assert ORIGIN_IP_ADDRESS_PATTERN.search(page_source_without_proxy)
 
@@ -43,7 +45,7 @@ def test_set_proxy(browser_type: BrowserType, proxy: ProxySettings | str) -> Non
     )
     with Browser(browser_settings_with_proxy) as browser_with_proxy:
         assert browser_with_proxy._browser_driver.settings._proxy_url == PROXY_URL
-        browser_with_proxy.open.url("https://httpbin.io/ip")
+        browser_with_proxy.open.url(HTTPBIN_IP_URL)
         page_source_with_proxy = browser_with_proxy.get.html.page_source()
         assert ORIGIN_IP_ADDRESS_PATTERN.search(page_source_with_proxy)
 
