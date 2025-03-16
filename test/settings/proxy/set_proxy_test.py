7af67be3ab6f05e4aa31d322447ee90f3054682a
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-03-16 16:56:01 +0100

    Refactored proxy test to constants instead of variables (part 2).

--- test/settings/proxy/set_proxy_test.py
+++ test/settings/proxy/set_proxy_test.py
@@ -4,6 +4,7 @@ from browserist import Browser, BrowserSettings
 
 PROXY_IP = "72.10.164.178"
 PROXY_PORT = 27849
+PROXY_URL = f"{PROXY_IP}:{PROXY_PORT}"
 
 ORIGIN_IP_ADDRESS_PATTERN = re.compile(r'"origin":\s*"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}"')  # This is the pattern of the response from httpbin.io/ip, e.g. '"origin": "127.0.0.1:8000"'.
 
@@ -18,11 +19,10 @@ def test_set_proxy() -> None:
         page_source_without_proxy = browser_without_proxy.get.html.page_source()
         assert ORIGIN_IP_ADDRESS_PATTERN.search(page_source_without_proxy)
 
-    proxy_url = f"{PROXY_IP}:{PROXY_PORT}"
     browser_settings_with_proxy = BrowserSettings(
         headless=True,
         check_connection=False,
-        proxy=proxy_url,
+        proxy=PROXY_URL,
     )
     with Browser(browser_settings_with_proxy) as browser_with_proxy:
         browser_with_proxy.open.url("https://httpbin.io/ip")
