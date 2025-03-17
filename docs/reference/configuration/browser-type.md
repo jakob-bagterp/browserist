e613308357eeeb702e264e018b00310e45a8c523
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-03-17 23:52:53 +0100

    Added documentation page for BrowserType.

---
+++ docs/reference/configuration/browser-type.md
@@ -0,0 +1,18 @@
+---
+title: Documentation of Browser Type Settings
+description: Learn how to use different browser types such as Chrome, Firefox, Edge, etc. with Browserist to automate your web scraping or perform browser compatibility testing.
+tags:
+    - Documentation
+    - Tutorial
+    - Settings
+    - Browser Types
+    - Chrome
+    - Edge
+    - Firefox
+    - Internet Explorer
+    - Safari
+---
+
+# `BrowserType`
+
+::: browserist.model.browser.base.type.BrowserType
--- src/browserist/model/browser/base/type.py
+++ src/browserist/model/browser/base/type.py
@@ -3,14 +3,42 @@ from enum import Enum, unique
 
 @unique
 class BrowserType(Enum):
-    """Class to define browser type.
+    """Class to define browser type as used in [`BrowserSettings`](browser-settings.md).
 
     Attributes:
-        CHROME: Chrome browser.
-        EDGE: Edge browser.
-        FIREFOX: Firefox browser.
-        INTERNET_EXPLORER: Internet Explorer browser.
-        SAFARI: Safari browser.
+        BrowserType.CHROME (Enum): Chrome browser. Default browser (except on Windows).
+        BrowserType.EDGE (Enum): Edge browser. Default on Windows.
+        BrowserType.FIREFOX (Enum): Firefox browser.
+        BrowserType.Enum (Enum): Internet Explorer browser.
+        BrowserType.SAFARI (Enum): Safari browser.
+
+
+    Example:
+        Use Firefox as browser type:
+
+        ```python title="" linenums="1" hl_lines="3"
+        from browserist import Browser, BrowserSettings
+
+        settings = BrowserSettings(browser_type=BrowserType.FIREFOX)
+
+        with Browser(settings) as browser:
+            browser.open.url("https://example.com")
+        ```
+
+        How to run multiple browsers:
+
+        ```python title="" linenums="1" hl_lines="3-5 7"
+        from browserist import Browser, BrowserSettings, BrowserType
+
+        chrome = BrowserSettings(type=BrowserType.CHROME)
+        edge = BrowserSettings(type=BrowserType.EDGE)
+        firefox = BrowserSettings(type=BrowserType.FIREFOX)
+
+        for settings in [chrome, edge, firefox]:
+            with Browser(settings) as browser:
+                browser.open.url("https://example.com")
+                browser.wait.seconds(5)
+        ```
     """
 
     CHROME = "Chrome"
