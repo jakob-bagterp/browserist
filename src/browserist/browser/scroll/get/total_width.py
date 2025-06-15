aff47d6a755216cb6d2dfcc09ce14a8d4e782f2d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-06-15 16:39:17 +0200

    Minor linting update of script.

--- src/browserist/browser/scroll/get/total_width.py
+++ src/browserist/browser/scroll/get/total_width.py
@@ -3,9 +3,10 @@ from ...tool.execute_script import tool_execute_script
 
 
 def get_total_scroll_width(browser_driver: BrowserDriver) -> int:
-    script_get_total_scroll_width = """return Math.max(
-        document.body.scrollWidth, document.documentElement.scrollWidth,
-        document.body.offsetWidth, document.documentElement.offsetWidth,
-        document.body.clientWidth, document.documentElement.clientWidth
-    );"""
+    script_get_total_scroll_width = """
+        return Math.max(
+            document.body.scrollWidth, document.documentElement.scrollWidth,
+            document.body.offsetWidth, document.documentElement.offsetWidth,
+            document.body.clientWidth, document.documentElement.clientWidth
+        );"""
     return int(tool_execute_script(browser_driver, script_get_total_scroll_width))
