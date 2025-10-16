ebf93fb1393371f7ce45386232c87bf07ff1422f
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-10-16 11:49:52 +0200

    Attempted fix of scroll test issues on Python 3.14.

--- test/browser/scroll/page/up_test.py
+++ test/browser/scroll/page/up_test.py
@@ -21,8 +21,8 @@ from browserist.helper import operating_system
 ])
 def test_scroll_page_up(pages: int, browser_default_headless: Browser) -> None:
     # TODO: Remove this once we have a fix for this exception:
-    if operating_system.is_macos() and any([is_python_version(3, 11), is_python_version(3, 12), is_python_version(3, 13)]):
-        pytest.skip("When this runs on MacOS with Python 3.11, 3.12 or 3.13, the last scroll position assertion fails.")
+    if operating_system.is_macos() and any([is_python_version(3, 11), is_python_version(3, 12), is_python_version(3, 13), is_python_version(3, 14)]):
+        pytest.skip("When this runs on MacOS with Python 3.11, 3.12, 3.13 or 3.14, the last scroll position assertion fails.")
 
     browser = reset_to_not_timed_out(browser_default_headless)
     browser.open.url(internal_url.SCROLL_LONG_VERTICAL)
