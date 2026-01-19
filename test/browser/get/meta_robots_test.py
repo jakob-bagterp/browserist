c7d5d393653ce6ddb703997a27bb09392b81e201
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-01-19 16:32:18 +0100

    Added minor comment to test.

--- test/browser/get/meta_robots_test.py
+++ test/browser/get/meta_robots_test.py
@@ -8,7 +8,7 @@ from browserist import Browser
 @pytest.mark.parametrize("url, expected_meta_robots", [
     (internal_url.MINI_SITE_HOMEPAGE, "noindex"),
     (internal_url.MINI_SITE_FEATURE_1, "index, follow"),
-    (internal_url.SEARCH, ""),
+    (internal_url.SEARCH, ""),  # Doesn't have any meta description.
 ])
 def test_get_meta_robots(url: str, expected_meta_robots: str, browser_default_headless: Browser) -> None:
     browser = reset_to_not_timed_out(browser_default_headless)
