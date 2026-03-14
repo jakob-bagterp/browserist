5e63c484d098345501106631f22201e60243ed53
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-03-14 17:53:00 +0100

    Minor updated comment.

--- test/_helper/environment.py
+++ test/_helper/environment.py
@@ -13,7 +13,7 @@ def skip_if_github_actions(
     return pytest.mark.skipif(is_github_actions(), reason=reason)
 
 
-SKIP_TIMING_PERFORMANCE_TESTS = "Test skipped in GitHub Actions as it evaluates timing and/or network performance indirectly, and so likely fails on GitHub Actions. Only run on a local machine."
+SKIP_TIMING_PERFORMANCE_TESTS = "Test skipped as it evaluates timing and/or network performance indirectly, and so it likely fails on GitHub Actions. Only run on a local machine."
 
 FAILS_ON_GITHUB_ACTIONS = (
     "Test skipped as it (most often) fails on GitHub Actions, but not when running on a local machine."
