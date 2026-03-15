056dbb2df43cc5685e86785edc6df2d2686992bd
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-03-15 10:03:44 +0100

    Added task kill process for Windows in attempt to forcefully close Pytest session.

--- test/conftest.py
+++ test/conftest.py
@@ -1,15 +1,29 @@
 import os
-import platform
+import subprocess
 
 import pytest
 
+from browserist.helper import operating_system
 
-def pytest_sessionfinish(session: pytest.Session, exitstatus: int | pytest.ExitCode) -> None:
-    """
-    Diagnostic hook: If Windows hangs due to background threads or file locks,
-    this forces the Python process to exit immediately with the correct status.
-    """
-    if platform.system() == "Windows":
-        # Force the OS to kill the process, skipping thread joins and garbage collection
-        # Cast exitstatus to int in case it is passed as a pytest.ExitCode enum
+
+def pytest_session_cleanup_for_windows(session: pytest.Session, exitstatus: int | pytest.ExitCode) -> None:
+    """If Windows hangs due to background threads or file locks, this forces the Python process to exit immediately with the correct status, including skipping thread joins and garbage collection of browser processes."""
+
+    def kill_process(process: str) -> None:
+        subprocess.run(
+            ["taskkill", "/F", "/IM", process, "/T"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False
+        )
+
+    if operating_system.is_windows():
+        browser_processes = [
+            "chromedriver.exe",
+            "chrome.exe",
+            "msedgedriver.exe",
+            "msedge.exe",
+            "geckodriver.exe",
+            "firefox.exe",
+        ]
+        for process in browser_processes:
+            kill_process(process)
+
         os._exit(int(exitstatus))
