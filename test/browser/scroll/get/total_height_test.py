7035bad636131a419f9291909e1ccb13d72c60bb
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-08-08 14:24:14 +0200

    Refactored tolerance for test_get_total_scroll_height(), so it's more flexible.

--- test/browser/scroll/get/total_height_test.py
+++ test/browser/scroll/get/total_height_test.py
@@ -8,17 +8,22 @@ from browserist.helper import operating_system
 
 
 @pytest.mark.parametrize(
-    "url, expected_total_scroll_height",
-    [(internal_url.NOT_SCROLLABLE, 540), (internal_url.SCROLL_VERTICAL_LONG, 40802)],
+    "url, expected_total_scroll_height, tolerance_percent",
+    [
+        (internal_url.NOT_SCROLLABLE, 540, 10),
+        (internal_url.SCROLL_VERTICAL_LONG, 40802, 60 if operating_system.is_windows() else 10),
+    ],
 )
 @pytest.mark.xdist_group(name="serial_scroll_tests")
 def test_get_total_scroll_height(
-    url: str, expected_total_scroll_height: int, browser_default_headless_scope_function: Browser
+    url: str,
+    expected_total_scroll_height: int,
+    tolerance_percent: float,
+    browser_default_headless_scope_function: Browser,
 ) -> None:
     browser = reset_to_not_timed_out(browser_default_headless_scope_function)
     browser.open.url(url)
     total_scroll_height = browser.scroll.get.total_height()
-    TOLERANCE_HEIGHT_PERCENT = 60 if operating_system.is_windows() else 10
-    minimum_height = _helper.tolerance.deduct(expected_total_scroll_height, TOLERANCE_HEIGHT_PERCENT)
-    maximum_height = _helper.tolerance.add(expected_total_scroll_height, TOLERANCE_HEIGHT_PERCENT)
+    minimum_height = _helper.tolerance.deduct(expected_total_scroll_height, tolerance_percent)
+    maximum_height = _helper.tolerance.add(expected_total_scroll_height, tolerance_percent)
     assert minimum_height < total_scroll_height < maximum_height
